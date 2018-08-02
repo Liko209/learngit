f2b93a05afcfea0b3c603ca9426a70ae38353957
Dennis Jiang <dennis.jiang@ringcentral.com>
2018-08-02 14:44:08 +0800

    fix(foundation): tslint issue

--- packages/foundation/src/network/util.ts
+++ packages/foundation/src/network/util.ts
@@ -5,7 +5,7 @@
  */
 function generateUUID() {
   let d = new Date().getTime();
-  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = ((d + Math.random() * 16) % 16) | 0; // eslint-disable-line
     d = Math.floor(d / 16);
     return (c === 'x' ? r : (r & 0x7) | 0x8).toString(16); // eslint-disable-line
