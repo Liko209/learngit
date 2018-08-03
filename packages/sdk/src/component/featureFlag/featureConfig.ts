9aa710d7108836af365950f4753c4f880a154a77
Andy Hu <869732751@qq.com>
2018-08-03 10:23:52 +0800

    feat(featureflag): featureflag

--- packages/sdk/src/component/featureFlag/featureConfig.ts
+++ packages/sdk/src/component/featureFlag/featureConfig.ts
@@ -1,7 +1,7 @@
 import { IFeatureConfig, BETA_FEATURE } from './interface';
 
 const featureConfig: IFeatureConfig = {
-  [BETA_FEATURE.LOG]: ['beta_enable_log'],
+  [BETA_FEATURE.LOG]: ['beta_enable_log_emails', 'beta_enable_log_domains'],
   [BETA_FEATURE.SMS]: ['SMS_beta'],
 };
 
