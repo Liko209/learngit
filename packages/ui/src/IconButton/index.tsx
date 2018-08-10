6717fca056b995311183e128eb51a2b9efb1a9a5
Chris Zhan <chris.zhan@ringcentral.com>
2018-08-10 13:20:40 +0800

    feat(component): shelf page header

--- packages/ui/src/IconButton/index.tsx
+++ packages/ui/src/IconButton/index.tsx
@@ -1,19 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
 import MuiIconButton, { IconButtonProps } from '@material-ui/core/IconButton';
-import { WithTheme } from '@material-ui/core';
+import { WithTheme, Icon, Tooltip } from '@material-ui/core';
 
-export type TIconButtonProps = {} & IconButtonProps &
+export type TIconButtonProps = {
+  tooltipTitle?: string;
+  invisible?: boolean;
+  awake?: boolean;
+  active?: boolean;
+  size?: 'small' | 'medium' | 'large';
+} & IconButtonProps &
   Partial<Pick<WithTheme, 'theme'>>;
+
+const MuiIconButtonWrapper = styled(MuiIconButton)`
+  &.muiIconButtonWrapper {
+    border-radius: 100%;
+    &:hover {
+      background-color: transparent;
+    }
+  }
+`;
+
+const IconWrapper = styled(Icon)``;
 
 export const CustomIconButton: React.SFC<TIconButtonProps> = (
   props: TIconButtonProps,
 ) => {
-  return <MuiIconButton {...props} />;
+  const { children, tooltipTitle, innerRef, awake, active, invisible, ...rest } = props;
+  const classes = {
+    root: 'muiIconButtonWrapper',
+  };
+  const main = (
+    <MuiIconButtonWrapper disableRipple={true} classes={classes} {...rest}>
+      <IconWrapper>{children}</IconWrapper>
+    </MuiIconButtonWrapper>
+  );
+  return tooltipTitle ? (<Tooltip title={tooltipTitle}>{main}</Tooltip>) : main;
 };
 
-export const IconButton = styled<TIconButtonProps>(CustomIconButton).attrs(
-  {},
-)``;
+const sizes = {
+  large: '24px',
+  medium: '20px',
+  small: '16px',
+};
+
+export const IconButton = styled<TIconButtonProps>(CustomIconButton)`
+  && {
+    width: ${props => props.size ? sizes[props.size] : sizes['medium']};
+    height: ${props => props.size ? sizes[props.size] : sizes['medium']};
+    ${MuiIconButtonWrapper} {
+      width: ${props => props.size ? sizes[props.size] : sizes['medium']};
+      height: ${props => props.size ? sizes[props.size] : sizes['medium']};
+    }
+    ${IconWrapper} {
+      color: ${props =>
+    props.disabled ? '#BFBFBF' :
+      props.active ? props.theme.palette.primary.main :
+        props.awake ? '#9e9e9e' :
+          props.invisible ? 'transparent' : '#BFBFBF'};
+      font-size: ${props => props.size ? sizes[props.size] : sizes['medium']};
+      :hover {
+        color: ${props => props.theme.palette.primary.main}
+      }
+    }
+  }
+`;
 
 export default IconButton;
