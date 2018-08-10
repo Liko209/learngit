b3f7880e77d83b6beceb316c58ff48ea077aa570
Lip Wang <lip.wang@ringcentral.com>
2018-08-10 14:01:12 +0800

    feat: move batch upload log back from old project

--- packages/sdk/src/service/windowEventListener.ts
+++ packages/sdk/src/service/windowEventListener.ts
@@ -3,17 +3,23 @@
  * @Date: 2018-06-20 10:48:13
  */
 
-import { SOCKET, DOCUMENT } from '../service/eventKey';
+import { SOCKET, DOCUMENT, WINDOW } from '../service/eventKey';
 import notificationCenter from './notificationCenter';
 
 window.addEventListener(
   'online',
-  () => notificationCenter.emit(SOCKET.NETWORK_CHANGE, { state: 'online' }),
+  () => {
+    notificationCenter.emit(SOCKET.NETWORK_CHANGE, { state: 'online' });
+    notificationCenter.emit(WINDOW.ONLINE, { onLine: true });
+  }
 );
 
 window.addEventListener(
   'offline',
-  () => notificationCenter.emit(SOCKET.NETWORK_CHANGE, { state: 'offline' }),
+  () => {
+    notificationCenter.emit(SOCKET.NETWORK_CHANGE, { state: 'offline' });
+    notificationCenter.emit(WINDOW.ONLINE, { onLine: false });
+  }
 );
 
 window.addEventListener('load', () => {
@@ -26,6 +32,10 @@ window.addEventListener('focus', () => {
   if (navigator.onLine) {
     notificationCenter.emit(SOCKET.NETWORK_CHANGE, { state: 'focus' });
   }
+});
+
+window.addEventListener('blur', () => {
+  notificationCenter.emit(WINDOW.BLUR);
 });
 
 document.addEventListener('visibilitychange', () => {
