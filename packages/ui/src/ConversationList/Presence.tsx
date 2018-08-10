6017f2570e6f4c7a0dd6a5a889e0a79dbf25009b
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-08-10 16:32:01 +0800

    fix(ui): d file

--- packages/ui/src/ConversationList/Presence.tsx
+++ packages/ui/src/ConversationList/Presence.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
 import { WithTheme } from '@material-ui/core';
 
 export type PresenceProps = {
   status?: string;
-} & Partial<Pick<WithTheme, 'theme'>>;
+} & Partial<Pick<WithTheme, 'theme'>> & React.HTMLAttributes<HTMLDivElement>;
 
 const PRESENCE_COLOR_MAP = {
   online: '#4cd964',
@@ -12,7 +13,7 @@ const PRESENCE_COLOR_MAP = {
   default: 'transparent',
 };
 
-const Presence = styled.div`
+const Presence = styled<PresenceProps, 'div'>('div')`
   width: 8px;
   height: 8px;
   margin: 6px 8px;
