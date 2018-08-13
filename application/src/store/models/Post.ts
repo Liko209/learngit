227595dc3bd1ecc34675c0dcec529930122aa375
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-08-13 14:34:15 +0800

    build(lerna): hoist

---
+++ application/src/store/models/Post.ts
@@ -0,0 +1,7 @@
+export default class PostModel {
+  static fromJS(data: any) {
+    return data;
+  }
+
+  dispose() {} // eslint-disable-line
+}
---
+++ application/src/store/models/Profile.ts
@@ -0,0 +1,10 @@
+export default class ProfileModel {
+  static fromJS(data: any) {
+    const { favorite_post_ids: favoritePostIds = [] } = data;
+    const model = {
+      favoritePostIds
+    };
+
+    return model;
+  }
+}
