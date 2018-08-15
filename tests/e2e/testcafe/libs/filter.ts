65fd72657d8aa8cc0696c83c1c8b4b6829dfbc34
potar.he <potar.he@ringcentral.com>
2018-08-15 15:59:44 +0800

    chore(e2e): wip: filter cases by tags

--- tests/e2e/testcafe/libs/filter.ts
+++ tests/e2e/testcafe/libs/filter.ts
@@ -1,35 +1,56 @@
-function formalCaseName(name:string,tags?:Array<string>): string{
-    let tagString : string = ""
-    if (tags){
-        for (let i in tags){
-            tagString += "[" + tags[i] + "]"
-        }
-        return tagString + " "+ name;
-    }
-    return name;
+/*
+ * @Author: Potar He(Potar.He@ringcentral.com)
+ * @Date: 2018-08-15 11:15:59
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+function isValidTag(tag: string): boolean {
+    return /^[a-zA-Z_$][a-zA-Z_$0-9 ]*$/.test(tag);
 }
 
-function parseCaseName(formalName: string): any {
-    let indexPosition: number = 0;
-    let tags:string[] = [];
-    while (true){
-        let tagStart: number = formalName.indexOf('[',indexPosition);
-        let tagEnd: number = formalName.indexOf(']',tagStart);
-        if (tagEnd <= indexPosition){
-            break;
-        }
-        indexPosition = tagEnd + 1;
-        let tag = formalName.slice(tagStart+1,tagEnd);
-        tags.push(tag);
+function validateTag(tag: string) {
+    if (!isValidTag(tag)) {
+        throw new Error(`Invalid tag value: ${tag}!`);
     }
-    let name: string = formalName.slice(indexPosition);
-    let caseName = {
-        "name": name.replace( /^\s*/, "").replace( /\s*$/, ""),
-        "tags": tags
-    };
-    return caseName;
 }
 
-let str = "[p1][p2] ahah  "
-console.log(parseCaseName(str));
-console.log(formalCaseName("ahah",['p1', 'p2']));
+function isValidName(name: string): boolean {
+    return /^[^\[]/.test(name);
+}
+
+function validateName(name: string) {
+    if (!isValidName(name)) {
+        throw new Error(`Invalid name value: ${name}!`);
+    }
+}
+
+export function formalName(name: string, tags?: Array<string>): string {
+    let formalName: string = name;
+    validateName(formalName);
+    if (tags) {
+        formalName = tags
+            .map(tag => tag.trim())
+            .map(tag => validateTag(tag) && `[${tag}]`)
+            .join("") + " " + formalName;
+    }
+    console.log("name?:"+ formalName);
+    return formalName;
+}
+
+export function parseFormalName(formalName: string): any {
+    let tags: Array<string> = [];
+    let rest = formalName;
+    let match;
+    while (match = /^\[([^\]]+?)\](.*)$/.exec(rest)) {
+        console.log(match);
+        tags.push(match[1]);
+        rest = match[2];
+    }
+    return { name: rest.trim(), tags: tags };
+}
+
+function filterByTags(tag: string, tags: Array<string>, include: boolean) {
+    
+}
+
+
