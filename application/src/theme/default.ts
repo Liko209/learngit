8fcfc156485f7acde68c0d32fcae1e6fee8fb559
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-08-17 08:32:22 +0800

    feat(theme): configure the theme

--- application/src/theme/default.ts
+++ application/src/theme/default.ts
@@ -1,34 +1,57 @@
 const theme = {
-  primary: {
-    // light: will be calculated from palette.primary.main,
-    main: '#ff4400',
-    // dark: will be calculated from palette.primary.main,
-    // contrastText: will be calculated to contast with palette.primary.main
+  palette: {
+    primary: {
+      light: '#18a4de',
+      main: '#0684bd',
+      dark: '#005488',
+      900: '#005488',
+      800: '#005488',
+      700: '#0684bd',
+      600: '#0684db',
+      500: '#18a4de',
+      400: '#34b1e2',
+      300: '#54bfe6',
+      200: '#82d1ed',
+      100: '#b2e3f4',
+      50: '#e1f4fb',
+    },
+    secondary: {
+      light: '#ffa424',
+      main: '#ff8800',
+      dark: '#f36703',
+      900: '#ea4b04',
+      800: '#f36703',
+      700: '#f97802',
+      600: '#ff8800',
+      500: '#ff9400',
+      400: '#ffa424',
+      300: '#ffb44c',
+      200: '#ffca7f',
+      100: '#ffdfb1',
+      50: '#fff3e0',
+    },
+    semantic: {
+      negative: '#f44336',
+      positive: '#26a304',
+      critical: '#f6ad16',
+      neutral: '#8c8c8c',
+    },
+    accent: {
+      ash: '#bfbfbf',
+      lake: '#69a3eb',
+      tiffany: '#22c2d6',
+      cateye: '#52d2b1',
+      grass: '#bdd655',
+      gold: '#edb63c',
+      persimmon: '#ff793d',
+      tomato: '#ff3737',
+    },
   },
-  secondary: {
-    light: '#0066ff',
-    main: '#0044ff',
-    // dark: will be calculated from palette.secondary.main,
-    contrastText: '#ffcc00',
+  shape: {
+    borderRadius: 2,
   },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    // In Japanese the characters are usually larger.
-    fontSize: 12,
-    // Tell Material-UI what's the font-size on the html element is.
-    htmlFontSize: 10,
+  spacing: {
+    unit: 4,
   },
 };
 
