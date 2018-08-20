932649b60a1be31c6637cd121f681e6d6844cb18
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-08-20 13:47:13 +0800

    feat(theme): remove theme config in application

--- application/src/theme/index.ts
+++ application/src/theme/index.ts
@@ -1,42 +1,16 @@
-import { createTheme } from 'ui-components/theme';
 import config from '@/config';
 
 interface IThemeConfig {
   default: string;
-  theme: string[];
-}
-
-function createThemes(config: IThemeConfig) {
-  const requireContext = require.context('./', false, /.ts$/);
-  const themePaths = requireContext.keys();
-  const themeNames = themePaths.map((path: string) =>
-    path.split('.')[1].split('/')[1],
-  );
-  const { theme } = config;
-
-  return theme
-    .map((name: string) => {
-      const index = themeNames.indexOf(name);
-      if (index !== -1) {
-        return {
-          name,
-          theme: createTheme(requireContext(themePaths[index])),
-        };
-      }
-      return;
-    })
-    .reduce((allTheme, theme) => {
-      if (theme) {
-        allTheme[theme.name] = theme.theme;
-      }
-      return allTheme;
-    },      {});
+  themes: string[];
 }
 
 const themeConfig: IThemeConfig = config.get('theme');
-const themes = createThemes(themeConfig);
 
-console.log(themes);
+const defaultTheme = themeConfig.default;
+const themes = themeConfig.themes;
 
-export const defaultTheme = themes[themeConfig.default];
-export default themes;
+export {
+  defaultTheme,
+  themes,
+};
