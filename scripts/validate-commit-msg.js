7fad42d341a0ac3ce00eb625aa5a21ba5edda849
Alvin Huang <alvin.huang@ringcentral.com>
2018-08-21 15:00:11 +0800

    fix(test): test

--- scripts/validate-commit-msg.js
+++ scripts/validate-commit-msg.js
@@ -1,6 +1,6 @@
 /*
  * @Author: Alvin Huang (alvin.huang@ringcentral.com)
- * @Date: 2018-7-18 15:08:04
+ * @Date: 2018-7-18 15:08:05
  * Copyright © RingCentral. All rights reserved.
  */
 const chalk = require('chalk')
