f1242a48cd23971f5e9567f01f2195a6dae14c11
Devin Lin <adntin@qq.com>
2018-08-22 16:58:13 +0800

    refactor(layout): horizon Button and Panel and Resizer

--- application/src/containers/Layout/HorizonPanel.tsx
+++ application/src/containers/Layout/HorizonPanel.tsx
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 
 type TPosition = 'left' | 'right';
 
-interface IWrapperPorps {
+interface IInternalPorps {
   width: number;
   minWidth?: number;
   maxWidth?: number;
@@ -11,34 +11,37 @@ interface IWrapperPorps {
   forcePosition?: TPosition;
 }
 
-interface IPanelProps extends IWrapperPorps {
+interface IExternalPorps extends IInternalPorps {
   onClick?: (event: MouseEvent) => void;
   children: ReactNode;
 }
 
 const Wrapper = styled.div`
-  display: inline-block;
-  height: 100%;
   vertical-align: top;
-  ${(props: IWrapperPorps) => props.width ? css`
-    width: ${(props: IWrapperPorps) => `${props.width}px`};
-  ` : css`
-    display: none;
-  `}
+  height: 100%;
+  display: ${(props: IInternalPorps) => props.width > 0 ? 'inline-block' : 'none'};
+  width: ${(props: IInternalPorps) => `${props.width}px`};
   ${props => props.forceDisplay && css`
     display: inline-block;
     width: 180px;
     position: absolute;
     top: 0;
     bottom: 0;
-    left: ${(props: IWrapperPorps) => props.forcePosition === 'left' ? 0 : 'auto'};
-    right: ${(props: IWrapperPorps) => props.forcePosition === 'right' ? 0 : 'auto'};
+    left: ${(props: IInternalPorps) => props.forcePosition === 'left' ? 0 : 'auto'};
+    right: ${(props: IInternalPorps) => props.forcePosition === 'right' ? 0 : 'auto'};
   `}
 `;
 
-const Panel = ({ width, minWidth, maxWidth, forceDisplay, forcePosition, onClick, children }: IPanelProps) => {
+const Panel = ({ width, minWidth, maxWidth, forceDisplay, forcePosition, onClick, children }: IExternalPorps) => {
   return (
-    <Wrapper width={width} data-min-width={minWidth} data-max-width={maxWidth} forceDisplay={forceDisplay} forcePosition={forcePosition} onClick={onClick}>
+    <Wrapper
+      width={width}
+      data-min-width={minWidth}
+      data-max-width={maxWidth}
+      forceDisplay={forceDisplay}
+      forcePosition={forcePosition}
+      onClick={onClick}
+    >
       {children}
     </Wrapper>
   );
