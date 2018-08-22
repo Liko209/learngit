bf428c9955a4c9256b339e48aa9b56240bec2287
Shining <shining.miao@ringcentral.com>
2018-08-22 16:27:26 +0800

    feat(ui): topbar ui component

--- packages/ui/src/atoms/Presence/Presence.tsx
+++ packages/ui/src/atoms/Presence/Presence.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { WithTheme } from '@material-ui/core';
 
 export type PresenceProps = {
-  status?: string;
+  presence?: 'online' | 'away' | 'offline' | 'default';
 } & Partial<Pick<WithTheme, 'theme'>> & React.HTMLAttributes<HTMLDivElement>;
 
 const PRESENCE_COLOR_MAP = {
@@ -17,9 +17,18 @@ const Presence = styled<PresenceProps, 'div'>('div')`
   width: 8px;
   height: 8px;
   margin: 6px 8px;
-  background: ${props => PRESENCE_COLOR_MAP[props.status || 'default']};
+  background: ${props => PRESENCE_COLOR_MAP[props.presence || 'default']};
   border-radius: 50%;
 `;
 
-export { Presence };
+const AvatarPresence = Presence.extend`
+  position: absolute;
+  right: -2px;
+  bottom: -1px;
+  border: 2px solid #fff;
+  z-index: 10;
+  margin: 0;
+`;
+
+export { Presence, AvatarPresence };
 export default { Presence };
