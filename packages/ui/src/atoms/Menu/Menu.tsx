ea54b7bcbcc615716ff0d4012e39bdc4e923f03d
Valor Lin <valor.lin@ringcentral.com>
2018-08-22 11:25:00 +0800

    feat(ui): Menu & MenuItem

---
+++ packages/ui/src/atoms/Menu/Menu.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Valor Lin (valor.lin@ringcentral.com)
+ * @Date: 2018-08-22 11:22:47
+ * Copyright © RingCentral. All rights reserved.
+ */
+import styled from 'styled-components';
+import MuiMenu from '@material-ui/core/Menu';
+
+const Menu = styled(MuiMenu)`
+  && {
+    font-size: 20px;
+    color: #171717;
+  }
+`;
+
+export { Menu };
+export default { Menu };
