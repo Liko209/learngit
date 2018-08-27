6050a96a7557c381b7632f0a84fa3ce7daa30090
Lyman Lai <lyman.lai@ringcentral.com>
2018-08-28 01:45:35 +0800

    chore(script): remove exit 1 in SA

--- autoDevOps/2.SA.sh
+++ autoDevOps/2.SA.sh
@@ -60,7 +60,6 @@ if [ "$hasLintError" = 1 ]; then
   syncFolderToServer $project/lint/ $lintFolder
   lintErrorUrl=https://lint.fiji.gliprc.com/$subDomain/$BUILD_NUMBER/index.html
   addEnv SAResult="> **SA Error**: $lintErrorUrl"
-  exit 1
 else
   addEnv SAResult="> **SA Error**: Do Not Have Lint Error"
 fi
