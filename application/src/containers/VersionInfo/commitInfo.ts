6a6035d6e146b6c3bf50473ff8486c5f4c6304fd
Lip Wang <lip.wang@ringcentral.com>
2018-09-05 14:47:56 +0800

    feat: add build version

---
+++ application/src/containers/VersionInfo/commitInfo.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+export const gitCommitInfo = {
+  currentBranch: 'feature/FIJI-120',
+  commitInfo: [{
+    rawMessage: 'eb3ce2f|Valor Lin|Wed May 30 13:37:37 2018 +0800|feat: add WEB-AQA-XIA env config|6c2946a',
+    treeHash: 'eb3ce2f',
+    author: 'Valor Lin',
+    date: 'Wed May 30 13:37:37 2018 +0800',
+    subject: 'feat: add WEB-AQA-XIA env config',
+    commitHash: '6c2946a',
+  },
+    {
+      rawMessage: 'd769fb7|Valor Lin|Wed May 30 10:52:19 2018 +0800|feat(config): add GLP-CI2-XMN env config|82069a0',
+      treeHash: 'd769fb7',
+      author: 'Valor Lin',
+      date: 'Wed May 30 10:52:19 2018 +0800',
+      subject: 'feat(config): add GLP-CI2-XMN env config',
+      commitHash: '82069a0',
+    },
+    {
+      rawMessage: 'd9ca52f|Valor Lin|Tue May 29 08:55:07 2018 +0000|Merge branch \'feature/FIJI-148\' into \'develop\'|26c9a09',
+      treeHash: 'd9ca52f',
+      author: 'Valor Lin',
+      date: 'Tue May 29 08:55:07 2018 +0000',
+      subject: 'Merge branch \'feature/FIJI-148\' into \'develop\'',
+      commitHash: '26c9a09',
+    },
+  ],
+};
