0054d40a59e27fa6a7ec3fc9630086a027b809e6
Devin Lin <adntin@qq.com>
2018-09-06 16:51:10 +0800

    feat(login): optimize language and server url and authenticator.

--- application/src/containers/UnifiedLogin/index.tsx
+++ application/src/containers/UnifiedLogin/index.tsx
@@ -5,12 +5,11 @@
  */
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import i18next, { TranslationFunction } from 'i18next';
+import { TranslationFunction } from 'i18next';
 import { translate } from 'react-i18next';
-import { stringify } from 'qs';
 import styled from 'styled-components';
+import getUrl from './getUrl';
 
-import config from '@/config';
 import EnvSelect from './EnvSelect';
 import Download from './Download';
 import LoginVersionStatus from '../VersionInfo/LoginVersionStatus';
@@ -52,124 +51,36 @@ const Button = styled.button`
   }
 `;
 
-const getLanguage = () => {
-  const lng = i18next.language || window.localStorage.getItem('i18nextLng') || 'en-US';
-  const arr = lng.split('-');
-  const map = {
-    de: ['DE'],
-    en: ['US', 'GB'],
-    es: ['ES', '419'],
-    fr: ['FR', 'CA'],
-    it: ['IT'],
-    pt: ['BR'],
-    ja: ['JP'],
-  };
-  const locals = map[arr[0]];
-  if (!locals) {
-    return 'en-US';
-  }
-  const local = locals.find((l: string) => l === arr[1]);
-  if (!local) {
-    return arr[0] + '-' + locals[0];
-  }
-  return lng;
-};
-
 interface IProps extends RouteComponentProps<{}> {
   t: TranslationFunction;
 }
 
-interface IStates {
-  disabled: boolean;
-  brandId: string;
-}
+interface IStates { }
 
 class UnifiedLogin extends React.Component<IProps, IStates> {
   constructor(props: IProps) {
     super(props);
-    this.state = {
-      disabled: false,
-      brandId: '1210',
-    };
   }
 
-  onChange = (event: React.FormEvent<HTMLSelectElement>) => {
-    this.setState({ brandId: event.currentTarget.value });
-  }
+  // onChange = (event: React.FormEvent<HTMLSelectElement>) => {
+  //   this.setState({ brandId: event.currentTarget.value });
+  // }
 
   onSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    const { brandId } = this.state;
     const { location } = this.props;
-    const { glip2 } = config.get('api');
-    const url = `${glip2.server}${glip2.apiPlatform}/oauth/authorize`;
-    const { from } = location.state || { from: {} };
-    const { pathname = '/', search = '', hash = '' } = from;
-    const state = pathname + search.replace('&', '$') + hash;
-    const redirect_uri = window.location.origin; // The URI must match exactly with the sandbox configuration
-    const params = {
-      redirect_uri,
-      state,
-      response_type: 'code',
-      client_id: glip2.clientId,
-      brand_id: brandId,
-      ui_locales: getLanguage(), // default en_US
-      force: true,
-      endpointId: '',
-      display: 'touch',
-      prompt: 'login sso',
-      scope: '',
-      ui_options: 'external_popup remember_me_on show_back_to_app',
-      hideNavigationBar: true,
-      glip_auth: true,
-      response_hint: 'remember_me+login_type',
-      title_bar: true,
-    };
-    window.location.href = url + '?' + stringify(params);
-
-    // Auth wiki
-    // https://wiki.ringcentral.com/pages/viewpage.action?pageId=207394909
-
-    // Glip sign In
-    // https://service.ringcentral.com/login/unifiedLoginM.html?
-    // session=-8895747689600087505
-    // &responseType=code
-    // &clientId=cZPfEqZkQxKa9dUEu9RkCA
-    // &brandId=1210
-    // &state=/rc
-    // &localeId=en_US
-    // &endpointId=
-    // &display=touch
-    // &prompt=login sso
-    // &scope=
-    // &appUrlScheme=https://app.glip.com/api/no-auth/rc-signon
-    // &ui_options=external_popup remember_me_on show_back_to_app
-    // &hideNavigationBar=true
-    // &glip_auth=true
-    // &response_hint=remember_me+login_type
-    // &title_bar=true
-
-    // window.location.href = `${glip2.server}${
-    //   glip2.apiPlatform
-    //   }/oauth/authorize?force=true&response_type=code&client_id=${
-    //   glip2.clientId
-    //   }&state=%2Frc&redirect_uri=${
-    //   window.location.origin
-    //   }/unified-login/&brand_id=${
-    //   glip2.brandId
-    //   }&glip_auth=true&display=touch&title_bar=true`;
+    window.location.href = getUrl(location);
   }
 
   render() {
-    const { disabled, brandId } = this.state;
     const { t } = this.props;
     return (
       <div>
         <Form onSubmit={this.onSubmit}>
-          <Button type="submit" disabled={disabled} data-anchor="btnLogin" >
+          <Button type="submit" data-anchor="btnLogin" >
             {t('SignIn')}
           </Button>
-          <select onChange={this.onChange} value={brandId} style={{ display: 'none' }}>
+          {/* <select onChange={this.onChange} value={brandId} style={{ display: 'none' }}>
             <option value="1210">RC US</option>
             <option value="3610">CA</option>
             <option value="2010">EU</option>
@@ -177,7 +88,7 @@ class UnifiedLogin extends React.Component<IProps, IStates> {
             <option value="5010">AU</option>
             <option value="3420">AT&T</option>
             <option value="7310">TELUS</option>
-          </select>
+          </select> */}
           <EnvSelect />
         </Form>
         <LoginVersionStatus />
@@ -188,16 +99,3 @@ class UnifiedLogin extends React.Component<IProps, IStates> {
 }
 
 export default translate('translations')(UnifiedLogin);
-
-// const map = {
-//   'de-DE': 'de-DE', // Deutsch
-//   'en-GB': 'en-GB', // English (U.K.)
-//   'en-US': 'en-US', // English (U.S.)
-//   'es-ES': 'es-ES', // Español
-//   'es-419': 'es-419', // Español (Latinoamérica)
-//   'fr-FR': 'fr-FR', // Français
-//   'fr-CA': 'fr-CA', // Français (Canada)
-//   'it-IT': 'it-IT', // Italiano
-//   'pt-BR': 'pt-BR', // Português (Brasil)
-//   'ja-JP': 'ja-JP', // 日本語
-// };
