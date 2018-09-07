aaa609fb92ff3600f6c0aa5e72a60db12b8d559a
Andy Hu <andy.hu@ringcentral.com>
2018-09-07 11:05:18 +0800

    test(e2e test): display messages of an individual conversation

--- tests/e2e/testcafe/page-models/components/ConversationList/TeamSection.ts
+++ tests/e2e/testcafe/page-models/components/ConversationList/TeamSection.ts
@@ -4,50 +4,33 @@
  * Copyright © RingCentral. All rights reserved.
  */
 import { ReactSelector } from 'testcafe-react-selectors';
-import { GroupAPI, PostAPI } from '../../../libs/sdk';
+import { GroupAPI } from '../../../libs/sdk';
 import { BaseComponent } from '../..';
 
 class TeamSection extends BaseComponent {
+
   get teamSection() {
     return ReactSelector('ConversationListSection').withProps('title', 'Teams');
   }
 
-  get teams() {
-    return this.teamSection.findReact('ConversationListItem');
+  get team() {
+    return this.teamSection.findReact('ConversationListItem').nth(0);
   }
 
-  get team0() {
-    return this.teams.nth(0);
-  }
-
-  shouldBeTeam() {
+  public shouldBeTeam() {
     return this.chain(async (t) => {
-      await this.team0();
-      const id = (await this.team0.getReact()).key;
+      await t.expect(this.team.exists).ok('Fail to find the team, probably caused by long-time loading');
+      const id = (await this.team.getReact()).key;
       const props = (await this._getTeamProps(id));
       return await t.expect(props.is_team).ok(`Team ${id} is not a team`);
     });
   }
 
-  teamNameShouldChange() {
+  public teamNameShouldBe(name) {
     return this.chain(async (t) => {
-      await this.team0();
-      const id = (await this.team0.getReact()).key;
-      const randomName = Math.random().toString(36).substring(7);
-      await t.expect(this._getTeamName(this.team0)).eql(randomName, { timeout: 10000 });
+      const text = () => this.team.findReact('Typography').innerText;
+      return await t.expect(text()).eql(name, 'wrong name');
     });
-  }
-
-  checkTeamIndex(id: number, i: number) {
-    return this.chain(async (t) => {
-      await this.teams();
-      const component = await this.teams.nth(i).getReact();
-      await t.expect(component.key).eql(id);
-    });
-  }
-
-  private _getTeamName(team: Selector) {
-    return team.findReact('Typography').innerText;
   }
 
   public createTeam() {
@@ -69,8 +52,8 @@ class TeamSection extends BaseComponent {
 
   modifyTeamName(name) {
     return  this.chain(async(t) => {
-      await t.expect(this.team0.exists).ok('Fail to find the team, probably caused by long-time loading');
-      const id = (await this.team0.getReact()).key;
+      await t.expect(this.team.exists).ok('Fail to find the team, probably caused by long-time loading');
+      const id = (await this.team.getReact()).key;
       return await GroupAPI.modifyGroupById(id, { set_abbreviation: name });
     });
   }
