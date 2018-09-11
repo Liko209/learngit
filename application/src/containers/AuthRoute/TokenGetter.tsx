a97c0924627c62807afcb4a9356f3e7c887c0325
Devin Lin <adntin@qq.com>
2018-09-11 18:19:01 +0800

    feat(unified): unified login use view model

--- application/src/containers/AuthRoute/TokenGetter.tsx
+++ application/src/containers/AuthRoute/TokenGetter.tsx
@@ -6,15 +6,12 @@
 
 import React, { Component } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { parse } from 'qs';
 import { TranslationFunction } from 'i18next';
 import { translate } from 'react-i18next';
 
 import ViewModel from './ViewModel';
 import ContentLoader from 'ui-components/organisms/ContentLoader';
 import Alert from 'ui-components/molecules/Dialog/Alert';
-import storeManager from '@/store';
-import { computed, observable, action } from 'mobx';
 import { observer } from 'mobx-react';
 
 interface IProps extends RouteComponentProps<{}> {
@@ -24,58 +21,38 @@ interface IProps extends RouteComponentProps<{}> {
 @observer
 class TokenGetter extends Component<IProps>  {
   private _vm: ViewModel;
-  @observable open: boolean;
 
   constructor(props: IProps) {
     super(props);
     this._vm = new ViewModel();
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     // RC User
     // http://localhost:3000/?state=STATE&code=CODE
     // Free User (Glip create a new user)
     // http://localhost:3000/?state=STATE&id_token=TOKEN
-    try {
-      const { location, history } = this.props;
-      const params = parse(location.search, { ignoreQueryPrefix: true });
-      const { state = '/', code, id_token: token } = params;
-      if (code || token) {
-        await this._vm.unifiedLogin({ code, token });
-      }
-      history.replace(state.replace('$', '&'));
-    } catch (e) {
-      this.setOpen(true);
-    }
+    const { location, history } = this.props;
+    this._vm.unifiedLogin(location, history);
   }
 
   onClose = () => {
     const { location, history } = this.props;
-    const params = parse(location.search, { ignoreQueryPrefix: true });
-    const { state = '/' } = params;
-    history.replace(state.replace('$', '&'));
-  }
-
-  @action.bound setOpen(open: boolean) {
-    this.open = open;
-  }
-
-  @computed get offline() {
-    const globalStore = storeManager.getGlobalStore();
-    return globalStore.get('network') === 'offline';
+    this._vm.onClose(location, history);
   }
 
   render() {
     const { t } = this.props;
+    const { offline, open } = this._vm;
     return (
       <React.Fragment>
         <ContentLoader />
         {
-          this.offline ?
+          offline ?
             <Alert open={true} header={t('signInFailedTitle')} onClose={this.onClose}>
               {t('signInFailedContentNetwork')}
             </Alert> :
-            <Alert open={this.open} header={t('signInFailedTitle')} onClose={this.onClose}>
+            <Alert open={open} header={t('signInFailedTitle')} onClose={this.onClose}>
               {t('signInFailedContent')}
             </Alert>
         }
--- application/src/index.tsx
+++ application/src/index.tsx
@@ -5,11 +5,9 @@ import '@/index.css';
 import registerServiceWorker from '@/registerServiceWorker';
 import '@/i18n';
 import { initAll } from '@/init';
-import notification from './notification';
 
 (async function () {
   await initAll();
-  notification();
 
   ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 
