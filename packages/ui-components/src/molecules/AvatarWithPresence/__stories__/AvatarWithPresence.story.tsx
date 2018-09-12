fa7dc05e3073018fb952165a7dabfd809c81fc4b
Shining <shining.miao@ringcentral.com>
2018-09-12 10:30:07 +0800

    fix(ui): change stories relative path

--- packages/ui-components/src/molecules/AvatarWithPresence/__stories__/AvatarWithPresence.story.tsx
+++ packages/ui-components/src/molecules/AvatarWithPresence/__stories__/AvatarWithPresence.story.tsx
@@ -3,32 +3,31 @@
  * @Date: 2018-08-17 16:27:05
  * Copyright © RingCentral. All rights reserved.
  */
-/// <reference path="../../../.storybook/storybook.d.ts" />
+/// <reference path="../../../../.storybook/storybook.d.ts" />
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs/react';
 
-import AvatarWithPresence from '.';
+import AvatarWithPresence from '..';
 
-import avatar from '../../atoms/Avatar/__stories__/img/avatar.jpg';
-import { withInfoDecorator } from '../../utils/decorators';
+import avatar from '../../../atoms/Avatar/__stories__/img/avatar.jpg';
+import { withInfoDecorator } from '../../../utils/decorators';
 
 const knobs = {
-  presence: () => select(
-    'presence',
-    {
-      online: 'online',
-      offline: 'offline',
-      away: 'away',
-    },
-    'online',
-  ),
+  presence: () =>
+    select(
+      'presence',
+      {
+        online: 'online',
+        offline: 'offline',
+        away: 'away',
+      },
+      'online',
+    ),
 };
 
 storiesOf('Molecules/AvatarWithPresence 🔜', module)
   .addDecorator(withInfoDecorator(AvatarWithPresence, { inline: true }))
   .addWithJSX('AvatarWithPresence', () => {
-    return (
-      <AvatarWithPresence src={avatar} presence={knobs.presence()} />
-    );
+    return <AvatarWithPresence src={avatar} presence={knobs.presence()} />;
   });
--- packages/ui-components/src/organisms/TopBar/__stories__/TopBar.story.tsx
+++ packages/ui-components/src/organisms/TopBar/__stories__/TopBar.story.tsx
@@ -3,19 +3,17 @@
  * @Date: 2018-08-21 10:31:21
  * Copyright © RingCentral. All rights reserved.
  */
-/// <reference path="../../../.storybook/storybook.d.ts" />
+/// <reference path="../../../../.storybook/storybook.d.ts" />
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withInfoDecorator } from '../../utils/decorators';
+import { withInfoDecorator } from '../../../utils/decorators';
 
-import TopBar from '.';
+import TopBar from '..';
 
-import avatar from '../../atoms/Avatar/__stories__/img/avatar.jpg';
+import avatar from '../../../atoms/Avatar/__stories__/img/avatar.jpg';
 
 storiesOf('Organisms/TopBar', module)
   .addDecorator(withInfoDecorator(TopBar, { inline: true }))
   .addWithJSX('TopBar', () => {
-    return (
-      <TopBar avatar={avatar} presence="online" />
-    );
+    return <TopBar avatar={avatar} presence="online" />;
   });
