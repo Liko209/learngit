d84413dcb5ea373e5b09b355ad2534caf5422f64
Alvin Huang <alvin.huang@ringcentral.com>
2018-09-16 19:25:25 +0800

    fix(menulist): fix event

--- packages/ui-components/src/molecules/MenuList/MenuList.tsx
+++ packages/ui-components/src/molecules/MenuList/MenuList.tsx
@@ -66,7 +66,7 @@ class MenuListPanel extends React.Component<TIconMore> {
                   <MenuList>
                     {
                       items!.map((item, index) => {
-                        return (<MenuItem onClick={handleClose.bind(this, index)} key={index}>{item}</MenuItem>);
+                        return (<MenuItem onClick={handleClose.bind(this, event, index)} key={index}>{item}</MenuItem>);
                       })
                     }
                   </MenuList>
