492e87871b083f44ad0554e56caa4b2cc0e6e857
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-09-18 10:19:39 +0800

    fix(test): store base notification subscribable test

--- application/src/store/base/__tests__/BaseNotificationSubscribable.test.ts
+++ application/src/store/base/__tests__/BaseNotificationSubscribable.test.ts
@@ -7,7 +7,7 @@ import BaseNotificationSubscribable from '../BaseNotificationSubscribable';
 import { notificationCenter } from 'sdk/service';
 
 // Using manual mock to improve mock priority.
-jest.mock('sdk', () => jest.genMockFromModule<any>('sdk'));
+// jest.mock('sdk/service', () => jest.genMockFromModule<any>('sdk'));
 
 let baseNotificationSubscribable: BaseNotificationSubscribable;
 
@@ -18,30 +18,33 @@ describe('BaseNotificationSubscribable', () => {
 
   describe('subscribeNotificationOnce()', () => {
     it('notificationCenter once should be called', () => {
+      const spy = jest.spyOn(notificationCenter, 'once');
       const callback = () => {};
       const notificationObservers = baseNotificationSubscribable.getNotificationObservers();
       baseNotificationSubscribable.subscribeNotificationOnce('group', callback);
       expect(notificationObservers['group'].length).toBe(1);
-      expect(notificationCenter.once).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('subscribeNotification()', () => {
     it('notificationCenter on should be called', () => {
+      const spy = jest.spyOn(notificationCenter, 'on');
       const callback = () => {};
       const notificationObservers = baseNotificationSubscribable.getNotificationObservers();
       baseNotificationSubscribable.subscribeNotification('post', callback);
       expect(notificationObservers['post'].length).toBe(1);
-      expect(notificationCenter.on).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('dispose()', () => {
     it('baseNotificationSubscribable notificationObservers size should be 0', () => {
+      const spy = jest.spyOn(notificationCenter, 'removeListener');
       baseNotificationSubscribable.dispose();
       const notificationObservers = baseNotificationSubscribable.getNotificationObservers();
       expect(Object.keys(notificationObservers).length).toBe(0);
-      expect(notificationCenter.removeListener).toHaveBeenCalledTimes(2);
+      expect(spy).toHaveBeenCalledTimes(2);
     });
   });
 });
