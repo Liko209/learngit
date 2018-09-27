30295828a251de0319100b3b24b20afce7bcda06
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-09-27 16:06:47 +0800

    feat(jui): grid

---
+++ packages/jui/src/foundation/Layout/Grid/Grid.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2018-09-27 14:20:23
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import * as React from 'react';
+import MuiGrid, { GridProps as MuiGridProps } from '@material-ui/core/Grid';
+
+export type JuiGridProps = MuiGridProps;
+
+export const JuiGrid: React.SFC<JuiGridProps> & {
+  dependencies?: any[];
+} = props => <MuiGrid {...props} />;
+
+JuiGrid.displayName = 'JuiGrid';
+JuiGrid.dependencies = [JuiGrid];
---
+++ packages/jui/src/foundation/Layout/Grid/__stories__/Grid.story.tsx
@@ -0,0 +1,41 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2018-09-27 14:24:37
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfoDecorator } from '../../../../foundation/utils/decorators';
+import { JuiGrid } from '..';
+import { JuiPaper } from '../../../../components/Paper';
+
+storiesOf('Foundation/Layout', module)
+  .addDecorator(withInfoDecorator(JuiGrid, { inline: true }))
+  .add('Grid', () => (
+    <div>
+      <JuiGrid container={true} spacing={24}>
+        <JuiGrid item={true} xs={12}>
+          <JuiPaper>xs=12</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={6}>
+          <JuiPaper>xs=6</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={6}>
+          <JuiPaper>xs=6</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={3}>
+          <JuiPaper>xs=3</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={3}>
+          <JuiPaper>xs=3</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={3}>
+          <JuiPaper>xs=3</JuiPaper>
+        </JuiGrid>
+        <JuiGrid item={true} xs={3}>
+          <JuiPaper>xs=3</JuiPaper>
+        </JuiGrid>
+      </JuiGrid>
+    </div>
+  ));
