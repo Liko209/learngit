43889957cbc9f237439cf4dc7021f1cb3b01b337
Nello Huang <nello.huang@ringcentral.com>
2018-09-27 17:25:28 +0800

    refactor(ui): dialog

---
+++ packages/jui/src/components/Dialog/__stories__/Alert.story.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean } from '@storybook/addon-knobs';
+import Dialog from '../Dialog';
+import JuiModal from '../Modal';
+import { withInfoDecorator } from '../../../foundation/utils/decorators';
+
+storiesOf('Component/Dialog 🔜', module)
+  .addDecorator(withInfoDecorator(Dialog))
+  .addWithJSX('Alert', () => {
+    const open = boolean('open', true);
+    const onCancel = () => {
+      alert('you clicked cancel');
+    };
+    const onOK = () => {
+      alert('you clicked OK');
+    };
+    return (
+      <JuiModal
+        open={open}
+        header="Alert header"
+        okCancel={true}
+        onCancel={onCancel}
+        onOK={onOK}
+      >
+        We are having trouble signing you in. Please try again later.
+      </JuiModal>
+    );
+  });
---
+++ packages/jui/src/components/Dialog/index.tsx
@@ -0,0 +1,3 @@
+export * from './Modal';
+export * from './Confirm';
+export * from './Alert';
