cea63a1365c0e322eabe58668353e5eedba7d8a4
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-09-28 20:07:56 +0800

    feat(application): header

---
+++ app/constants.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2018-09-28 18:22:53
+ * Copyright © RingCentral. All rights reserved.
+ */
+enum CONVERSATION_TYPES {
+  TEAM,
+  NORMAL_GROUP,
+  ME,
+  SMS,
+  NORMAL_ONE_TO_ONE,
+}
+
+export { CONVERSATION_TYPES };
