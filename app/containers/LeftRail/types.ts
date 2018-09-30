f071367296fef6cd77f81d8b7b8fc994b0057478
Valor Lin <valor.lin@ringcentral.com>
2018-09-30 21:25:41 +0800

    feat(app): remove

--- app/containers/LeftRail/types.ts
+++
@@ -1,9 +0,0 @@
-import { SECTION_TYPE } from './Section/constants';
-
-type LeftRailProps = {};
-
-type LeftRailViewProps = {
-  sections: SECTION_TYPE[];
-};
-
-export { LeftRailProps, LeftRailViewProps };
