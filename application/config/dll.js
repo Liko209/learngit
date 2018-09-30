a4517c23b9294bbbb99b31a78444ac885f57dde3
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-09-30 20:00:01 +0800

    feat(refactoring): jui path

--- application/config/dll.js
+++ application/config/dll.js
@@ -3,10 +3,10 @@
  * @Date: 2018-08-30 11:02:26
  * Copyright © RingCentral. All rights reserved.
  */
-const fs = require('fs');
-const { resolve } = require('path');
-const pullAll = require('lodash/pullAll');
-const uniq = require('lodash/uniq');
+const fs = require("fs");
+const { resolve } = require("path");
+const pullAll = require("lodash/pullAll");
+const uniq = require("lodash/uniq");
 
 /**
  * The DLL Plugin provides a dramatic speed increase to webpack build and hot module reloading
@@ -22,18 +22,18 @@ dllPlugin = {
      * we need to exclude dependencies which are not intended for the browser
      * by listing them here.
      */
-    exclude: ['sanitize.css', 'sdk', 'ui-components'],
+    exclude: ["sanitize.css", "sdk", "ui-components", "jui"],
 
     /**
      * Specify any additional dependencies here. We include core-js and lodash
      * since a lot of our dependencies depend on them and they get picked up by webpack.
      */
-    include: ['core-js', 'babel-polyfill', 'lodash'],
+    include: ["core-js", "babel-polyfill", "lodash"],
 
     // The path where the DLL manifest and bundle will get built
     path: resolve(
       fs.realpathSync(process.cwd()),
-      'node_modules/boilerplate-dlls'
+      "node_modules/boilerplate-dlls"
     )
   },
 
