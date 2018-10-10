f1c73fe70772f1debdbf134a06ace40cce644657
Valor Lin <valor.lin@ringcentral.com>
2018-10-10 10:10:21 +0800

    fix(app): read conversation

--- application/src/containers/ConversationPage/Stream/Stream.ViewModel.ts
+++ application/src/containers/ConversationPage/Stream/Stream.ViewModel.ts
@@ -133,11 +133,6 @@ class StreamViewModel extends StoreViewModel {
       posts: [],
     };
   }
-
-  // private _afterRendered() {
-  //   this.stateService.markAsRead(this.groupId);
-  //   this.stateService.updateLastGroup(this.groupId);
-  // }
 }
 
 export { StreamViewModel };
