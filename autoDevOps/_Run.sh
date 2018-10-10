929da574873a6c14e637947e88de51f21adf77b8
Andy Hu <andy.hu@ringcentral.com>
2018-10-10 16:44:40 +0800

    feat(e2e): remove e2e auto deps

--- autoDevOps/_Run.sh
+++ autoDevOps/_Run.sh
@@ -10,21 +10,21 @@ echo '====Start Run'
 . $autoDevOps/2.SA.sh
 
 if [ "$hasLintError" != 1 ]; then
-  # # # # 3. UT
-  . $autoDevOps/3.UT.sh
-
-  # # # 4. Build And Deploy
-  if [ "$demoHasUpdate" ]; then
-    . $autoDevOps/4.BuildAndDeployDemo.sh
-  else
-    . $autoDevOps/4.BuildAndDeployApplication.sh
-  fi
-
-  # 5. E2E
-  . $autoDevOps/5.E2E.sh
-
-  # # 6. Puppeteer
-  # . $autoDevOps/6.Puppeteer.sh
+    # # # # 3. UT
+    . $autoDevOps/3.UT.sh
+    
+    # # # 4. Build And Deploy
+    if [ "$demoHasUpdate" ]; then
+        . $autoDevOps/4.BuildAndDeployDemo.sh
+    else
+        . $autoDevOps/4.BuildAndDeployApplication.sh
+    fi
+    
+    # 5. E2E
+    # . $autoDevOps/5.E2E.sh
+    
+    # # 6. Puppeteer
+    # . $autoDevOps/6.Puppeteer.sh
 else
-  addEnv hasLintError=1
+    addEnv hasLintError=1
 fi
