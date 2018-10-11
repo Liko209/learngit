75284c2516966d727962558d858bc61ecbcf0467
Devin Lin <devin.lin@ringcentral.com>
2018-10-11 09:42:41 +0800

    fix(layout): browser compatibility

--- application/src/containers/Home/Wrapper.tsx
+++ application/src/containers/Home/Wrapper.tsx
@@ -1,3 +1,9 @@
+/*
+ * @Author: Devin Lin (devin.lin@ringcentral.com)
+ * @Date: 2018-10-11 09:40:29
+ * Copyright © RingCentral. All rights reserved.
+ */
+
 import styled from 'styled-components';
 
 const Wrapper: any = styled.div`
@@ -5,6 +11,7 @@ const Wrapper: any = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
 `;
 
 export default Wrapper;
