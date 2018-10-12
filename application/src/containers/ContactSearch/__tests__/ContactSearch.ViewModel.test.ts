e17a853a440552294675f8b73c97a41d6d03250c
Shining <shining.miao@ringcentral.com>
2018-10-12 15:31:39 +0800

    test(app): add contactsearchvm test

--- application/src/containers/ContactSearch/__tests__/ContactSearch.ViewModel.test.ts
+++ application/src/containers/ContactSearch/__tests__/ContactSearch.ViewModel.test.ts
@@ -3,3 +3,8 @@
  * @Date: 2018-10-12 15:19:44
  * Copyright © RingCentral. All rights reserved.
  */
+// import { ContactSearchViewModel } from '../ContactSearch.ViewModel';
+
+describe('ContactSearchVM', () => {
+  it('fetchSearch', () => {});
+});
