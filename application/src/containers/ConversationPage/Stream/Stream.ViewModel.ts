0b5fabdbe7c967c2706fce3a6c5636dac4a9bd1c
Dennis Jiang <dennis.jiang@ringcentral.com>
2018-10-12 16:53:52 +0800

    fix(umi): multi set state api issue

--- application/src/containers/ConversationPage/Stream/Stream.ViewModel.ts
+++ application/src/containers/ConversationPage/Stream/Stream.ViewModel.ts
@@ -49,6 +49,11 @@ type TTransformedElement = {
   meta?: any;
 };
 class PostTransformHandler extends TransformHandler<TTransformedElement, Post> {
+  onAppended: Function;
+  constructor(handler: FetchSortableDataListHandler, onAppended: Function) {
+    super(handler);
+    this.onAppended = onAppended;
+  }
   onAdded(direction: FetchDataDirection, addedItems: ISortableModel[]) {
     const updated = _(addedItems)
       .map(item => ({
@@ -59,6 +64,9 @@ class PostTransformHandler extends TransformHandler<TTransformedElement, Post> {
       .reverse()
       .value();
     const inFront = FetchDataDirection.UP === direction;
+    if (!inFront) {
+      this.onAppended();
+    }
     this.listStore.append(updated, inFront); // new to old
   }
 
@@ -85,10 +93,9 @@ class StreamViewModel extends StoreViewModel {
   }
 
   onReceiveProps(props: StreamProps) {
-    this.markAsRead();
     if (this.groupId === props.groupId) return;
-
     this.groupId = props.groupId;
+    this.markAsRead();
     const postDataProvider: IFetchSortableDataProvider<Post> = {
       fetchData: async (offset: number, direction, pageSize, anchor) => {
         try {
@@ -124,11 +131,17 @@ class StreamViewModel extends StoreViewModel {
       },
     );
 
-    this._transformHandler = new PostTransformHandler(orderListHandler);
+    this._transformHandler = new PostTransformHandler(
+      orderListHandler,
+      this.markAsRead.bind(this),
+    );
     this.autorun(() => {
-      this.postIds = _(this._transformHandler.listStore.items)
+      const postIds = _(this._transformHandler.listStore.items)
         .map('value')
         .value() as number[];
+      if (!_.isEqual([...this.postIds], postIds)) {
+        this.postIds = postIds;
+      }
     });
     this.loadInitialPosts();
   }
