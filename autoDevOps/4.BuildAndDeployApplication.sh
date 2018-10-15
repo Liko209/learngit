4b1f7186e9ef52df5cc6619123e4356cab2781d3
Andy Hu <andy.hu@ringcentral.com>
2018-10-15 18:23:25 +0800

    feat(CICD): raised coverage test threshold

--- autoDevOps/4.BuildAndDeployApplication.sh
+++ autoDevOps/4.BuildAndDeployApplication.sh
@@ -12,18 +12,6 @@ if [ $? -eq 0 ]; then
     else
         echo "Not need to update linked Domain"
     fi
-    if [ "$gitlabActionType" == 'MERGE' ]; then
-        git stash
-        git checkout $gitlabSourceBranch
-        git reset --hard origin/$gitlabSourceBranch
-        cd $project
-        git stash pop
-        npm run raise-coverage
-        if [ "$(git diff ${project}/config)" ]; then
-            git add $project/config
-            git commit -m 'feat(CICD): raised coverage test threshold' && git push
-        fi
-    fi
 else
     echo "Not need to update linked Domain"
 fi
