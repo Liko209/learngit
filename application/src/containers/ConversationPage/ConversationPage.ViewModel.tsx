f62cc733881e8aae35e0050a31879db552a6aac4
Devin Lin <devin.lin@ringcentral.com>
2018-10-15 10:52:58 +0800

    fix(send): send message input show permissions

--- application/src/containers/ConversationPage/ConversationPage.ViewModel.tsx
+++ application/src/containers/ConversationPage/ConversationPage.ViewModel.tsx
@@ -23,6 +23,9 @@ class ConversationPageViewModel extends AbstractViewModel {
 
   @computed
   get canPost() {
+    if (this._permissions.length === 0) {
+      return true;
+    }
     return this._permissions.includes(PERMISSION_ENUM.TEAM_POST);
   }
 
