24300156d82d1d4db4b4179c34e348726bbfb410
Devin Lin <devin.lin@ringcentral.com>
2018-10-16 14:58:09 +0800

    fix(send): send message input box shadow

--- packages/jui/src/pattern/MessageInput/MessageInput.tsx
+++ packages/jui/src/pattern/MessageInput/MessageInput.tsx
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
   .quill {
     width: 100%;
     align-self: flex-end;
-    box-shadow: 0px -1px 5px rgba(0,0,0,0.2);
+    box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.14);
     z-index: ${({ theme }) => `${theme.zIndex.mobileStepper}`};
   }
   .ql-snow {
