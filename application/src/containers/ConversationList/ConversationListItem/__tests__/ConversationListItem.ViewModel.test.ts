586fd87105bf6ae9f88cf0014c30f22db3573902
Chris Zhan <chris.zhan@ringcentral.com>
2018-10-16 13:08:58 +0800

    feat(unit test): unit test

---
+++ application/src/containers/ConversationList/ConversationListItem/__tests__/ConversationListItem.ViewModel.test.ts
@@ -0,0 +1,28 @@
+/*
+ * @Author: Chris Zhan (chris.zhan@ringcentral.com)
+ * @Date: 2018-10-16 10:45:21
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { ConversationListItemViewModel } from '../ConversationListItem.ViewModel';
+import storeManager from '@/store/base/StoreManager';
+
+const model = new ConversationListItemViewModel();
+describe('ConversationListItemViewModel', () => {
+  it('should not be selected by default', () => {
+    model.props.groupId = 1;
+    expect(model.selected).toBe(false);
+  });
+
+  it('should be selected if this conversation item id is current opened group id', () => {
+    model.props.groupId = 1;
+    storeManager.getGlobalStore = jest.fn().mockReturnValue({
+      get: jest.fn((key: string) => {
+        if (key === 'currentConversationId') {
+          return 1;
+        }
+        return null;
+      }),
+    });
+    expect(model.selected).toBe(true);
+  });
+});
