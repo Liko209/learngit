e78a958a58d41df30dc5945b086611a3c1245352
Henry Xu <henry.xu@ringcentral.com>
2018-10-19 09:58:47 +0800

    refactor(e2e): typo, getter, etc.

--- tests/e2e/testcafe/v2/sdk/glip.ts
+++ tests/e2e/testcafe/v2/sdk/glip.ts
@@ -6,7 +6,6 @@ import { RcPlatformSdk } from "./platform";
 export class GlipSdk {
   axiosClient: AxiosInstance;
   accessToken: string;
-  headers: object;
 
   constructor(private glipServerUrl: string, private platform: RcPlatformSdk) {
     this.axiosClient = axios.create({
@@ -30,14 +29,15 @@ export class GlipSdk {
     });
   }
 
-  async auth(forMobile: boolean = true) {
-    const res = await this.authByRcToken(forMobile)
-    this.accessToken = res["headers"]["x-authorization"]
-    this.headers = {
-      accept: 'application/json',
-      authorization: "Bearer " + this.accessToken,
-      'content-type': 'application/json'
+  get headers() {
+    return {
+      Authorization: "Bearer " + this.accessToken
     }
+  }
+
+  async auth(forMobile: boolean = false) {
+    const res = await this.authByRcToken(forMobile)
+    this.accessToken = res.headers["x-authorization"]
   }
 
   getPerson(personId) {
@@ -48,59 +48,58 @@ export class GlipSdk {
   }
 
   updatePerson(personId, personDict: object) {
-    const uri = `api/person/${personId}`
+    const uri = `api/person/${personId}`;
     return this.axiosClient.put(uri, personDict, {
       headers: this.headers,
     });
   }
 
-  //need system user auth token
   deactivated(personId) {
     this.updatePerson(personId, { "deactivated": true });
   }
 
   getTeams() {
-    const uri = `api/teams`
+    const uri = `api/teams`;
     return this.axiosClient.get(uri, {
       headers: this.headers,
     });
   }
 
   createTeam(name, memebersByGlipId: number[]) {
-    const uri = `api/team`
+    const uri = `api/team`;
     const data = {
       "is_team": true,
       "is_public": false,
       "members": memebersByGlipId,
       "set_abbreviation": name
-    }
+    };
     return this.axiosClient.post(uri, data, {
       headers: this.headers,
     });
   }
 
   sendPost(groupId: number, text: string) {
-    const uri = `api/post`
+    const uri = `api/post`;
     const data = {
       "group_id": groupId,
       "is_new": true,
       "is_sms": true,
       "text": text
-    }
+    };
     return this.axiosClient.post(uri, data, {
       headers: this.headers,
     });
   }
 
   getProfile(profileId) {
-    const uri = `api/profile/${profileId}`
+    const uri = `api/profile/${profileId}`;
     return this.axiosClient.get(uri, {
       headers: this.headers
     });
   }
 
   updateProfile(profileId, data: object) {
-    const uri = `api/profile/${profileId}`
+    const uri = `api/profile/${profileId}`;
     return this.axiosClient.put(uri, data, {
       headers: this.headers
     });
