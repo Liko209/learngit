2364d4ae30b36dc8f5e6695886c03b88a5b857a1
Lip Wang <lip.wang@ringcentral.com>
2018-10-22 15:17:50 +0800

    fix: add ts-node

--- autoDevOps/4.BuildAndDeployApplication.sh
+++ autoDevOps/4.BuildAndDeployApplication.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 echo '====Init build version info'
 cd $project/application/src/containers/VersionInfo/
-ts-node GitRepo.ts
+npx ts-node GitRepo.ts
 
 echo '====Start Build application'
 cd $project/application
