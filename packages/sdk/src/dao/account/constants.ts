3e868ef0fa477a7a5fab9c14ba63e50d38a06a32
Andy Hu <andy.hu@ringcentral.com>
2018-10-24 21:48:28 +0800

    feat(init): init

--- packages/sdk/src/dao/account/constants.ts
+++ packages/sdk/src/dao/account/constants.ts
@@ -9,7 +9,7 @@ export const ACCOUNT_PROFILE_ID = 'profile_id';
 export const ACCOUNT_COMPANY_ID = 'company_id';
 export const ACCOUNT_CLIENT_CONFIG = 'client_config';
 export const ACCOUNT_CONVERSATION_LIST_LIMITS = 'max_conversations';
-
+export const UNREAD_TOGGLE_ON = 'unread_toggle_on';
 export const ACCOUNT_KEYS = [
   ACCOUNT_USER_ID,
   ACCOUNT_PROFILE_ID,
