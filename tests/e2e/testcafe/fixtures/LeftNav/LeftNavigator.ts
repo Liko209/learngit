280cbc4f6de0287267b6a0b141bea5f20d9b474c
Henry Xu <henry.xu@ringcentral.com>
2018-10-24 16:23:47 +0800

    refactor(e2e): LeftNavigator.ts

--- tests/e2e/testcafe/fixtures/LeftNav/LeftNavigator.ts
+++ tests/e2e/testcafe/fixtures/LeftNav/LeftNavigator.ts
@@ -3,22 +3,23 @@
  * @Date: 2018-08-22 17:13:54
  * Copyright © RingCentral. All rights reserved.
  */
+import * as assert from 'assert';
+import * as _ from 'lodash';
 import { formalName } from '../../libs/filter';
 import { h } from '../../v2/helpers';
 import { setupCase, teardownCase } from '../../init';
 import { AppRoot } from '../../v2/page-models/AppRoot';
 import { SITE_URL } from '../../config';
+import { URL } from 'url';
 
-fixture('LeftNav')
+fixture('Left Navigator')
   .beforeEach(setupCase('GlipBetaUser(1210,4488)'))
   .afterEach(teardownCase());
 
-test(formalName('Left nav redirect', ['P0', 'LeftNav']), async (t) => {
-  // every test case should start with following lines
+test(formalName('Enter every entries in left navigator', ['P0', 'LeftNav']), async (t) => {
   const user = h(t).rcData.mainCompany.users[4];
   const app = new AppRoot(t);
 
-  // using Given-When-Then statements to write report
   await h(t).withLog(`Given I login Jupiter with ${user.company.number}#${user.extension}`, async () => {
     await h(t).directLoginWithUser(SITE_URL, user);
     await app.homePage.ensureLoaded();
@@ -39,9 +40,13 @@ test(formalName('Left nav redirect', ['P0', 'LeftNav']), async (t) => {
   ];
 
   for (const entry of leftPanelEntries) {
-    await h(t).withLog(`When I click ${entry.name}`, async () => {
+    await h(t).withLog(`When I click "${entry.name}" entry`, async () => {
       await entry.enter();
     });
-    await h(t).log(`Then I enter ${entry.name}`, true);
+    await h(t).log(`Then I should enter ${entry.name} panel`, true);
+    await h(t).withLog(`And the url should be "/${entry.name}"`, async () => {
+      const url = new URL(await h(t).href);
+      assert.equal(_.trim(url.pathname, '/'), entry.name);
+    });
   }
 });
