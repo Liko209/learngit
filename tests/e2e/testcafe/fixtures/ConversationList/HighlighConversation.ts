30eae7414f6ed209198626d21169119d61b0053b
Chris Zhan <chris.zhan@ringcentral.com>
2018-10-24 16:12:39 +0800

    test(e2e): highlight & teamsection

---
+++ tests/e2e/testcafe/fixtures/ConversationList/HighlighConversation.ts
@@ -0,0 +1,103 @@
+/*
+ * @Author: Chris Zhan (chris.zhan@ringcentral.com)
+ * @Date: 2018-10-24 14:25:44
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { formalName } from '../../libs/filter';
+import { h } from '../../v2/helpers';
+import { setupCase, teardownCase } from '../../init';
+import { AppRoot } from '../../v2/page-models/AppRoot';
+import { SITE_URL } from '../../config';
+import { GlipSdk } from '../../v2/sdk/glip';
+import { ClientFunction } from 'testcafe';
+
+fixture('HighlightConversation')
+  .beforeEach(setupCase('GlipBetaUser(1210,4488)'))
+  .afterEach(teardownCase());
+
+test(
+  formalName('Open last conversation when login', [
+    'JPT-144',
+    'P2',
+    'ConversationList',
+  ]),
+  async (t: TestController) => {
+    const app = new AppRoot(t);
+    const users = h(t).rcData.mainCompany.users;
+    const user = users[7];
+    const userPlatform = await h(t).sdkHelper.sdkManager.getPlatform(user);
+    const glipSDK: GlipSdk = await h(t).sdkHelper.sdkManager.getGlip(user);
+    const directMessageSection =
+      app.homePage.messagePanel.directMessagesSection;
+
+    let group;
+    await h(t).withLog(
+      'Given I have an extension with a group chat',
+      async () => {
+        group = await userPlatform.createGroup({
+          type: 'Group',
+          members: [user.rcId, users[5].rcId, users[6].rcId],
+        });
+      },
+    );
+
+    await h(t).withLog(
+      'And the conversation should not be hidden',
+      async () => {
+        await glipSDK.updateProfileByGlipId(user.glipId, {
+          [`hide_group_${group.data.id}`]: false,
+        });
+      },
+    );
+
+    await h(t).withLog(
+      'Given the group chat is last group selected',
+      async () => {
+        await glipSDK.updateStateByGlipId(user.glipId, {
+          last_group_id: group.data.id,
+        });
+      },
+    );
+
+    await h(t).withLog(
+      `When I login Jupiter with this extension: ${user.company.number}#${
+        user.extension
+      }`,
+      async () => {
+        await h(t).directLoginWithUser(SITE_URL, user);
+        await app.homePage.ensureLoaded();
+      },
+    );
+
+    await h(t).withLog(
+      'Then the group should be opened automatically after login',
+      async () => {
+        const getLocation = ClientFunction(() => window.location.href);
+        const url = await getLocation();
+        const str = url.toString().split('messages/');
+        await t.expect(str[1]).eql(group.data.id);
+      },
+    );
+
+    await h(t).withLog('And the group is highlighted', async () => {
+      const textStyle = await directMessageSection.conversations
+        .filter(`[data-group-id="${group.data.id}"]`)
+        .find('p').style;
+      await t.expect(textStyle.color).eql('rgb(6, 132, 189)');
+    });
+
+    await h(t).withLog(
+      'And the content is shown on the conversation page',
+      async () => {
+        await t
+          .expect(
+            app.homePage.messagePanel.conversationPage.withAttribute(
+              'data-group-id',
+              group.data.id,
+            ).exists,
+          )
+          .ok();
+      },
+    );
+  },
+);
--- tests/e2e/testcafe/v2/sdk/glip.ts
+++ tests/e2e/testcafe/v2/sdk/glip.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosInstance, } from "axios";
+import axios, { AxiosInstance } from 'axios';
 import * as querystring from 'querystring';
 
-import { RcPlatformSdk } from "./platform";
+import { RcPlatformSdk } from './platform';
 
 export class GlipSdk {
   axiosClient: AxiosInstance;
@@ -10,38 +10,40 @@ export class GlipSdk {
   constructor(private glipServerUrl: string, private platform: RcPlatformSdk) {
     this.axiosClient = axios.create({
       baseURL: this.glipServerUrl,
-    })
+    });
   }
 
   async authByRcToken(forMobile: boolean = true) {
-    const encodedToken = Buffer.from(JSON.stringify(this.platform.token)).toString('base64');
+    const encodedToken = Buffer.from(
+      JSON.stringify(this.platform.token),
+    ).toString('base64');
     const url = 'api/login';
     const data = {
       for_mobile: forMobile,
       remember_me: false,
       rc_access_token_data: encodedToken,
-    }
+    };
     return await this.axiosClient.put(url, querystring.stringify(data), {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'X-RC-Access-Token-Data': encodedToken,
-      }
+      },
     });
   }
 
   get headers() {
     return {
-      Authorization: "Bearer " + this.accessToken
-    }
+      Authorization: 'Bearer ' + this.accessToken,
+    };
   }
 
   async auth(forMobile: boolean = false) {
-    const res = await this.authByRcToken(forMobile)
-    this.accessToken = res.headers["x-authorization"]
+    const res = await this.authByRcToken(forMobile);
+    this.accessToken = res.headers['x-authorization'];
   }
 
   getPerson(personId: string) {
-    const uri = `api/person/${personId}`
+    const uri = `api/person/${personId}`;
     return this.axiosClient.get(uri, {
       headers: this.headers,
     });
@@ -55,58 +57,58 @@ export class GlipSdk {
   }
 
   deactivated(personId: string) {
-    this.updatePerson(personId, { "deactivated": true });
+    this.updatePerson(personId, { deactivated: true });
   }
 
   getTeams() {
-    const uri = `api/teams`;
+    const uri = 'api/teams';
     return this.axiosClient.get(uri, {
       headers: this.headers,
     });
   }
 
   createTeam(name: string, memebersByGlipId: number[]) {
-    const uri = `api/team`;
+    const uri = 'api/team';
     const data = {
-      "is_team": true,
-      "is_public": false,
-      "members": memebersByGlipId,
-      "set_abbreviation": name
+      is_team: true,
+      is_public: false,
+      members: memebersByGlipId,
+      set_abbreviation: name,
     };
     return this.axiosClient.post(uri, data, {
       headers: this.headers,
     });
   }
 
-  getTeam(groupId){
-    const uri = `/api/group/${groupId}`
+  getTeam(groupId) {
+    const uri = `/api/group/${groupId}`;
     return this.axiosClient.get(uri, {
       headers: this.headers,
     });
   }
 
-  updateGroup(groupId, data: object){
-    const uri = `/api/group/${groupId}`
+  updateGroup(groupId, data: object) {
+    const uri = `/api/group/${groupId}`;
     return this.axiosClient.put(uri, data, {
       headers: this.headers,
     });
   }
 
-  modifyGroupName(groupId, name){
-    const uri = `/api/group/${groupId}`
-    const data = {"set_abbreviation": name}
+  modifyGroupName(groupId, name) {
+    const uri = `/api/group/${groupId}`;
+    const data = { set_abbreviation: name };
     return this.axiosClient.put(uri, data, {
       headers: this.headers,
     });
   }
 
   sendPost(groupId: number, text: string) {
-    const uri = `api/post`;
+    const uri = 'api/post';
     const data = {
-      "group_id": groupId,
-      "is_new": true,
-      "is_sms": true,
-      "text": text
+      group_id: groupId,
+      is_new: true,
+      is_sms: true,
+      text,
     };
     return this.axiosClient.post(uri, data, {
       headers: this.headers,
@@ -116,7 +118,7 @@ export class GlipSdk {
   getProfile(profileId: string) {
     const uri = `api/profile/${profileId}`;
     return this.axiosClient.get(uri, {
-      headers: this.headers
+      headers: this.headers,
     });
   }
 
@@ -128,23 +130,38 @@ export class GlipSdk {
   updateProfile(profileId: string, data: object) {
     const uri = `api/profile/${profileId}`;
     return this.axiosClient.put(uri, data, {
-      headers: this.headers
+      headers: this.headers,
     });
   }
 
-  async updateProfileByGlipId(glipId, data: object){
+  updateState(stateId: string, data: object) {
+    const uri = `api/state/${stateId}`;
+    return this.axiosClient.put(uri, data, {
+      headers: this.headers,
+    });
+  }
+
+  async updateProfileByGlipId(glipId, data: object) {
     const profileId = await this.getProfileIdByPersonId(glipId);
-    return await this.updateProfile(profileId, data)
+    return await this.updateProfile(profileId, data);
   }
 
   async getProfileIdByPersonId(personId: string) {
-    return (await this.getPerson(personId)).data.profile_id
+    return (await this.getPerson(personId)).data.profile_id;
+  }
+
+  async getStateIdByPersonId(personId: string) {
+    return (await this.getPerson(personId)).data.state_id;
+  }
+
+  async updateStateByGlipId(glipId, data: object) {
+    const stateId = await this.getStateIdByPersonId(glipId);
+    return await this.updateState(stateId, data);
   }
 
   async skipCloseConversationConfirmation(personId: string, value: boolean) {
     const profileId = await this.getProfileIdByPersonId(personId);
-    const data: object = { "skip_close_conversation_confirmation": value };
-    return await this.updateProfile(profileId, data)
+    const data: object = { skip_close_conversation_confirmation: value };
+    return await this.updateProfile(profileId, data);
   }
-
 }
