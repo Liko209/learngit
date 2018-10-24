0ba1218b32685feece54f33584b72041be965ce1
Henry Xu <henry.xu@ringcentral.com>
2018-10-25 00:06:21 +0800

    fix(e2e): MessagePanel

--- tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessagePanel/index.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessagePanel/index.ts
@@ -181,9 +181,4 @@ export class MessagePanel extends BaseWebComponent {
   get conversationPage() {
     return this.getSelector('.conversation-page');
   }
-
-  get conversationList(){
-    return this.getComponent(ConversationListSection)
-  }
-
 }
