6a3073c4ed07a4c71b035885dad516d47df44e57
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2018-10-25 10:09:43 +0800

    feat(post items): add file header

--- application/src/containers/PostItems/idToPostItemComponent.ts
+++ application/src/containers/PostItems/idToPostItemComponent.ts
@@ -1,3 +1,8 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2018-10-25 10:09:11
+ * Copyright © RingCentral. All rights reserved.
+ */
 import { GlipTypeUtil } from 'sdk/utils';
 import { POST_ITEM_TYPES } from '@/constants';
 import { Event } from './event';
