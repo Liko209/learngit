a2f8b6f2f56eb53e7dfd367cf0fe8bc0700e5526
Henry Xu <henry.xu@ringcentral.com>
2018-10-26 01:06:43 +0800

    fix(e2e): ConversationStream.ts

--- tests/e2e/testcafe/fixtures/ConversationStream/ConversationStream.ts
+++ tests/e2e/testcafe/fixtures/ConversationStream/ConversationStream.ts
@@ -1,3 +1,6 @@
+import * as _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+
 import { formalName } from '../../libs/filter';
 import { setupCase, teardownCase } from '../../init';
 import { h } from '../../v2/helpers';
@@ -18,14 +21,14 @@ test(
     const app = new AppRoot(t);
     const users = h(t).rcData.mainCompany.users;
     const user = users[7];
-    const msgList = ['First post','Second post','Third post']
+    const posts = _.range(3).map(i => `${i} ${uuid()}`);
     let teamId;
     const userPlatform = await h(t).sdkHelper.sdkManager.getPlatform(user);
 
     await h(t).withLog('Given I have an extension with 1 team chat', async () => {
       teamId = (await userPlatform.createGroup({
         isPublic: true,
-        name: `My Team ${Math.random().toString(10)} `,
+        name: uuid(),
         type: 'Team',
         members: [user.rcId, users[5].rcId, users[6].rcId],
       })).data.id;
@@ -39,45 +42,51 @@ test(
     await h(t).withLog('Then I enter the conversation', async () => {
       const teamsSection = app.homePage.messagePanel.teamsSection;
       await teamsSection.expand();
-      const teamConversation =  teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
+      const teamConversation = teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
       await t.click(teamConversation)
     });
 
     await h(t).withLog('When I send 3 posts in order via API', async () => {
-      await msgList.forEach(async (msg) => {
-        await userPlatform.createPost({ text: msg }, teamId);
-        await t.wait(2e3)
-      })
+      for (const post of posts) {
+        await userPlatform.createPost({ text: post }, teamId);
+        await t.wait(1e3)
+      }
     });
 
-    await h(t).withLog('Then the conversation must display the 3 posts in order.', async () => {
+    const postsSelector = await app.homePage.messagePanel.conversationSection.posts;
+    await h(t).withLog('Then I will receive those posts', async () => {
+      await t.expect(postsSelector.withText(new RegExp(posts.join('|'))).count).eql(3, { timeout: 5e3 });
+    });
+
+    await h(t).withLog('And the conversation must display the 3 posts in order.', async () => {
       await t.wait(3e3);
       const posts = await app.homePage.messagePanel.conversationSection.posts;
       const length = await posts.count
-      for (let i = 0; i < length; i = i + 1) {
+
+      for (let i = 0; i < length; i++) {
         await t.expect(posts.nth(i).child('div').nth(1)
-        .child('div').nth(1).textContent)
-        .eql(msgList[i]);
+          .child('div').nth(1).textContent)
+          .eql(posts[i]);
       }
     });
   }
 );
 
 test(
-  formalName('No post in conversation when the conversation', 
-  ['P2','JPT-53','ConversationStream',]
-),
+  formalName('No post in conversation when the conversation',
+    ['P2', 'JPT-53', 'ConversationStream',]
+  ),
   async (t: TestController) => {
     const app = new AppRoot(t);
     const users = h(t).rcData.mainCompany.users;
     const user = users[4];
     let teamId;
     const userPlatform = await h(t).getPlatform(user);
-    
+
     await h(t).withLog('Given I have an extension with 1 team chat', async () => {
       teamId = (await userPlatform.createGroup({
         isPublic: true,
-        name: `My Team ${Math.random().toString(10)} `,
+        name: uuid(),
         type: 'Team',
         members: [user.rcId, users[5].rcId, users[6].rcId],
       })).data.id;
@@ -91,14 +100,14 @@ test(
     await h(t).withLog('Then I can enter the conversation', async () => {
       const teamsSection = app.homePage.messagePanel.teamsSection;
       await teamsSection.expand();
-      const teamConversation =  teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
+      const teamConversation = teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
       await t.click(teamConversation);
     });
 
-    await h(t).withLog('And I can not find  any post in the new created conversation', async () => {
+    await h(t).withLog('And I can not find any post in the new created conversation', async () => {
       await t.wait(2e3);
-      const posts = await app.homePage.messagePanel.conversationSection.posts;
-      await t.expect(posts.exists).notOk("no post");
+      const postsSelector = await app.homePage.messagePanel.conversationSection.posts;
+      await t.expect(postsSelector.exists).notOk();
     });
   }
 );
@@ -112,24 +121,23 @@ test(
     const app = new AppRoot(t);
     const users = h(t).rcData.mainCompany.users;
     const user = users[4];
-    let teamId;
-    const lastPost = "last post";
-    const newPost = "new post";
-
-
     const userPlatform = await h(t).getPlatform(user);
-    
+
+    const lastPost = `last post ${uuid()}`;
+    const newPost = `new post ${uuid()}`;
+
+    let teamId;
     await h(t).withLog('Given I have an extension with 1 team chat', async () => {
       teamId = (await userPlatform.createGroup({
         isPublic: true,
-        name: `My Team ${Math.random().toString(10)}`,
+        name: uuid(),
         type: 'Team',
         members: [user.rcId, users[5].rcId, users[6].rcId],
       })).data.id;
-    }); 
+    });
 
     await h(t).withLog(`Then I send a post "${lastPost}" to the team before login`, async () => {
-        await userPlatform.createPost({ text: lastPost }, teamId);
+      await userPlatform.createPost({ text: lastPost }, teamId);
     });
 
     await h(t).withLog(`When I login Jupiter with this extension: ${user.company.number}#${user.extension}`, async () => {
@@ -140,12 +148,11 @@ test(
     await h(t).withLog('Then I can enter the team conversation', async () => {
       const teamsSection = app.homePage.messagePanel.teamsSection;
       await teamsSection.expand();
-      const teamConversation =  teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
+      const teamConversation = teamsSection.conversations.filter(`[data-group-id="${teamId}"]`);
       await t.click(teamConversation);
     });
 
     await h(t).withLog(`And I can find post "${lastPost}"  in the conversation posts history`, async () => {
-      await t.wait(2e3);
       const posts = await app.homePage.messagePanel.conversationSection.posts;
       await t.expect(posts.nth(-1).textContent).contains(lastPost);
     })
@@ -154,7 +161,7 @@ test(
       await userPlatform.createPost({ text: newPost }, teamId);
     });
 
-    await h(t).withLog(`Then I can check the newest post is "${newPost}"`, async () => {
+    await h(t).withLog(`Then I can check the latest post is "${newPost}"`, async () => {
       await t.wait(1e3);
       const posts = await app.homePage.messagePanel.conversationSection.posts;
       await t.expect(posts.nth(-1).textContent).contains(newPost);
