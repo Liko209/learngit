51a7405689448a310b8ca6106912d8d696d21706
mia.cai <mia.cai@ringcentral.com>
2018-10-30 13:59:45 +0800

    test(e2e): logout

---
+++ tests/e2e/testcafe/fixtures/Logout.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Mia Cai (mia.cai@ringcentral.com)
+ */
+
+import { formalName } from '../libs/filter';
+import { h } from '../v2/helpers'
+import { setupCase, teardownCase } from '../init';
+import { AppRoot } from "../v2/page-models/AppRoot";
+import { SITE_URL } from '../config';
+
+fixture('Logout')
+    .beforeEach(setupCase('GlipBetaUser(1210,4488)'))
+    .afterEach(teardownCase());
+
+test(formalName('Logout', ['JPT-', 'P0', 'Logout']), async (t) => {
+    const user = h(t).rcData.mainCompany.users[4];
+    const app = new AppRoot(t);
+
+    await h(t).withLog(`Given I login Jupiter with ${user.company.number}#${user.extension}`, async () => {
+        await h(t).directLoginWithUser(SITE_URL, user);
+        await app.homePage.ensureLoaded();
+    });
+    await h(t).withLog('When I Click top bar avatar', async () => {
+        await app.homePage.settingMenu.clickTopBarAvatar();
+    })
+    await h(t).withLog('Then show menu should appear', async () => {
+        await app.homePage.settingMenu.menuExists();
+    })
+    await h(t).withLog('When Click logout button', async () => {
+        await app.homePage.settingMenu.clickLogout();
+
+    })
+    await h(t).withLog('Then logout successfully (url contains unified-login)', async () => {
+        await t.expect(h(t).href).contains('unified-login');
+    }, true)
+});
--- tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/SettingMenu.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/SettingMenu.ts
@@ -1,5 +1,25 @@
 import { BaseWebComponent } from "../../BaseWebComponent";
-
 export class SettingMenu extends BaseWebComponent {
+    get root() {
+        return this.getSelector('#root');
+    }
+    get menu() {
+        return this.getSelectorByAutomationId('avatarMenu');
+    }
+    get logoutButton() {
+        return this.getSelectorByAutomationId('signOut');
+    }
+    get topBarAvatar() {
+        return this.getSelectorByAutomationId('topBarAvatar');
+    }
+    async menuExists() {
+        await this.t.expect(this.menu.exists).ok();
+    }
+    async clickTopBarAvatar() {
+        await this.t.click(this.topBarAvatar);
+    }
+    async clickLogout() {
+        await this.t.click(this.logoutButton);
+    }
 
 }
