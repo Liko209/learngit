a964f2b527be8d88cc59547b0b557137086a62a3
Devin Lin <devin.lin@ringcentral.com>
2018-11-07 18:44:39 +0800

    feat(emoji): emoji support from glipdown convert

---
+++ application/src/containers/ConversationSheet/TextMessage/Emoji/convertKeys.ts
@@ -0,0 +1,54 @@
+/*
+ * @Author: Devin Lin (devin.lin@ringcentral.com)
+ * @Date: 2018-11-07 10:27:20
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import { mapEscape, mapUnescape, mapSpecial, mapAscii, mapUnicode } from './map';
+
+type MapData = {
+  [index: string]: string;
+};
+
+const regExpEscape = new RegExp(Object.keys(mapEscape).join('|'), 'g');
+
+const regExpUnescape = new RegExp(Object.keys(mapUnescape).join('|'), 'g');
+
+const regExpSpecial = new RegExp(Object.keys(mapSpecial).map(key => mapSpecial[key]).join('|'), 'g');
+
+const convertKeys = (mapData: MapData): MapData => {
+  const map = {};
+  for (const key in mapData) {
+    let _key = key;
+    // It is required after the Glipdown transformation, For example, < ' ...
+    _key = _key.replace(regExpEscape, (match: string) => mapEscape[match]);
+    // Regular expressions need it, For example, / \ $ ( ) [ ] ^ ...
+    _key = _key.replace(regExpSpecial, (match: string) => mapSpecial[match]);
+    map[_key] = mapData[key];
+  }
+  return map;
+};
+
+const convertMapAscii = convertKeys(mapAscii);
+
+const convertMapUnicode = convertKeys(mapUnicode);
+
+// Regular expression for colon (EmojiOne, Custom)
+const regExpColon = /:([^:]\S*?)(?=:)/g; // /(?<=:)(\S+?)(?=:)/g; /(?<=:)([^:]\S*?)(?=:)/g;
+
+// Ascii keys regular expression, an exact match
+const regExpAscii = new RegExp(`^${Object.keys(convertMapAscii).join('$|^')}$`, 'g');
+
+// Unicode keys regular expression
+const regExpUnicode = new RegExp(`${Object.keys(convertMapUnicode).join('|')}`, 'g');
+
+export {
+  regExpEscape,
+  regExpUnescape,
+  regExpSpecial,
+  convertMapAscii,
+  convertMapUnicode,
+  regExpColon,
+  regExpAscii,
+  regExpUnicode,
+};
