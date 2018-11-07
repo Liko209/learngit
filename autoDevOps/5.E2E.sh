55d7f54f7f7ebe85a78b1d58da8d235284c3e254
Henry Xu <henry.xu@ringcentral.com>
2018-11-07 16:04:05 +0800

    chore(e2e): enable e2e tests on merge

--- autoDevOps/5.E2E.sh
+++ autoDevOps/5.E2E.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-echo '====Start E2E'
+echo '====Start E2E===='
 
 cd $project/tests/e2e/testcafe
 git clean -xdf
@@ -7,7 +7,8 @@ npm i
 
 export SITE_URL=$appUrl
 export SCREENSHOTS_PATH=./screenshots
-# SELENIUM_SERVER is setup in jenkins as we may want to change it any time without update the code
+
+echo "Following options are used by E2E framework"
 echo $SELENIUM_SERVER
 echo $BROWSERS
 echo $RC_PLATFORM_APP_KEY
@@ -17,21 +18,20 @@ echo $ACTION
 echo $BRANCH
 echo $SCREENSHOTS_PATH
 
-mkdir -p $SCREENSHOTS_PATH
-npx ts-node multi-run.ts
-rm -r $SCREENSHOTS_PATH
+export SELENIUM_SERVER=$SELENIUM_SERVER
+export BROWSERS=$BROWSERS
+export RC_PLATFORM_APP_KEY=$RC_PLATFORM_APP_KEY
+export RC_PLATFORM_APP_SECRET=$RC_PLATFORM_APP_SECRET
+export ACTION=$ACTION
+export BRANCH=$BRANCH
+export ENABLE_REMOTE_DASHBOARD=true
 
+mkdir -p $SCREENSHOTS_PATH
+npx ts-node create-run-id.ts
+npx ts-node multi-run.ts
 e2eResult=$?
 
-xmlResult=$project/tests/e2e/testcafe/allure/allure-results
-htmlResult=$project/tests/e2e/testcafe/allure/html-results
-allure generate $xmlResult -o $htmlResult
-
-localFolder=$htmlResult
-remoteFolder=e2e/$subDomain/$BUILD_NUMBER
-syncFolderToServer $localFolder $remoteFolder
-
-E2EUrl=https://e2e.fiji.gliprc.com/$subDomain/$BUILD_NUMBER/index.html
-addEnv E2EResult="> **E2E Url**: $E2EUrl"
+export REPORT_URL=`cat ./reportUrl`
+addEnv E2EResult="> **E2E Url**: $REPORT_URL"
 
 echo "E2E Result: $e2eResult"
