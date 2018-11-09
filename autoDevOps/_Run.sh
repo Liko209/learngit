38c5d8a8961a9e187d854ff5cf2023d7322d3355
Henry Xu <henry.xu@ringcentral.com>
2018-11-09 10:06:50 +0800

    chore(pipeline): move e2e to the end of pipeline

--- autoDevOps/_Run.sh
+++ autoDevOps/_Run.sh
@@ -10,7 +10,6 @@ n_procs=(
     ". $autoDevOps/3.UT.sh"
     ". $autoDevOps/4.BuildAndDeployApplication.sh"
     ". $autoDevOps/4.BuildAndDeployJui.sh"
-    ". $autoDevOps/5.E2E.sh"
     #". $autoDevOps/6.Puppeteer.sh"
 )
 
@@ -29,5 +28,6 @@ for pid in ${pids[*]}; do
         exit 1
     fi
 done
+. $autoDevOps/5.E2E.sh
 
 addEnv hasLintError=$hasLintError
