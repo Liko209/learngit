fd65d0ff94921a7df53e8387dafb90d03e31c3c1
Nello Huang <nello.huang@ringcentral.com>
2018-11-13 14:42:12 +0800

    test(file and helper): test

--- application/src/containers/ConversationSheet/Files/__tests__/Files.ViewModel.test.ts
+++ application/src/containers/ConversationSheet/Files/__tests__/Files.ViewModel.test.ts
@@ -9,19 +9,16 @@ import { service } from 'sdk';
 jest.mock('../../../../store/utils');
 // import ItemModel from '@/store/models/Item';
 // import { FileType } from '../types';
-import { FileType } from '../types';
 
 const { ItemService } = service;
-ItemService.getInstance = jest.fn().mockReturnValue({
-
-});
+ItemService.getInstance = jest.fn().mockReturnValue({});
 
 const filesItemVM = new FilesViewModel();
 filesItemVM.props.ids = [1, 2, 3];
 
 const mockItemValue = {
   name: 'filename',
-  type: 1,
+  type: 2,
   id: 1,
   pages: {
     file_id: 11,
@@ -30,7 +27,6 @@ const mockItemValue = {
   isNew: false,
   isDocument: true,
   url: 'https://material-ui.com/',
-  getFileType: () => mockItemValue,
 };
 
 describe('filesItemVM', () => {
@@ -46,20 +42,13 @@ describe('filesItemVM', () => {
     expect(filesItemVM._ids).toEqual([1, 2, 3]);
   });
 
-  it('get files', () => {
-
-    expect(filesItemVM.files).toEqual({
-      [FileType.image]: [],
-      [FileType.document]: [{
-        ...mockItemValue,
-      }, { ...mockItemValue }, { ...mockItemValue }],
-      [FileType.others]: [],
-    });
-  });
-
   it('get items', () => {
-    expect(filesItemVM.items).toMatchObject([{
-      ...mockItemValue,
-    }, { ...mockItemValue }, { ...mockItemValue }]);
+    expect(filesItemVM.items).toMatchObject([
+      {
+        ...mockItemValue,
+      },
+      { ...mockItemValue },
+      { ...mockItemValue },
+    ]);
   });
 });
