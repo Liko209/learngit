384a8dde3b6dd7e40c2cbb8fadfa67e6811c2227
potar.he <potar.he@ringcentral.com>
2018-11-14 20:04:55 +0800

    refactor(e2e): conversationPage webcomponent

---
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessagePanel/ConversationPage.ts
@@ -0,0 +1,115 @@
+import { BaseWebComponent } from '../../../BaseWebComponent';
+
+
+class BaseConversationPage extends BaseWebComponent {
+  get posts() {
+    return this.self.find('[data-name="conversation-card"]');
+  }
+
+  get header() {
+    return this.getSelectorByAutomationId('conversation-page-header');
+  }
+
+  get title() {
+    return this.getSelectorByAutomationId('conversation-page-header-title');
+  }
+
+  get leftWrapper() {
+    return this.header.find('.left-wrapper');
+  } 
+
+  nthPostItem(nth: number) {
+    return this.getComponent(PostItem, this.posts.nth(nth));
+  }
+
+  postItemById(postId: string) {
+    return this.getComponent(PostItem, this.posts.filter(`[data-id="${postId}"]`));
+  }
+
+  get streamWrapper() {
+    return this.getSelectorByAutomationId('jui-stream-wrapper');
+  }
+
+  get stream() {
+    return this.getSelectorByAutomationId('jui-stream');
+  }
+
+  async expectStreamScrollToBottom() {
+    const scrollTop = await this.streamWrapper.child(0).scrollTop;
+    const streamHeight = await this.stream.clientHeight;
+    const streamWrapperHeight = await this.streamWrapper.clientHeight;
+    await this.t.expect(scrollTop).eql(streamHeight - streamWrapperHeight);
+  }
+}
+
+export class ConversationPage extends BaseConversationPage {
+  get self() {
+    return this.getSelector('.conversation-page');
+  }
+
+  get messageInputArea() {
+    this.warnFlakySelector();
+    return this.self.child().find('.ql-editor');
+  }
+
+  async sendMessage(message: string) {
+    await this.t
+      .typeText(this.messageInputArea, message)
+      .click(this.messageInputArea)
+      .pressKey('enter');
+  }
+  
+  async favorite(){
+    await this.t.click(this.leftWrapper.find('span').withText('star').nextSibling('input'));
+  }
+
+  async unFavorite(){
+    await this.t.click(this.leftWrapper.find('span').withText('star_border').nextSibling('input'));
+  }
+  
+  get currentGroupId() {
+    return this.self.getAttribute('data-group-id');
+  }
+} 
+
+export class MentionPage extends BaseConversationPage {
+  get self() {
+    return this.getSelectorByAutomationId('post-list-page');
+  }
+} 
+
+export class PostItem extends BaseWebComponent {
+  get avatar() {
+    return this.self.find(`[data-name="avatar"]`);
+  }
+
+  get name() {
+    return this.self.find(`[data-name="name"]`);
+  }
+
+  get userStatus() {
+    this.warnFlakySelector();
+    return this.name.nextSibling('div')
+  }
+
+  get time() {
+    return this.self.find(`[data-name="time"]`);
+  }
+
+  get body() {
+    return this.self.find(`[data-name="body"]`);
+  }
+
+  get text() {
+    return this.self.find(`[data-name="text"]`);
+  }
+
+  // mention page only
+  get conversationName() {
+    return this.self.find('.conversation-name')
+  }
+
+  async goToConversation() {
+    await this.t.click(this.conversationName);
+  }
+}
