26266c2ca3cc195ed8eab3501463b95a32e8f6cc
Alvin Huang <alvin.huang@ringcentral.com>
2018-11-26 17:22:41 +0800

    feat(FIJI-365): [profile entry]add profile component

---
+++ application/src/containers/common/Profile/Profile.View.tsx
@@ -0,0 +1,34 @@
+/*
+ * @Author: Alvin Huang (alvin.huang@ringcentral.com)
+ * @Date: 2018-11-26 17:02:02
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import React, { Component, MouseEvent } from 'react';
+import { translate, WithNamespaces } from 'react-i18next';
+import { ProfileButtonProps } from './types';
+// import { JuiModal } from '@/containers/Dialog';
+type Props = ProfileButtonProps & WithNamespaces;
+import styled from 'jui/foundation/styled-components';
+
+const StyledParam = styled.p`
+  color: ${({ theme }) => theme.palette.primary.main};
+  font-size: ${({ theme }) => theme.typography.button.fontSize};
+  cursor: pointer;
+`;
+class ProfileButton extends Component<Props> {
+  private onClick = (event: MouseEvent<HTMLElement>) => {
+    this.props.handleGlobalGroupId(event);
+    // JuiModal.open(GroupTeamProfile, { size: 'medium' });
+  }
+  render() {
+    const { t } = this.props;
+    return (
+      <StyledParam onClick={this.onClick}>{t('Profile')}</StyledParam>
+    );
+  }
+}
+
+const ProfileButtonView = translate('translations')(ProfileButton);
+
+export { ProfileButtonView };
---
+++ application/src/containers/common/Profile/types.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: Alvin Huang (alvin.huang@ringcentral.com)
+ * @Date: 2018-11-26 17:02:02
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { MouseEvent } from 'react';
+type ProfileButtonProps = {
+  id: number;
+  handleGlobalGroupId: (event: MouseEvent<HTMLElement>) => void
+};
+export { ProfileButtonProps };
