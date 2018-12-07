3ebef4d2655abe1f36967c13db51877ed6863d8c
Devin Lin <devin.lin@ringcentral.com>
2018-12-07 15:11:19 +0800

    test(FIJI-1999): [Person profile dialog header] add profile dialog person view UT

---
+++ application/src/containers/Profile/Dialog/Person/__tests__/Person.test.tsx
@@ -0,0 +1,27 @@
+/*
+ * @Author: Devin Lin (devin.lin@ringcentral.com)
+ * @Date: 2018-11-27 09:50:18
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ProfileDialogPerson } from '../Person';
+import {
+  JuiDialogTitleWithAction,
+  JuiDialogContentWithFill,
+} from 'jui/components/Dialog';
+import { ProfileDialogPersonTitle } from '../Title';
+
+describe('ProfileDialogPerson', () => {
+  describe('render()', () => {
+    it('should be render title and content component when instance component', () => {
+      const Wrapper = shallow(
+        <ProfileDialogPerson id={1} dismiss={() => {}} />,
+      );
+      expect(Wrapper.find(JuiDialogTitleWithAction)).toHaveLength(1);
+      expect(Wrapper.find(JuiDialogContentWithFill)).toHaveLength(1);
+      expect(Wrapper.find(ProfileDialogPersonTitle)).toHaveLength(1);
+    });
+  });
+});
