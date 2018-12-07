1f7f5276293e51fcebd233a1098580c9131df815
Chris Zhan <chris.zhan@ringcentral.com>
2018-12-07 18:25:20 +0800

    feat(FIJI-1955): [[Tech] Toast Manager Container] remove useless comments

--- application/src/containers/ToastWrapper/Toast/types.ts
+++ application/src/containers/ToastWrapper/Toast/types.ts
@@ -3,18 +3,10 @@
  * @Date: 2018-12-06 14:41:51
  * Copyright © RingCentral. All rights reserved.
  */
-// import { JuiSnackbarsType, MessageAlignment } from 'jui/components/Snackbars';
 import { ShowNotificationOptions } from '@/containers/Notification';
-// import {Snackbar} from '@material-ui/core/Snackbar';
 
 type ToastProps = ShowNotificationOptions & {
   id?: number;
-  // type: JuiSnackbarsType;
-  // message: React.ReactNode | string;
-  // action?: React.ReactNode;
-  // messageAlign?: MessageAlignment;
-  // autoHideDuration?: number;
-  // dismissible?: boolean;
   dismiss: () => void;
 };
 
