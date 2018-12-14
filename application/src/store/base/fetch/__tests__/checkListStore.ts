120619da92885d3ad3c59dc46733e1453703e32f
Valor Lin <valor.lin@ringcentral.com>
2018-12-14 12:45:05 +0800

    feat(FIJI-1934): [There's no "Today" indicator above today's messages] restore checkListStore

---
+++ application/src/store/base/fetch/__tests__/checkListStore.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: Steve Chen (steve.chen@ringcentral.com)
+ * @Date: 2018-10-05 23:07:55
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { ListStore } from '../index';
+
+const checkListStore = <T>(listStore: ListStore<T>, expectData: T[]) => {
+  expect(listStore.items).toEqual(expectData);
+};
+export default checkListStore;
