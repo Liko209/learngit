0e59313afbbafcc0147eb5900ff301384a484e26
wayne zhou <wayne.zhou@ringcentral.com>
2018-12-17 17:02:44 +0800

    feat(FIJI-2115): [tooltip arrow position] remove parser.ts change

--- packages/sdk/src/utils/error/parser.ts
+++ packages/sdk/src/utils/error/parser.ts
@@ -39,25 +39,9 @@ class ErrorParser {
     const { data, status, statusText } = resp;
 
     /**
-     * From resp.statusText
-     */
-    if (statusText === 'Network Error') {
-      return new BaseError(
-        ErrorTypes.API_NETWORK,
-        'Api Error: Please check whether server crash',
-      );
-    }
-
-    if (statusText === 'NOT NETWORK CONNECTION') {
-      return new BaseError(
-        ErrorTypes.API_NETWORK,
-        'Api Error: Please check network connection',
-      );
-    }
-    /**
      * From resp.data
      */
-    if (data && data.error) {
+    if (data.error) {
       let httpErrorCode: string = '';
       let httpErrorMessage: string = '';
 
@@ -79,6 +63,23 @@ class ErrorParser {
       if (code) {
         return new BaseError(code, httpErrorMessage);
       }
+    }
+
+    /**
+     * From resp.statusText
+     */
+    if (statusText === 'Network Error') {
+      return new BaseError(
+        ErrorTypes.API_NETWORK,
+        'Api Error: Please check whether server crash',
+      );
+    }
+
+    if (statusText === 'NOT NETWORK CONNECTION') {
+      return new BaseError(
+        ErrorTypes.API_NETWORK,
+        'Api Error: Please check network connection',
+      );
     }
 
     /**
