073bff7fc9ef31f632e6f5838ee761d3cc9657da
Devin Lin <devin.lin@ringcentral.com>
2018-12-17 17:14:40 +0800

    feat(FIJI-2167): [Should delay 0.5s to show loading when sending one post] send post message delay l

--- application/src/containers/ConversationCard/ProgressActions/ProgressActions.View.tsx
+++ application/src/containers/ConversationCard/ProgressActions/ProgressActions.View.tsx
@@ -16,9 +16,17 @@ import { POST_STATUS } from 'sdk/service';
 import { JuiModal } from '@/containers/Dialog';
 
 type Props = ProgressActionsViewProps & WithNamespaces;
+type States = {
+  postStatus?: POST_STATUS;
+};
 
 @observer
-class ProgressActionsViewComponent extends Component<Props> {
+class ProgressActionsViewComponent extends Component<Props, States> {
+  timer: NodeJS.Timer;
+  state: States = {
+    postStatus: POST_STATUS.SUCCESS,
+  };
+
   delete = () => {
     const { deletePost, t } = this.props;
     JuiModal.confirm({
@@ -42,33 +50,83 @@ class ProgressActionsViewComponent extends Component<Props> {
     } catch (e) {}
   }
 
+  renderLoading = () => {
+    const { postStatus } = this.state;
+    if (postStatus === POST_STATUS.INPROGRESS) {
+      return <JuiCircularProgress size={12} />;
+    }
+    return null;
+  }
+
+  renderResend = () => {
+    const { postStatus } = this.state;
+    const { t } = this.props;
+    if (postStatus === POST_STATUS.FAIL) {
+      return (
+        <JuiIconButton
+          variant="plain"
+          tooltipTitle={t('resendPost')}
+          onClick={this.resend}
+          size="small"
+          color="semantic.negative"
+        >
+          refresh
+        </JuiIconButton>
+      );
+    }
+    return null;
+  }
+
+  renderDelete = () => {
+    const { postStatus } = this.state;
+    const { t } = this.props;
+    if (postStatus === POST_STATUS.FAIL) {
+      return (
+        <JuiIconButton
+          variant="plain"
+          tooltipTitle={t('deletePost')}
+          onClick={this.delete}
+          size="small"
+        >
+          delete
+        </JuiIconButton>
+      );
+    }
+    return null;
+  }
+
+  setPostStatus = () => {
+    const { status } = this.props.post;
+    clearTimeout(this.timer);
+    if (status === POST_STATUS.INPROGRESS) {
+      this.timer = setTimeout(() => {
+        this.setState({ postStatus: status });
+      },                      500);
+    } else {
+      this.setState({ postStatus: status });
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.post.status !== this.state.postStatus) {
+      this.setPostStatus();
+    }
+  }
+
+  componentDidMount() {
+    this.setPostStatus();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
   render() {
-    const { t, post } = this.props;
-    const { status = POST_STATUS.SUCCESS } = post;
     return (
       <JuiActions>
-        {status === POST_STATUS.INPROGRESS && <JuiCircularProgress size={12} />}
-        {status === POST_STATUS.FAIL && (
-          <JuiIconButton
-            variant="plain"
-            tooltipTitle={t('resendPost')}
-            onClick={this.resend}
-            size="small"
-            color="semantic.negative"
-          >
-            refresh
-          </JuiIconButton>
-        )}
-        {status === POST_STATUS.FAIL && (
-          <JuiIconButton
-            variant="plain"
-            tooltipTitle={t('deletePost')}
-            onClick={this.delete}
-            size="small"
-          >
-            delete
-          </JuiIconButton>
-        )}
+        {this.renderLoading()}
+        {this.renderResend()}
+        {this.renderDelete()}
       </JuiActions>
     );
   }
