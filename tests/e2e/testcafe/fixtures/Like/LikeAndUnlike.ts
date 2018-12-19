70a75b04ecd19294114a69de0f0c0b93ddeeca92
potar.he <potar.he@ringcentral.com>
2018-12-19 16:02:30 +0800

    chore(FIJI-2353) [implement JPT-308 case] finish case JPT-308 and define e2e scope

--- tests/e2e/testcafe/fixtures/Like/LikeAndUnlike.ts
+++ tests/e2e/testcafe/fixtures/Like/LikeAndUnlike.ts
@@ -10,7 +10,7 @@ import { setupCase, teardownCase } from '../../init';
 import { AppRoot } from "../../v2/page-models/AppRoot";
 import { v4 as uuid } from 'uuid';
 
-fixture('LikeAndUnlike')
+fixture('Like/LikeAndUnlike')
   .beforeEach(setupCase('GlipBetaUser(1210,4488)'))
   .afterEach(teardownCase());
 
@@ -19,11 +19,8 @@ test(formalName('Operating a message that you first like then unlike', ['JPT-304
   const userA = users[4];
   userA.sdk = await h(t).getSdk(userA);
   const userB = users[5];
-  const userBGlip = await h(t).getGlip(userB);
   const app = new AppRoot(t);
 
-  
- 
   let teamId, postId;
   await h(t).withLog(`Given I 1 team chat (T1) have members userA(${userA.company.number}#${userA.extension}) and userB(${userB.company.number}#${userB.extension}) and 1 post (M1) in T1.`, async () => {
     teamId = (await userA.sdk.platform.createGroup({
@@ -32,8 +29,6 @@ test(formalName('Operating a message that you first like then unlike', ['JPT-304
       type: 'Team',
       members: [userA.rcId, userB.rcId],
     })).data.id;
-    await userA.sdk.glip.showGroups(userA.rcId, teamId);
-    await userBGlip.showGroups(userB.rcId, teamId);
     postId = (await userA.sdk.platform.sendTextPost(
       `test "like" ${uuid()}`,
       teamId
@@ -51,7 +46,7 @@ test(formalName('Operating a message that you first like then unlike', ['JPT-304
     await t.useRole(roleA);
     await postCard.ensureLoaded();
     await t.hover(postCard.self)
-  })
+  });
 
   await h(t).withLog(`Then Appear action bar and there have hollow "unlike" icon `, async () => {
     await t.hover(postCard.likeToggleOnActionBar);
@@ -85,7 +80,6 @@ test(formalName('Operating a message that you first like then unlike', ['JPT-304
   await h(t).withLog(`When userB view the message M1 card`, async () => {
     await t.useRole(roleB);
     await app.homePage.ensureLoaded();
-
     await postCard.ensureLoaded();
   });
 
@@ -170,3 +164,162 @@ test(formalName('Operating a message that you first like then unlike', ['JPT-304
   });
 });
 
+
+test(formalName('Like a message that you not first like then unlike', ['JPT-308', 'P2', 'Like', 'Potar.He']), async (t) => {
+  const users = h(t).rcData.mainCompany.users
+  const userA = users[4];
+  userA.sdk = await h(t).getSdk(userA);
+  const userB = users[5];
+  const otherGlip = await h(t).getGlip(users[6]);
+  const app = new AppRoot(t);
+
+  let teamId, postId;
+  await h(t).withLog(`Given I 1 team chat (T1) have members userA(${userA.company.number}#${userA.extension}) and userB(${userB.company.number}#${userB.extension}) and 1 post (M1) in T1.`, async () => {
+    teamId = await userA.sdk.platform.createGroup({
+      isPublic: true,
+      name: uuid(),
+      type: 'Team',
+      members: [userA.rcId, userB.rcId, users[6].rcId],
+    }).then(res => {
+      return res.data.id}
+    );
+    postId = await userA.sdk.platform.sendTextPost(
+      `test "like" ${uuid()}`,
+      teamId
+    ).then(res => {return res.data.id});
+  });
+
+  const enterSpecifyTeam = async (app) => {
+    await app.homePage.messageTab.teamsSection.conversationEntryById(teamId).enter();
+  } 
+  const roleA = await h(t).userRole(userA, enterSpecifyTeam);
+  const roleB = await h(t).userRole(userB, enterSpecifyTeam);
+  const postCard = app.homePage.messageTab.conversationPage.postItemById(postId);
+
+  await h(t).withLog(`And userC like the M1`, async () => {
+    await otherGlip.likePost(postId);
+  })
+  await h(t).withLog(`When userA open T1`, async () => {
+    await t.useRole(roleA);
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" icon with number 1 at M1 footer`, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  });
+
+  await h(t).withLog(`When userB open T1`, async () => {
+    await t.useRole(roleB);
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" icon with number 1 at M1 footer`, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  });
+
+  await h(t).withLog(`When userA hover M1`, async () => {
+    await t.useRole(roleA);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" icon on Action Bar`, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  }); 
+
+  // A click like on Action Bar
+  await h(t).withLog(`When userA click "unlike" icon on Action Bar`, async () => {
+    await postCard.clickLikeOnActionBar();
+  })
+
+  await h(t).withLog(`Then the both hollow "unlike" icon change to solid "like" icon and the post Like number should be 2 `, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).eql('thumb_up');
+    await t.expect(postCard.likeButtonOnFooter.textContent).eql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(2);
+  }); 
+
+  await h(t).withLog(`When userB check the M1`, async () => {
+    await t.useRole(roleB);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" with number 2 on M1 footer`, async () => {
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(2);
+  }); 
+
+  await h(t).withLog(`When userA click the solid "like" icon`, async () => {
+    await t.useRole(roleA);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+    await postCard.clickLikeOnActionBar();
+  });
+
+  await h(t).withLog(`Then the both hollow "unlike" icon change to solid icon and the post Like number should be 1 `, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).notEql('thumb_up');
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  }); 
+
+  await h(t).withLog(`When userB check the M1`, async () => {
+    await t.useRole(roleB);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" with number 1 on M1 footer`, async () => {
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  });
+
+  // A click like on footer
+  await h(t).withLog(`When userA click "unlike" icon on Action Bar`, async () => {
+    await t.useRole(roleA);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+    await postCard.clickLikeButtonOnFooter();
+  })
+
+  await h(t).withLog(`Then the both hollow "unlike" icon change to solid "like" icon and the post Like number should be 2 `, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).eql('thumb_up');
+    await t.expect(postCard.likeButtonOnFooter.textContent).eql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(2);
+  }); 
+
+  await h(t).withLog(`When userB check the M1`, async () => {
+    await t.useRole(roleB);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" with number 2 on M1 footer`, async () => {
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(2);
+  }); 
+
+  await h(t).withLog(`When userA click the solid "like" icon`, async () => {
+    await t.useRole(roleA);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+    await postCard.clickLikeButtonOnFooter();
+  });
+
+  await h(t).withLog(`Then the both hollow "unlike" icon change to solid icon and the post Like number should be 1 `, async () => {
+    await t.expect(postCard.likeToggleOnActionBar.textContent).notEql('thumb_up');
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  }); 
+
+  await h(t).withLog(`When userB check the M1`, async () => {
+    await t.useRole(roleB);
+    await app.homePage.ensureLoaded();
+    await postCard.ensureLoaded();
+  })
+
+  await h(t).withLog(`Then there is a hollow "unlike" with number 1 on M1 footer`, async () => {
+    await t.expect(postCard.likeButtonOnFooter.textContent).notEql('thumb_up');
+    await t.expect(await postCard.getLikeCount()).eql(1);
+  });
+});
