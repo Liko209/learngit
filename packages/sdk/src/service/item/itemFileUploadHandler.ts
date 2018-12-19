af3c8038f5db894a22cd7e5b1b5a3ba993364184
Thomas Yang <thomas.yang@ringcentral.com>
2018-12-19 19:40:04 +0800

    fix(FIJI-2375): [Update failed when selected one file to attachment then re-select it then tap update] fix failed to handle duplicated success file

--- packages/sdk/src/service/item/itemFileUploadHandler.ts
+++ packages/sdk/src/service/item/itemFileUploadHandler.ts
@@ -418,8 +418,9 @@ class ItemFileUploadHandler {
     const uploadingFiles = this._uploadingFiles.get(groupId);
     if (uploadingFiles) {
       const filteredRes = uploadingFiles.filter((e: ItemFile) => {
-        return e.id !== preInsertId;
+        return e.id !== preInsertId && e.id !== itemFile.id;
       });
+
       filteredRes.push(itemFile);
       this._uploadingFiles.set(groupId, filteredRes);
     }
