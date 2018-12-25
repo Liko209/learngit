bcb08a84b090548f8c802efd46a561f7e24c41b4
Shining <shining.miao@ringcentral.com>
2018-12-25 11:10:11 +0800

    fix(FIJI-1963): [[Create team]The previous search results will be displayed then the current search results will be displayed] reset handleChange

--- packages/jui/src/pattern/ContactSearch/ContactSearch.tsx
+++ packages/jui/src/pattern/ContactSearch/ContactSearch.tsx
@@ -180,11 +180,15 @@ class JuiContactSearch extends React.PureComponent<Props, State> {
       selectedItem = [...selectedItem, item];
     }
 
-    this.setState({
-      selectedItem,
-      inputValue: '',
-    });
-    this.props.onSelectChange(this.state.selectedItem);
+    this.setState(
+      {
+        selectedItem,
+        inputValue: '',
+      },
+      () => {
+        this.props.onSelectChange(this.state.selectedItem);
+      },
+    );
   }
 
   handleDelete = (item: Suggestion) => () => {
