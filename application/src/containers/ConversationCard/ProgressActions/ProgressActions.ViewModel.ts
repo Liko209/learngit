c5da614955e832393e5579d44705c7f97d8a5995
isaac.liu <isaac.liu@ringcentral.com>
2018-12-25 10:58:20 +0800

    feat(FIJI-2088): [[UI] Error handling] make toast auto hide.

--- application/src/containers/ConversationCard/ProgressActions/ProgressActions.ViewModel.ts
+++ application/src/containers/ConversationCard/ProgressActions/ProgressActions.ViewModel.ts
@@ -58,7 +58,7 @@ class ProgressActionsViewModel extends AbstractViewModel<ProgressActionsProps>
         type: 'error',
         messageAlign: 'left',
         fullWidth: false,
-        dismissible: true,
+        dismissible: false,
       });
     }
   }
