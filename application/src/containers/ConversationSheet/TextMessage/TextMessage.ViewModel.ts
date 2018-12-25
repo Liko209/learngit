a1b42199e595b8b935ee2ffbb5b67e541ae29419
Devin Lin <devin.lin@ringcentral.com>
2018-12-25 17:18:26 +0800

    fix(FIJI-2485): [Conversation card at mentions support for group and person] at mention support group

--- application/src/containers/ConversationSheet/TextMessage/TextMessage.ViewModel.ts
+++ application/src/containers/ConversationSheet/TextMessage/TextMessage.ViewModel.ts
@@ -6,14 +6,16 @@
 import { computed } from 'mobx';
 import { StoreViewModel } from '@/store/ViewModel';
 import PostModel from '@/store/models/Post';
+import GroupModel from '@/store/models/Group';
 import CompanyModel from '@/store/models/Company';
 import { getEntity, getGlobalValue } from '@/store/utils';
-import { Post, Person, Company } from 'sdk/models';
+import { Post, Person, Group, Company } from 'sdk/models';
 import { ENTITY_NAME } from '@/store';
 import { GLOBAL_KEYS } from '@/store/constants';
 import PersonModel from '@/store/models/Person';
 import { FormatToHtml } from './FormatToHtml';
 import { TextMessageProps } from './types';
+import { GlipTypeUtil, TypeDictionary } from 'sdk/utils';
 
 class TextMessageViewModel extends StoreViewModel<TextMessageProps> {
   @computed
@@ -21,16 +23,30 @@ class TextMessageViewModel extends StoreViewModel<TextMessageProps> {
     return getEntity<Post, PostModel>(ENTITY_NAME.POST, this.props.id);
   }
 
+  private _getGroup(id: number) {
+    return getEntity<Group, GroupModel>(ENTITY_NAME.GROUP, id);
+  }
+
+  private _getPerson(id: number) {
+    return getEntity<Person, PersonModel>(ENTITY_NAME.PERSON, id);
+  }
+
   @computed
   private get _atMentions() {
     const post = this._post;
     const atMentionNonItemIds = (post && post.atMentionNonItemIds) || [];
     const kv = {};
-    atMentionNonItemIds.forEach((personId: number) => {
-      kv[personId] = getEntity<Person, PersonModel>(
-        ENTITY_NAME.PERSON,
-        personId,
-      ).displayName;
+    atMentionNonItemIds.forEach((id: number) => {
+      const type = GlipTypeUtil.extractTypeId(id);
+      if (
+        type === TypeDictionary.TYPE_ID_GROUP ||
+        type === TypeDictionary.TYPE_ID_TEAM
+      ) {
+        kv[id] = this._getGroup(id).displayName;
+      }
+      if (type === TypeDictionary.TYPE_ID_PERSON) {
+        kv[id] = this._getPerson(id).userDisplayName;
+      }
     });
     return kv;
   }
@@ -46,7 +62,9 @@ class TextMessageViewModel extends StoreViewModel<TextMessageProps> {
     if (currentCompanyId <= 0) {
       return {};
     }
-    const company = getEntity<Company, CompanyModel>(ENTITY_NAME.COMPANY, currentCompanyId) || {};
+    const company =
+      getEntity<Company, CompanyModel>(ENTITY_NAME.COMPANY, currentCompanyId) ||
+      {};
     return company.customEmoji || {};
   }
 
