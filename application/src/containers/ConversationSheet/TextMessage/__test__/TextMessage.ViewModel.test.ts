742375db83b633e171bc7f08fc0d6695ff0f5a7c
Devin Lin <devin.lin@ringcentral.com>
2018-12-26 08:47:40 +0800

    fix(FIJI-2485): [Conversation card at mentions support for group and person] change original unit test

--- application/src/containers/ConversationSheet/TextMessage/__test__/TextMessage.ViewModel.test.ts
+++ application/src/containers/ConversationSheet/TextMessage/__test__/TextMessage.ViewModel.test.ts
@@ -4,37 +4,62 @@
  * Copyright © RingCentral. All rights reserved.
  */
 import { getEntity } from '../../../../store/utils';
+import { ENTITY_NAME } from '@/store';
 import { TextMessageViewModel } from '../TextMessage.ViewModel';
 
 jest.mock('../../../../store/utils');
-const TextMessageVM = new TextMessageViewModel();
 
-const mockPostEntity = (text: string) => {
-  (getEntity as jest.Mock).mockReturnValue({
-    text,
-    atMentionNonItemIds: [1234],
-  });
+const mockPostData = {
+  text: 'Post text',
+  atMentionNonItemIds: [1],
 };
-describe('TextMessage', () => {
+
+const mockGroupData = {
+  displayName: 'Team name',
+};
+
+const mockPersonData = {
+  userDisplayName: 'Person name',
+};
+
+const mockMap = {
+  [ENTITY_NAME.POST]: mockPostData,
+  [ENTITY_NAME.GROUP]: mockGroupData,
+  [ENTITY_NAME.PERSON]: mockPersonData,
+};
+
+let vm: TextMessageViewModel;
+
+describe('TextMessageViewModel', () => {
   beforeAll(() => {
     jest.resetAllMocks();
+    (getEntity as jest.Mock).mockImplementation((name, id) => {
+      return mockMap[name];
+    });
   });
-  describe('formatHtml', () => {
-    it('url', () => {
-      mockPostEntity('https://www.baidu.com');
-      expect(TextMessageVM.html).toBe(
+
+  beforeEach(() => {
+    vm = new TextMessageViewModel();
+  });
+
+  describe('html', () => {
+    it('should be get url format text when text has link', () => {
+      mockPostData.text = 'https://www.baidu.com';
+      expect(vm.html).toBe(
         "<a href='https://www.baidu.com' target='_blank' rel='noreferrer'>https://www.baidu.com</a>",
       );
     });
-    it('email', () => {
-      mockPostEntity('xxx@163.com');
-      expect(TextMessageVM.html).toBe(
+
+    it('should be get email format text when text has email', () => {
+      mockPostData.text = 'xxx@163.com';
+      expect(vm.html).toBe(
         "<a href='mailto:xxx@163.com' target='_blank' rel='noreferrer'>xxx@163.com</a>",
       );
     });
-    it('bold font', () => {
-      mockPostEntity('**awesome**');
-      expect(TextMessageVM.html).toBe('<b>awesome</b>');
+
+    it('should be get bold font format text when there are two asterisks before and after', () => {
+      mockPostData.text = '**awesome**';
+      expect(vm.html).toBe('<b>awesome</b>');
     });
   });
 });
