5261348eec4715e8063d4de19aeba08be10995ea
Nello Huang <nello.huang@ringcentral.com>
2018-12-28 12:31:50 +0800

    test(FIJI-2002): [Improve modal] creat team new message test

--- application/src/containers/NewMessage/__tests__/NewMessage.ViewModel.test.ts
+++ application/src/containers/NewMessage/__tests__/NewMessage.ViewModel.test.ts
@@ -7,8 +7,6 @@
 import { service } from 'sdk';
 import { BaseError, ErrorTypes } from 'sdk/utils';
 import { err, ok } from 'foundation';
-import { getGlobalValue } from '../../../store/utils';
-import storeManager from '../../../store/index';
 import { NewMessageViewModel } from '../NewMessage.ViewModel';
 jest.mock('../../Notification');
 jest.mock('../../../store/utils');
@@ -33,7 +31,6 @@ describe('NewMessageVM', () => {
       get: jest.fn(),
       set: jest.fn(),
     };
-    jest.spyOn(storeManager, 'getGlobalStore').mockReturnValue(gs);
   });
 
   it('new message success', async () => {
@@ -84,20 +81,5 @@ describe('NewMessageVM', () => {
     const memberIds = [1, 2];
     const result = await newMessageVM.newMessage(memberIds, message);
     expect(result).toBeNull();
-  });
-
-  it('isOpen', () => {
-    (getGlobalValue as jest.Mock).mockReturnValue(undefined);
-    expect(newMessageVM.isOpen).toBe(false);
-    (getGlobalValue as jest.Mock).mockReturnValue(true);
-    expect(newMessageVM.isOpen).toBe(true);
-  });
-
-  it('inputReset', () => {
-    newMessageVM.inputReset();
-    expect(newMessageVM.emailErrorMsg).toBe('');
-    expect(newMessageVM.disabledOkBtn).toBe(true);
-    expect(newMessageVM.emailError).toBe(false);
-    expect(newMessageVM.serverError).toBe(false);
   });
 });
