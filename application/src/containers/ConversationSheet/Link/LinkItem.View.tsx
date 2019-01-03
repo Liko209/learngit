fce524ffce4f718b84a6b4e93acb4e2e77664d84
Alvin Huang <alvin.huang@ringcentral.com>
2019-01-03 10:35:01 +0800

    fix(FIJI-2724): [Click the name of URL card will go to wrong page] fix link protocol

--- application/src/containers/ConversationSheet/Link/LinkItem.View.tsx
+++ application/src/containers/ConversationSheet/Link/LinkItem.View.tsx
@@ -26,9 +26,18 @@ class LinkItemView extends React.Component<Props> {
         {postItems.map((item: LinkItemModel) => {
           // In Glip must has this key
           // hard code in order to show the current image
-          const image = item.image
-            ? `${item.image}&key=4527f263d6e64d7a8251b007b1ba9972`
-            : '';
+          let itemUrlWithProtocol;
+          const imgStamp = '&key=4527f263d6e64d7a8251b007b1ba9972';
+          const itemUrl = item.url;
+          if (itemUrl) {
+            itemUrlWithProtocol =
+              itemUrl.match('http://|https://')
+                ? itemUrl
+                : `http://${itemUrl}`;
+          } else {
+            itemUrlWithProtocol = itemUrl;
+          }
+          const image = item.image ? `${item.image}${imgStamp}` : '';
           return (item.title || item.image || item.summary) &&
             !item.doNotRender &&
             !item.deactivated ? (
@@ -37,7 +46,7 @@ class LinkItemView extends React.Component<Props> {
               title={item.title}
               summary={item.summary}
               thumbnail={image}
-              url={item.url}
+              url={itemUrlWithProtocol}
               onLinkItemClose={this.onLinkItemClose(item.id)}
               favicon={
                 item.favicon
