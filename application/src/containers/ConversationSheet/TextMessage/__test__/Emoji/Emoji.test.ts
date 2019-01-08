af9bd9194ef758c6d27f9e097449b8ac1da67cd3
Devin Lin <devin.lin@ringcentral.com>
2019-01-08 15:12:46 +0800

    fix(FIJI-2727): [[Production] The emoji in conversation is shown incorrectly] fixed duplication key

--- application/src/containers/ConversationSheet/TextMessage/__test__/Emoji/Emoji.test.ts
+++ application/src/containers/ConversationSheet/TextMessage/__test__/Emoji/Emoji.test.ts
@@ -8,10 +8,11 @@ import { Markdown } from 'glipdown';
 import { Emoji } from '../../Emoji';
 import { mapEmojiOne, mapAscii, mapUnicode } from '../../Emoji/map';
 
-const staticHttpServer = 'https://www.abc.com/a/b';
+const staticHttpServer = 'https://www.emojione.com/';
 const customEmojiMap = {
-  rc: { data: 'https://www.123.com/a/rc.png' },
-  att: { data: 'https://www.123.com/a/att.png' },
+  rc: { data: 'https://www.custom.com/rc.png' },
+  att: { data: 'https://www.custom.com/att.png' },
+  horse: { data: 'https://www.custom.com/horse.png' },
 };
 const arrNotMatch = [':xyz:', ':zyx:'];
 
@@ -22,9 +23,10 @@ function format(text: string) {
   return emoji.text;
 }
 
-describe('Format one kind emoji, only one kind emoji, has specific class name', () => {
+describe('Format one kind emoji', () => {
   const CLASS_NAME = 'enlarge-emoji';
-  const getRegExp = (unicode: string) => new RegExp(`^<img.+?${CLASS_NAME}.+?${unicode}\.png[^>]+?>$`);
+  const getRegExp = (unicode: string) =>
+    new RegExp(`^<img.+?${CLASS_NAME}.+?${unicode}[^>]+?>$`);
   const _runAll = (mapOriginalData: object) => {
     Object.keys(mapOriginalData).forEach((originKey: string) => {
       const result = format(originKey);
@@ -33,27 +35,27 @@ describe('Format one kind emoji, only one kind emoji, has specific class name', 
     });
   };
 
-  it('All EmojiOne', async () => {
+  it('should be a img tag when input every emojiOne key', async () => {
     _runAll(mapEmojiOne);
   });
 
-  it('All Ascii', async () => {
+  it('should be a img tag when input every ascii key', async () => {
     _runAll(mapAscii);
   });
 
-  it('All Unicode', async () => {
+  it('should be a img tag when input every unicode key', async () => {
     _runAll(mapUnicode);
   });
 
-  it('All Custom', async () => {
+  it('should be a img tag when input every custom key', async () => {
     const map = {};
     for (const key in customEmojiMap) {
-      map[`:${key}:`] = customEmojiMap[key];
+      map[`:${key}:`] = customEmojiMap[key].data;
     }
     _runAll(map);
   });
 
-  it('All Not match', async () => {
+  it('should be not a img tag when input every not match key', async () => {
     arrNotMatch.forEach((originKey: string) => {
       const result = format(originKey);
       const regExp = new RegExp(`^${originKey}$`);
@@ -63,8 +65,19 @@ describe('Format one kind emoji, only one kind emoji, has specific class name', 
 });
 
 describe('format multiple kind emoji', () => {
-  it('EmojiOne + Ascii + Unicode + Custom + Not match ', async () => {
-    const keys = [':smile:', ':cry:', '<3', ':D', ':rc:', ':att:', ':xxx:', ':yyy:', '🇭🇰', '🌶'];
+  it('should be match regular expressions when mixture emojiOne and ascii and unicode and custom and not match key', async () => {
+    const keys = [
+      ':smile:',
+      ':cry:',
+      '<3',
+      ':D',
+      ':rc:',
+      ':att:',
+      ':xxx:',
+      ':yyy:',
+      '🇭🇰',
+      '🌶',
+    ];
     const left = 'abc';
     const right = '123';
 
@@ -84,8 +97,63 @@ describe('format multiple kind emoji', () => {
     const key2 = Markdown(keys[2]);
     const key3 = Markdown(keys[3]);
 
-    const regExp = new RegExp(`^${left}${regExpEmojiOne0}${regExpEmojiOne1}${key2}${key3}${regExpCustom4}${regExpCustom5}${keys[6]}${keys[7]}${regExpUnicode8}${regExpUnicode9}${right}$`);
+    const regExp = new RegExp(
+      `^${left}${regExpEmojiOne0}${regExpEmojiOne1}${key2}${key3}${regExpCustom4}${regExpCustom5}${
+        keys[6]
+      }${keys[7]}${regExpUnicode8}${regExpUnicode9}${right}$`,
+    );
     expect(result).toMatch(regExp);
   });
   // 'abc:smile::cry:<3:D:rc::att::xxx::yyy123:'.match()
+});
+
+describe('Duplicate mapping key, because glip has old data problems', () => {
+  it('should be a img tag when duplicate key both in custom and emojione', async () => {
+    const result = format(':horse:');
+    // custom emoji first
+    const regExp = new RegExp(
+      `^<img[^>]+?${customEmojiMap.horse.data}[^>]+?>$`,
+    );
+    expect(result).toMatch(regExp);
+  });
+});
+
+describe('Emoji key colon regexp expression', () => {
+  it('should be a img tag when there are a colon before and after it', async () => {
+    const result = format('::smile::');
+    const unicode = mapEmojiOne[':smile:'].fname;
+    const regExp = new RegExp(`^:<img[^>]+?${unicode}[^>]+?>:$`);
+    expect(result).toMatch(regExp);
+  });
+
+  it('should be a img tag when there are emoji keys in the back', async () => {
+    const result = format(':cry:smile');
+    const unicode = mapEmojiOne[':cry:'].fname;
+    const regExp = new RegExp(`^<img[^>]+?${unicode}[^>]+?>smile$`);
+    expect(result).toMatch(regExp);
+  });
+
+  it('should be a img tag when there are emoji keys in the front', async () => {
+    const result = format('cry:smile:');
+    const unicode = mapEmojiOne[':smile:'].fname;
+    const regExp = new RegExp(`^cry<img[^>]+?${unicode}[^>]+?>$`);
+    expect(result).toMatch(regExp);
+  });
+
+  it('should be a img tag when there are one emoji keys effective', async () => {
+    const result = format(':cry:smile:');
+    const unicode = mapEmojiOne[':cry:'].fname;
+    const regExp = new RegExp(`^<img[^>]+?${unicode}[^>]+?>smile:$`);
+    expect(result).toMatch(regExp);
+  });
+
+  it('should be two img tag when there are two emoji keys effective', async () => {
+    const result = format(':cry::smile:');
+    const unicode1 = mapEmojiOne[':cry:'].fname;
+    const unicode2 = mapEmojiOne[':smile:'].fname;
+    const regExp1 = `^<img[^>]+?${unicode1}[^>]+?>`;
+    const regExp2 = `<img[^>]+?${unicode2}[^>]+?>$`;
+    const regExp = new RegExp(regExp1 + regExp2);
+    expect(result).toMatch(regExp);
+  });
 });
