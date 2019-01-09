60b1b45f111992ba66821e3b8b1e3f7898fe026c
jayson zhang <jayson.zhang@ringcentral.com>
2019-01-09 17:32:47 +0800

    feat(FIJI-2756): [Telephony] VoIP Account Send Re-reg with new transport

--- packages/voip/src/signaling/__tests__/RTCSipUserAgent.test.ts
+++ packages/voip/src/signaling/__tests__/RTCSipUserAgent.test.ts
@@ -43,7 +43,7 @@ describe('RTCSipUserAgent', async () => {
   });
 
   describe('register', () => {
-    it('Should emit registered event when register success [JPT-599]', () => {
+    it('Should register function have been called [JPT-599]', () => {
       const eventEmitter = new EventEmitter2();
       const userAgent = new RTCSipUserAgent(
         provisionData,
