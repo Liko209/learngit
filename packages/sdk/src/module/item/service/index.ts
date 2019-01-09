54dcbd307edb3e894413a83adc77670d60e24167
Looper Wang <looper.wang@ringcentral.com>
2019-01-09 16:31:30 +0800

    Merge branch 'develop' into feature/FIJI-2588

--- packages/sdk/src/module/item/service/index.ts
+++ packages/sdk/src/module/item/service/index.ts
@@ -3,9 +3,6 @@
  * @Date: 2019-01-02 16:03:08
  * Copyright © RingCentral. All rights reserved.
  */
-
-export { default as ProgressService, PROGRESS_STATUS } from './progress';
-export { default as GroupService } from './group';
 import { ItemService } from './ItemService';
 
 export { ItemService };
