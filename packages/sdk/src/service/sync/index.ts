969a24b87d70050699c898bb8af6a52a26f7233c
Thomas Yang <thomas.yang@ringcentral.com>
2019-01-14 15:56:44 +0800

    feat(FIJI-2866): [[SDK] Display Image] clear sub item dao when 504 gate error happened.

--- packages/sdk/src/service/sync/index.ts
+++ packages/sdk/src/service/sync/index.ts
@@ -13,6 +13,12 @@ import GroupDao from '../../dao/group';
 import PersonDao from '../../dao/person';
 import PostDao from '../../dao/post';
 import ItemDao from '../../dao/item';
+import { EventItemDao } from '../../module/item/module/event/dao/EventItemDao';
+import { FileItemDao } from '../../module/item/module/file/dao/FileItemDao';
+import { NoteItemDao } from '../../module/item/module/note/dao/NoteItemDao';
+import { TaskItemDao } from '../../module/item/module/task/dao/TaskItemDao';
+import { LinkItemDao } from '../../module/item/module/link/dao/LinkItemDao';
+
 import { LAST_INDEX_TIMESTAMP } from '../../dao/config/constants';
 import {
   fetchIndexData,
@@ -121,7 +127,12 @@ export default class SyncService extends BaseService {
 
   private async _handleSyncIndexError(result: any) {
     const error = ErrorParserHolder.getErrorParser().parse(result);
-    if (error.isMatch({ type: ERROR_TYPES.NETWORK, codes: [ERROR_CODES_NETWORK.GATEWAY_TIMEOUT] })) {
+    if (
+      error.isMatch({
+        type: ERROR_TYPES.NETWORK,
+        codes: [ERROR_CODES_NETWORK.GATEWAY_TIMEOUT],
+      })
+    ) {
       notificationCenter.emitKVChange(SERVICE.SYNC_SERVICE.START_CLEAR_DATA);
       await this._handle504GateWayError();
       notificationCenter.emitKVChange(SERVICE.SYNC_SERVICE.END_CLEAR_DATA);
@@ -140,6 +151,21 @@ export default class SyncService extends BaseService {
     await groupDao.clear();
     const personDao = daoManager.getDao(PersonDao);
     await personDao.clear();
+
+    const eventItemDao = daoManager.getDao(EventItemDao);
+    await eventItemDao.clear();
+
+    const fileItemDao = daoManager.getDao(FileItemDao);
+    await fileItemDao.clear();
+
+    const noteItemDao = daoManager.getDao(NoteItemDao);
+    await noteItemDao.clear();
+
+    const taskItemDao = daoManager.getDao(TaskItemDao);
+    await taskItemDao.clear();
+
+    const linkItemDao = daoManager.getDao(LinkItemDao);
+    await linkItemDao.clear();
 
     await this._firstLogin();
   }
