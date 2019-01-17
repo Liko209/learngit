47bd94f7a61a7c092c6b5c617a0345fc0779dccd
Henry Xu <henry.xu@ringcentral.com>
2019-01-17 15:12:25 +0800

    chore(FIJI-2911): [format report data] update jenkins script

--- autoDevOps/jenkins/gitlabIntegration.groovy
+++ autoDevOps/jenkins/gitlabIntegration.groovy
@@ -40,8 +40,9 @@ def condStage(Map args, Closure block) {
     assert args.name, 'stage name is required'
     String name = args.name
     Boolean enable = (null == args.enable) ? true: args.enable
-    Integer time = (null == args.timeout) ? 1800 : args.timeout
-    return timeout(time: time, unit: 'SECONDS') {
+    Integer time = (null == args.timeout) ? 600: args.timeout
+    Boolean activity = (null == args.activity) ? true: args.activity
+    return timeout(time: time, activity: activity, unit: 'SECONDS') {
         stage(name, enable ? block : {
             echo "Skip stage: ${name}"
         })
