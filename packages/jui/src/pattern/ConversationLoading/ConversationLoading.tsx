d5e612262e6e0052d762209740351bc697fb675d
Nello Huang <nello.huang@ringcentral.com>
2019-01-18 10:58:14 +0800

    feat(FIJI-2528): [Can't open a conversation after clicking "try again" when network recover] clear t

--- packages/jui/src/pattern/ConversationLoading/ConversationLoading.tsx
+++ packages/jui/src/pattern/ConversationLoading/ConversationLoading.tsx
@@ -62,15 +62,21 @@ type JuiConversationLoadingProps = {
   onClick: () => void;
 };
 
+type State = {
+  showLoading: boolean;
+  showLink: boolean;
+};
+
 const DELAY_LOADING = 300;
 
 class JuiConversationLoading extends React.Component<
   JuiConversationLoadingProps,
-  {}
+  State
 > {
   timer: NodeJS.Timeout;
+  showLinkTimer: NodeJS.Timeout;
 
-  state = {
+  readonly state = {
     showLoading: false,
     showLink: true,
   };
@@ -89,16 +95,17 @@ class JuiConversationLoading extends React.Component<
 
   componentWillReceiveProps(props: JuiConversationLoadingProps) {
     if (props.showTip) {
-      setTimeout(() => {
+      this.showLinkTimer = setTimeout(() => {
         this.setState({
           showLink: true,
         });
-      },         1000);
+      },                              1000);
     }
   }
 
   componentWillUnmount() {
     clearTimeout(this.timer);
+    clearTimeout(this.showLinkTimer);
   }
 
   onClick = () => {
