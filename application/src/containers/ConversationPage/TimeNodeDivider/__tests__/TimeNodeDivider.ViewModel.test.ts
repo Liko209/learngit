b0406fdbc38f61a8ced6013ade0001eff8692ef2
Looper Wang <looper.wang@ringcentral.com>
2019-01-21 11:34:48 +0800

    fix(FIJI-2848): [Lost timestamp and date indicator in some post.] UT cover.

--- application/src/containers/ConversationPage/TimeNodeDivider/__tests__/TimeNodeDivider.ViewModel.test.ts
+++ application/src/containers/ConversationPage/TimeNodeDivider/__tests__/TimeNodeDivider.ViewModel.test.ts
@@ -30,6 +30,10 @@ const DAY = 24 * 3600 * 1000;
 const DATE_2019_1_4 = 1546564919703;
 const DATE_2019_1_3 = 1546564919703 - DAY;
 const DATE_2019_1_2 = 1546564919703 - 2 * DAY;
+const DATE_2019_1_1 = 1546564919703 - 3 * DAY;
+const DATE_2018_12_30 = 1546564919703 - 5 * DAY;
+const DATE_2018_12_29 = 1546564919703 - 6 * DAY;
+const DATE_2018_12_28 = 1546564919703 - 7 * DAY;
 const DATE_2019_1_5 = 1546564919703 + DAY;
 
 describe('TimeNodeDividerViewModel', () => {
@@ -49,6 +53,33 @@ describe('TimeNodeDividerViewModel', () => {
       timeNodeDividerViewModel.props.value = DATE_2019_1_2;
       expect(timeNodeDividerViewModel.text).toBe('Wednesday');
     });
+
+    it.each`
+      data               | expected
+      ${DATE_2019_1_2}   | ${'Wednesday'}
+      ${DATE_2019_1_1}   | ${'Tuesday'}
+      ${DATE_2018_12_30} | ${'Sunday'}
+      ${DATE_2018_12_29} | ${'Saturday'}
+    `(
+      'should be Weekday format when createdAt is ${data}. [JPT-701]',
+      ({ data, expected }) => {
+        global.Date.now = jest.fn(() => DATE_2019_1_4);
+        timeNodeDividerViewModel.props.value = data;
+        expect(timeNodeDividerViewModel.text).toBe(expected);
+      },
+    );
+    it.each`
+      data               | expected
+      ${DATE_2019_1_3}   | ${'Monday'}
+      ${DATE_2018_12_28} | ${'Friday'}
+    `(
+      'should be Weekday format when createdAt is ${data}. [JPT-701]',
+      ({ data, expected }) => {
+        global.Date.now = jest.fn(() => DATE_2019_1_4);
+        timeNodeDividerViewModel.props.value = data;
+        expect(timeNodeDividerViewModel.text).not.toBe(expected);
+      },
+    );
     it('should be date format when createdAt is 2019/1/5. [JPT-701]', () => {
       global.Date.now = jest.fn(() => DATE_2019_1_4);
       timeNodeDividerViewModel.props.value = DATE_2019_1_5;
