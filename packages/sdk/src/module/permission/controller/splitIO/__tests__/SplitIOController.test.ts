d14a6e65b91b123e25dc9d5783d91dc295402312
Lip Wang <lip.wang@ringcentral.com>
2019-01-22 16:44:23 +0800

    feat(FIJI-2496): [Build entitlements management framework-SplitIO] add UT

---
+++ packages/sdk/src/module/permission/controller/splitIO/__tests__/SplitIOController.test.ts
@@ -0,0 +1,51 @@
+/*
+ * @Author: Lip Wang (lip.wang@ringcentral.com)
+ * @Date: 2019-01-22 16:24:47
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { SplitIOClient } from '../SplitIOClient';
+import { SplitIOController } from '../SplitIOController';
+import { notificationCenter, SERVICE } from '../../../../../service';
+import UserPermissionType from '../../../types';
+import { resolve } from 'path';
+
+jest.mock('../SplitIOClient');
+jest.mock('../../../../../service');
+
+SplitIOClient.prototype.hasPermission = async (type: UserPermissionType) => {
+  const result = (await type) === UserPermissionType.JUPITER_CREATE_TEAM;
+  return result;
+};
+
+describe('SplitIOController', () => {
+  describe('constructor', () => {
+    it('should call notificationCenter.on three times', () => {
+      new SplitIOController(() => {});
+      expect(notificationCenter.on).toHaveBeenCalledTimes(3);
+    });
+  });
+  describe('hasPermission', () => {
+    it('should return local default value when SplitIO is not ready', async () => {
+      const controller = new SplitIOController(() => {});
+      const permission = await controller.hasPermission(
+        UserPermissionType.JUPITER_CREATE_TEAM,
+      );
+      expect(permission).toBeFalsy();
+    });
+    it('should return SplitIO value when SplitIO is ready', async () => {
+      const controller = new SplitIOController(() => {});
+      Object.assign(controller, {
+        isClientReady: true,
+        splitIOClient: new SplitIOClient({}),
+      });
+      let result = await controller.hasPermission(
+        UserPermissionType.JUPITER_CREATE_TEAM,
+      );
+      expect(result).toBeTruthy();
+      result = await controller.hasPermission(
+        UserPermissionType.JUPITER_SEND_NEW_MESSAGE,
+      );
+      expect(result).toBeFalsy();
+    });
+  });
+});
---
+++ packages/sdk/src/module/permission/controller/splitIO/__tests__/SplitIODefaultPermissions.test.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Lip Wang (lip.wang@ringcentral.com)
+ * @Date: 2019-01-22 16:20:23
+ * Copyright © RingCentral. All rights reserved.
+ */
+import SplitIODefaultPermissions from '../SplitIODefaultPermissions';
+describe('SplitIODefaultPermissions', () => {
+  it('should have correct default value', () => {
+    expect(SplitIODefaultPermissions.JUPITER_CREATE_TEAM).toBeFalsy();
+    expect(SplitIODefaultPermissions.JUPITER_SEND_NEW_MESSAGE).toBeFalsy();
+  });
+});
