d2fae9b097c04508079bd75df2fe14457724e85c
Devin Lin <devin.lin@ringcentral.com>
2019-01-22 10:06:06 +0800

    feat(FIJI-3056): [Download button can't disappear when focus on other arear] pull out the public code

--- application/src/containers/RightRail/LinkItem/LinkItem.View.tsx
+++ application/src/containers/RightRail/LinkItem/LinkItem.View.tsx
@@ -9,32 +9,20 @@ import {
   JuiListItemText,
   JuiListItem,
   JuiListItemIcon,
-  JuiListItemSecondaryText,
-  JuiListItemSecondarySpan,
 } from 'jui/components/Lists';
 import { JuiThumbnail } from 'jui/components/Thumbnail';
 import { LinkItemViewProps } from './types';
 import { JuiIconography } from 'jui/foundation/Iconography';
+import { SecondaryText } from '../SecondaryText.View';
 
 class LinkItemView extends Component<LinkItemViewProps> {
-  private _renderSecondaryText = () => {
-    const { personName, createdTime } = this.props;
-    return (
-      <JuiListItemSecondaryText>
-        <JuiListItemSecondarySpan text={personName} isEllipsis={true} />
-        &nbsp;·&nbsp;
-        <JuiListItemSecondarySpan text={createdTime} />
-      </JuiListItemSecondaryText>
-    );
-  }
-
   private _openLink = () => {
     const { url } = this.props;
     window.open(url, '_blank');
   }
 
   render() {
-    const { link } = this.props;
+    const { link, personName, createdTime } = this.props;
     const textPrimary = link.title || link.url || '';
     const faviconUrl = link.faviconUrl;
     return (
@@ -51,7 +39,8 @@ class LinkItemView extends Component<LinkItemViewProps> {
         </JuiListItemIcon>
         <JuiListItemText
           primary={textPrimary}
-          secondary={this._renderSecondaryText()}
+          secondary={
+            <SecondaryText personName={personName} createdTime={createdTime} />}
         />
       </JuiListItem>
     );
