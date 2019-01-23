776918de99c6e7949db289363a5c6d5d9378687f
Chris Zhan <chris.zhan@ringcentral.com>
2019-01-23 15:59:35 +0800

    fix(FIJI-2779): [Cant show loading when scrolling down to load more posts] fix loading bottom

--- packages/jui/src/pattern/ConversationPage/Stream.tsx
+++ packages/jui/src/pattern/ConversationPage/Stream.tsx
@@ -15,7 +15,6 @@ const StyledDiv = styled<JuiStreamProps, 'div'>('div')`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 100%;
 `;
 
 const JuiStream = (props: JuiStreamProps) => (
