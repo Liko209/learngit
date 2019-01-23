e0cc42688b5cbfab53e60bc125134a8c053a450f
Henry Xu <henry.xu@ringcentral.com>
2019-01-23 13:17:19 +0800

    chore(FIJI-2911): [format report data] script to list fixture files

--- autoDevOps/jenkins/dailyJob.groovy
+++ autoDevOps/jenkins/dailyJob.groovy
@@ -1,4 +1,5 @@
 import jenkins.model.*
+import java.util.TimeZone
 
 // conditional stage
 def condStage(Map args, Closure block) {
@@ -47,35 +48,55 @@ def runOnSeleniumGrid(
     String siteEnv,
 
     String[] browsers,
-    Integer chuckSize,
     String seleniumServer,
+
     Integer concurrency,
-    Integer maxRetry
+    Integer maxRetry,
+    Integer chuckSize
 ) {
     if (!browsers && !nodeName)
         return
     node (nodeName) {
         // install nodejs tool
-        env.NODEJS_HOME = tool nodejsTool
+        env.NODEJS_HOME = tool "v10.9.0"
         env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+
+        // global env
         env.TZ='UTC-8'
+
+        sh 'env'
 
         // stage 1: checkout stage
         checkoutStage('selenium-grid', gitUrl, gitBranch, gitCredentialId)
-        // stage 2: e2e automation (no exception should be thrown in this stage)
-        dir('tests/e2e/testcafe') {
+
+        // stage 2: e2e automation
+        dir('tests/e2e/testcafe') { withEnv(["ENABLE_REMOTE_DASHBOARD=true"]) {
+            // install dependencies
             sh 'npm install --unsafe-perm --cache-min=9999'
+
+            // screenshot path for allure report
             sh 'mkdir -p ./screenshots'
-            List<List<String>> fixtureChunks = sh(returnStdout: true, script: 'cat lint/*.txt').split('\n').collect{ it.trim() }.collate(chuckSize)
+
+            // create an report id for beat dashboard
+            String localDate = new Date().format("yyyy-MM-dd", TimeZone.getTimeZone("Asia/Shanghai"))
+            withEnv([ "RUN_NAME=[Jupiter][Daily][${gitBranch}][${siteUrl}][${localDate}]", ]) {
+                sh 'npx ts-node create-run-id.ts'
+            }
+
+            // todo: log this info to description
+            echo sh(returnStdout: true, script: 'cat reportUrl || true').trim()
+
+            // split fixtures into small chucks and group by check size
+            List<List<String>> fixtureChunks = sh(returnStdout: true, script: 'npx ts-node ./scripts/ls-fixtures.ts').split('\n').collect{ it.trim() }.collate(chuckSize)
 
             Map tasks = [:]
+            // split tasks by browsers level
             browsers.each { browser ->
                 parallelTasks[browser] = {
                     condStage(name: "selenium-grid: e2e ${browser}") {
                         // the reason we split fixtures into small chunk is there is a chance that browser connection is lost during execution
                         // in order to reduce the effect of this cases
                         fixtureChunks.each { fixtures ->
-                            String date = new Date().format("yyyy-MM-dd", timezone=TimeZone.getTimeZone("Asia/Shanghai"))
                             withEnv([
                                 "FIXTURES=${fixtures.join(',')}",
                                 "SITE_URL=${siteUrl}",
@@ -84,16 +105,18 @@ def runOnSeleniumGrid(
                                 "SELENIUM_SERVER=${seleniumServer}",
                                 "CONCURRENCY=${concurrency}",
                                 "SCREENSHOTS_PATH=./screenshots",
-                                "ENABLE_REMOTE_DASHBOARD=true",
                                 "DEBUG_MODE=false",
                                 "SCREENSHOT_WEBP_QUALITY=80",
-                                "RUN_NAME=[Jupiter][Daily][${date}]",
                             ]) {
+                                // print env for debug
+                                sh 'env'
+                                // retry until success if the error is cause by lost connection
                                 for (int i = 0; i < maxRetry; i++) {
                                     Integer status = sh(returnStatus: true, script: 'npm run e2e')
-                                    if (! (status in [1, 7]))
+                                    echo "exit code is ${status}"
+                                    if (! (status in [1, 7]))  // status code of lost connection
                                         break
-                                    sleep 60 * 5
+                                    sleep 60 // retry after 1 min
                                 }
                             }
                         }
@@ -101,14 +124,14 @@ def runOnSeleniumGrid(
                 }
             }
             parallel tasks
-        }
+        }}
         // post execution: archive report
+        // fixme: following code is not portable because allure and java should pre-install on the node
         allure(
             commandline: 'allure-2.7',
             includeProperties: false,
             results: [[ path: 'tests/e2e/testcafe/allure/allure-results' ]]
         )
-        // post execution:
     }
 }
 
@@ -116,9 +139,11 @@ def runOnSeleniumGrid(
 
 
 // start to build
-String seleniumNode = params.SELENIUM_NODE
-String winElectronNode = params.WIN_ELECTRON_NODE
-String macElectronNode = params.MAC_ELECTRON_NODE
+
+// fixme: it seems like node type can only be fetch from env, I don't know why
+String seleniumNode = params.SELENIUM_NODE || env.SELENIUM_NODE
+// String winElectronNode = params.WIN_ELECTRON_NODE
+// String macElectronNode = params.MAC_ELECTRON_NODE
 
 String gitUrl = params.GIT_URL
 String gitBranch = params.GIT_BRANCH
@@ -127,13 +152,21 @@ String gitCredentialId = params.GIT_CREDENTIAL
 String siteUrl = params.SITE_URL
 String siteEnv = params.SITE_ENV
 
-String[] browsers = params.BROWSERS.split(',').collect{ it.trim() }
-String[] seleniumBrowsers = browsers.findAll{ it.startsWith('selenium:')}
 
+String[] browsers = params.BROWSERS.split('\n').collect{ it.trim() }
+String[] seleniumBrowsers = browsers.findAll{ it.startsWith('selenium:')}
+String seleniumServer = params.SELENIUM_SERVER
+
+// todo: fixtures, tags should also be configurable
 
 Map tasks = [
     'selenium-grid': {
-        runOnSeleniumGrid(seleniumNode, gitUrl, gitBranch, gitCredentialId, siteUrl, siteEnv, seleniumBrowsers)
+        runOnSeleniumGrid(
+            seleniumNode,
+            gitUrl, gitBranch, gitCredentialId,
+            siteUrl, siteEnv,
+            seleniumBrowsers, seleniumServer,
+            2, 2, 5)
     },
 
     'electron-mac': {
@@ -146,84 +179,3 @@ Map tasks = [
 ]
 
 parallel(tasks)
-
-
-
-
-
-
-
-
-
-
-
-
-
-node('SDET-XMN054'){
-    try{
-        stage("run testcafe test"){
-            sh '''
-            git clean -xdf
-            cd $WORKSPACE/tests/e2e/testcafe
-            npm install
-            '''
-        }
-        stage("parallel browsers"){
-            def str = params.BROWSERS
-            def tasks = [:]
-            def aaa = str.split(",")
-            for (int i = 0; i < aaa.length; i++) {
-                def t = i*5
-                def browser = aaa[i]
-                def cmd = '''cd tests/e2e/testcafe/
-                        BROWSERS="'''+browser+'''" npm run e2e'''
-                tasks[aaa[i]] = {
-                    stage("run e2e") {
-                        sleep t
-                        timeout(time:45, unit: 'MINUTES', activity: true) {
-                            def status = 0
-                            for (int j = 0; j < browser_error_max_retry.toInteger(); j++ ){
-                                status = sh returnStatus: true, script: cmd
-                                if (status == 1 || status == 7) {
-                                    sleep 300
-                                    continue
-                                }
-                                else {
-                                    break
-                                }
-                            }
-                            if (status) {
-                                currentBuild.result = 'FAILED'
-                            }
-                        }
-                    }
-                }
-            }
-            parallel(tasks)
-        }
-    }
-    finally{
-            stage('Archive Results'){
-                allure commandline: 'allure-2.7',includeProperties: false, jdk: '', results: [[path: 'tests/e2e/testcafe/allure/allure-results']]
-            }
-            stage("post"){
-                if (params.QUIET_MODE == "false"){
-                    def mark = ':no_entry_sign:'
-                    if (currentBuild.result == "SUCCESS" | currentBuild.result == null){
-                        mark = ':white_check_mark:'
-                    }
-                    emailext body: '''
-$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!
-
-> **Browsers**: $BROWSERS
-> **Results**: ${BUILD_LOG_REGEX,regex=".*/.* failed .*",maxMatches=0,showTruncatedLines=false}
-> **Status** : '''+mark+''' ${BUILD_STATUS}
-> **Details**: ${BUILD_URL}console
-> **Allure Report**: ${BUILD_URL}allure
-''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: 'jupiter_develop_ci@ringcentral.glip.com, test_message@ringcentral.glip.com'
-                }else{
-                    echo "quiet mode, no email"
-                }
-            }
-        }
-    }
