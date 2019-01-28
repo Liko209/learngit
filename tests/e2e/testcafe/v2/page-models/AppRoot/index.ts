d35d6583ffffb6c748121cecc514b189a9951cf2
Henry Xu <henry.xu@ringcentral.com>
2019-01-28 16:56:56 +0800

    test(FIJI-3216): [fix failing cases] provide an application wide spinner waiter

--- tests/e2e/testcafe/v2/page-models/AppRoot/index.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/index.ts
@@ -1,8 +1,11 @@
 import 'testcafe';
+import * as assert from 'assert';
+
 import { BaseWebComponent } from '../BaseWebComponent';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import { SITE_URL } from '../../../config';
+import { H } from '../../helpers';
 
 export class AppRoot extends BaseWebComponent {
   async ensureLoaded() { }
@@ -19,12 +22,27 @@ export class AppRoot extends BaseWebComponent {
     return this.getComponent(HomePage);
   }
 
+  get spinners() {
+    return this.getSelector('div[role="progressbar"]');
+  }
+
   get pagePath() {
     return this.t.eval(() => window.location.pathname);
   }
 
   async reload() {
     await this.t.eval(() => location.reload(true));
+  }
+
+  async waitForAllSpinnersToDisappear(timeout: number = 30e3) {
+    // this is an application-wide spinner waiter
+    try {
+      await H.retryUntilPass(async () => assert(await this.spinners.count > 0), 4);
+    } catch (e) {
+      // it's ok if spinner doesn't exist
+    } finally {
+      await this.t.expect(this.spinners.count).eql(0, { timeout });
+    }
   }
 
   async openConversationByUrl(groupId: number | string) {
