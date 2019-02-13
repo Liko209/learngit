b9aa040a4f36a78ba5da240c57bb97ddf6422c63
Henry Xu <henry.xu@ringcentral.com>
2019-02-13 20:29:02 +0800

    test(FIJI-3360): [Changes made to teams should be reflected on instant search result dropdown] implement script

--- tests/e2e/testcafe/v2/sdk/platform.ts
+++ tests/e2e/testcafe/v2/sdk/platform.ts
@@ -21,7 +21,6 @@ export class RcPlatformSdk {
         await this.refresh();
         return true;
       }
-      logger.error('error response', err.response);
       return false;
     });
   }
@@ -40,7 +39,7 @@ export class RcPlatformSdk {
     return await this.init();
   }
 
-  async createExtension(data: object, accountId: string = '~') {
+  async createExtension(data: any, accountId: string = '~') {
     const url = `restapi/v1.0/account/${accountId}/extension`;
     return await this.retryRequestOnException(async () => {
       return await this.sdk.post(url, data);
@@ -59,21 +58,21 @@ export class RcPlatformSdk {
     return await this.createPost(data, groupId);
   }
 
-  async createGroup(data: object) {
+  async createGroup(data: any) {
     const url = 'restapi/v1.0/glip/groups';
     return await this.retryRequestOnException(async () => {
       return await this.sdk.post(url, data);
     });
   }
 
-  async createTeam(data: object) {
+  async createTeam(data: any) {
     const url = 'restapi/v1.0/glip/teams';
     return await this.retryRequestOnException(async () => {
       return await this.sdk.post(url, data);
     });
   }
 
-  async updateTeam(data: object, chatId: string) {
+  async updateTeam(data: any, chatId: string) {
     const url = `restapi/v1.0/glip/teams/${chatId}`;
     return await this.retryRequestOnException(async () => {
       return await this.sdk.patch(url, data);
@@ -93,22 +92,38 @@ export class RcPlatformSdk {
       return await this.sdk.post(url);
     });
   }
-  async addTeamMember(data: object, chatId: string) {
+
+  async addTeamMember(data: any, chatId: string) {
+    return await this.editGroupMembers({
+      addedPersonIds: data.map(member => member.id),
+    }, chatId);
+    // fixme: using following code after glpa upgrade to latest
     const url = `restapi/v1.0/glip/teams/${chatId}/add`;
     return await this.retryRequestOnException(async () => {
       return await this.sdk.post(url, data);
     });
   }
 
-  async removeTeamMember(data: object, chatId: string) {
+  async removeTeamMember(data: any, chatId: string) {
+    return await this.editGroupMembers({
+      removedPersonIds: data.map(member => member.id),
+    }, chatId);
+    // fixme: using following code after glpa upgrade to latest
     const url = `restapi/v1.0/glip/teams/${chatId}/remove`;
     return await this.retryRequestOnException(async () => {
       return await this.sdk.post(url, data);
     });
   }
 
+  async editGroupMembers(data: any, chatId: string) {
+    const url = `restapi/v1.0/glip/groups/${chatId}/bulk-assign`;
+    return await this.retryRequestOnException(async () => {
+      return await this.sdk.post(url, data);
+    });
+  }
+
   // deprecated
-  async createAndGetGroupId(data: object) {
+  async createAndGetGroupId(data: any) {
     return await this.createGroup(data).then(res => res.data.id);
   }
 
