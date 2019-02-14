2f04d2ad0ee9230cf189212421a9650b6995db70
Shining <shining.miao@ringcentral.com>
2019-02-14 14:20:30 +0800

    feat(FIJI-2707): [Prompt user if user refreshes page while uploading file(s)] clean draft after send post

--- application/src/containers/ConversationPage/MessageInput/MessageInput.ViewModel.ts
+++ application/src/containers/ConversationPage/MessageInput/MessageInput.ViewModel.ts
@@ -113,6 +113,14 @@ class MessageInputViewModel extends StoreViewModel<MessageInputProps>
     });
   }
 
+  cleanDraft = () => {
+    this._groupConfigService.updateDraft({
+      draft: '',
+      id: this._oldId,
+      attachment_item_ids: [],
+    });
+  }
+
   @computed
   get _group() {
     return getEntity<Group, GroupModel>(ENTITY_NAME.GROUP, this.id);
@@ -180,11 +188,7 @@ class MessageInputViewModel extends StoreViewModel<MessageInputProps>
 
   private async _sendPost(content: string, ids: number[]) {
     this.contentChange('');
-    this._groupConfigService.updateDraft({
-      draft: '',
-      id: this._oldId,
-      attachment_item_ids: [],
-    });
+    this.cleanDraft();
     const items = this.items;
     try {
       let realContent: string = content;
