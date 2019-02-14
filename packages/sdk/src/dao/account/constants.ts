818148d0dd601ee0e3e2874eb0f150b59e2a885d
Steven Zhuang <steven.zhuang@ringcentral.com>
2019-02-14 15:18:58 +0800

    feat(FIJI-3269): [The user of other tab did not sign out when the user login in two browser tabs and sign out on one of the tab] Reload by monitoring the StorageEvent.

--- packages/sdk/src/dao/account/constants.ts
+++ packages/sdk/src/dao/account/constants.ts
@@ -4,12 +4,15 @@
  * @Last Modified by: Valor Lin (valor.lin@ringcentral.com)
  * @Last Modified time: 2018-09-03 16:11:54
  */
+
+export const ACCOUNT_COLLECTION_NAME = 'account';
 export const ACCOUNT_USER_ID = 'user_id';
 export const ACCOUNT_PROFILE_ID = 'profile_id';
 export const ACCOUNT_COMPANY_ID = 'company_id';
 export const ACCOUNT_CLIENT_CONFIG = 'client_config';
 export const ACCOUNT_CONVERSATION_LIST_LIMITS = 'max_conversations';
 export const UNREAD_TOGGLE_ON = 'unread_toggle_on';
+export const ACCOUNT_USER_ID_KEY = `${ACCOUNT_COLLECTION_NAME}/${ACCOUNT_USER_ID}`;
 export const ACCOUNT_KEYS = [
   ACCOUNT_USER_ID,
   ACCOUNT_PROFILE_ID,
