0bd291956c3fcc4a164cfc8a2d9a621a943faec1
Lip Wang <lip.wang@ringcentral.com>
2019-02-14 15:53:36 +0800

    feat(FIJI-3478): [Refactor Profile Service] fixed ts lint issue

--- packages/sdk/src/module/profile/service/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: Lip Wang (lip.wang@ringcentral.com)
- * @Date: 2019-02-12 13:19:10
- * Copyright © RingCentral. All rights reserved.
- */
-export { ProfileService as default } from './profileService';
-export { ProfileService } from './profileService';
