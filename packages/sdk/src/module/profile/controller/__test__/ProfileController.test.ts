76cbf9b6058b8cb2e2297065a4de4fd6f53aff2b
vicky.zhu <Vicky.Zhu@ringcentral.com>
2019-02-14 13:16:38 +0800

    feat(FIJI-3478): [Refactor Profile Service] add ProfileDataController & ProfileController UT

---
+++ packages/sdk/src/module/profile/controller/__test__/ProfileController.test.ts
@@ -0,0 +1,61 @@
+/*
+ * @Author: Lip Wang (lip.wangn@ringcentral.com)
+ * @Date: 2018-03-01 10:49:44
+ */
+import { ProfileController } from '../ProfileController';
+import { Api } from '../../../../api';
+import { MockEntitySourceController } from './MockEntitySourceController';
+// import { daoManager, ProfileDao } from '../../../../dao';
+import {
+  buildEntitySourceController,
+  buildPartialModifyController,
+  buildRequestController,
+} from '../../../../framework/controller';
+
+jest.mock('../../../../framework/controller');
+jest.mock('../../../../api');
+jest.mock('../../../../dao');
+
+describe('ProfileController', () => {
+  let profileController: ProfileController;
+  let mockEntitySourceController: MockEntitySourceController;
+  // const profileDao: ProfileDao = new ProfileDao(null);
+
+  beforeEach(() => {
+    // jest.spyOn(daoManager, 'getDao').mockReturnValue(profileDao);
+
+    mockEntitySourceController = new MockEntitySourceController();
+    profileController = new ProfileController(mockEntitySourceController);
+
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getProfileActionController()', () => {
+    it('should call partial modify controller', async () => {
+      Object.assign(Api, {
+        glipNetworkClient: null,
+      });
+
+      buildPartialModifyController.mockImplementationOnce(() => {
+        return undefined;
+      });
+
+      buildRequestController.mockImplementationOnce(() => {
+        return undefined;
+      });
+
+      buildEntitySourceController.mockImplementationOnce(() => {
+        return undefined;
+      });
+
+      const result = profileController.getProfileActionController();
+      expect(result instanceof ProfileController).toBe(true);
+      expect(buildEntitySourceController).toBeCalledTimes(1);
+      expect(buildPartialModifyController).toBeCalledTimes(1);
+      expect(buildRequestController).toBeCalledTimes(1);
+    });
+  });
+});
