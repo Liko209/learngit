6876cec81067ad369535f969ef33df2aa87d8a61
isaac.liu <isaac.liu@ringcentral.com>
2019-02-15 16:24:14 +0800

    fix(FIJI-3025): [The image preview should be retained after user switch between tabs/conversations or refreshes page] use delay hoc.

--- application/src/containers/common/PreloadImg/PreloadImg.tsx
+++ application/src/containers/common/PreloadImg/PreloadImg.tsx
@@ -3,8 +3,9 @@
  * @Date: 2019-01-24 13:35:29
  * Copyright © RingCentral. All rights reserved.
  */
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { JuiFade } from 'jui/foundation/Transitions';
+import { withDelay } from 'jui/hoc/withDelay';
 
 type PreloadImgProps = {
   url?: string;
@@ -15,61 +16,42 @@ type PreloadImgProps = {
 type PreloadImgState = {
   loaded: boolean;
   isError: boolean;
-  showPlaceholder: boolean;
 };
 
 const cacheUrl = {};
 const DELAY_SHOW_PLACEHOLDER_TIME = 500;
 
+const DelayWrapper = withDelay(Fragment);
+
 class PreloadImg extends Component<PreloadImgProps, PreloadImgState> {
-  private _delayTimer: NodeJS.Timeout;
   constructor(props: PreloadImgProps) {
     super(props);
     this.state = {
       loaded: false,
       isError: false,
-      showPlaceholder: false,
     };
-  }
-
-  clearDelayTimer = () => {
-    if (this._delayTimer) {
-      clearTimeout(this._delayTimer);
-    }
-  }
-
-  componentWillMount() {
-    this._delayTimer = setTimeout(() => {
-      this.setState({ showPlaceholder: true });
-    },                            DELAY_SHOW_PLACEHOLDER_TIME);
-  }
-
-  componentWillUnMount() {
-    this.clearDelayTimer();
   }
 
   handleLoad = () => {
     const { url } = this.props;
-    this.setState({ loaded: true, showPlaceholder: true });
     if (url) cacheUrl[url] = true;
-    this.clearDelayTimer();
+    this.setState({ loaded: true });
   }
 
   handleError = () => {
     const { url } = this.props;
     if (url) {
-      this.setState({ isError: true, loaded: true, showPlaceholder: true });
-      this.clearDelayTimer();
+      this.setState({ isError: true, loaded: true });
     }
   }
 
   render() {
     const { children, placeholder, url } = this.props;
-    const { loaded, isError, showPlaceholder } = this.state;
+    const { loaded, isError } = this.state;
 
     if (loaded && !isError) {
       return (
-        <JuiFade in={true} timeout={1500}>
+        <JuiFade in={true} timeout={700}>
           {children}
         </JuiFade>
       );
@@ -81,13 +63,17 @@ class PreloadImg extends Component<PreloadImgProps, PreloadImgState> {
 
     return (
       <>
-        <img
-          src={url}
-          onLoad={this.handleLoad}
-          onError={this.handleError}
-          style={{ display: 'none' }}
-        />
-        {showPlaceholder && placeholder}
+        {url && (
+          <img
+            src={url}
+            onLoad={this.handleLoad}
+            onError={this.handleError}
+            style={{ display: 'none' }}
+          />
+        )}
+        <DelayWrapper delay={DELAY_SHOW_PLACEHOLDER_TIME}>
+          {placeholder}
+        </DelayWrapper>
       </>
     );
   }
