f5d22cb16a2941371764bacb8558bfda1fa0f421
potar.he <potar.he@ringcentral.com>
2019-02-15 18:38:08 +0800

    test(FIJI-3226): [[e2e] Restrict non admin team members from pinning posts [create team entry]] implemented JPT-1082

--- tests/e2e/testcafe/fixtures/CreateTeam/PinPost.ts
+++ tests/e2e/testcafe/fixtures/CreateTeam/PinPost.ts
@@ -2,7 +2,7 @@
  * @Author: Potar.He 
  * @Date: 2019-02-15 16:37:06 
  * @Last Modified by: Potar.He
- * @Last Modified time: 2019-02-15 18:35:05
+ * @Last Modified time: 2019-02-15 18:37:49
  */
 
 
@@ -22,7 +22,6 @@ fixture('TeamSetting/PinPost')
 test(formalName(`Restrict non admin team members from pinning posts in Create team dialog`, ['P2', 'JPT-1082', 'PinPost', 'Potar.He']), async t => {
   const app = new AppRoot(t);
   const adminUser = h(t).rcData.mainCompany.users[4];
-  const memberUser = h(t).rcData.mainCompany.users[5];
 
   await h(t).platform(adminUser).init();
   await h(t).glip(adminUser).init();
@@ -96,7 +95,7 @@ test(formalName(`Restrict non admin team members from pinning posts in Create te
     await createTeamModal.clickCreateButton();
   });
 
-  await h(t).withLog(`Then The sending post status should on and  pinning post should off (via API).`, async () => {
+  await h(t).withLog(`Then The sending post status should on and pinning post should off (via API).`, async () => {
     await H.retryUntilPass(async () => {
       const teamId = await teamSection.conversationEntryByName(teamName2).groupId;
       const userPermissionsValue = await h(t).glip(adminUser).getGroup(teamId).then(res => res.data.permissions.user.level);
@@ -105,4 +104,37 @@ test(formalName(`Restrict non admin team members from pinning posts in Create te
     });
   });
 
+  await h(t).withLog('When I open Create Team in AddActionMenu', async () => {
+    await app.homePage.openAddActionMenu();
+    await app.homePage.addActionMenu.createTeamEntry.enter();
+    await createTeamModal.ensureLoaded();
+  });
+
+  await h(t).withLog(`Then Display Post& Pin toggle (Enable status)`, async () => {
+    await t.expect(createTeamModal.mayPostMessageToggle.checked).ok();
+    await t.expect(createTeamModal.mayPinPostToggle.checked).ok();
+  });
+
+  await h(t).withLog(`When I turn on Post Message toggle`, async () => {
+    await createTeamModal.turnOnMayPostMessage();
+  });
+
+  await h(t).withLog(`Then The pinning toggle should be able to turn on/off.`, async () => {
+    await t.expect(createTeamModal.mayPinPostToggle.hasAttribute('disabled')).notOk();
+  });
+
+  await h(t).withLog(`When admin turn on the pinging toggle and create team`, async () => {
+    await createTeamModal.typeTeamName(teamName2);
+    await createTeamModal.turnOnMayPinPost();
+    await createTeamModal.clickCreateButton();
+  });
+
+  await h(t).withLog(`Then The sending post and pinning post status should on (via API).`, async () => {
+    await H.retryUntilPass(async () => {
+      const teamId = await teamSection.conversationEntryByName(teamName2).groupId;
+      const userPermissionsValue = await h(t).glip(adminUser).getGroup(teamId).then(res => res.data.permissions.user.level);
+      assert.ok(1 << 0 & Number(userPermissionsValue), `permission value Error: ${userPermissionsValue}`);
+      assert.ok(1 << 3 & Number(userPermissionsValue), `permission value Error: ${userPermissionsValue}`);
+    });
+  });
 });
