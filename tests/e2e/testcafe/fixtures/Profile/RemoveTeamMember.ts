9f95e3b8360187ea3d3ee8accbadd00224628ff4
mia.cai <mia.cai@ringcentral.com>
2019-02-18 12:53:17 +0800

    feat(FIJI-3356): [[E2E]Change Team Admin] resolve conflict with develop

--- tests/e2e/testcafe/fixtures/Profile/RemoveTeamMember.ts
+++ tests/e2e/testcafe/fixtures/Profile/RemoveTeamMember.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: Potar.He 
  * @Date: 2019-02-14 16:15:37 
- * @Last Modified by: Potar.He
- * @Last Modified time: 2019-02-14 18:26:18
+ * @Last Modified by: 
+ * @Last Modified time: 2019-02-18 12:51:11
  */
 import * as assert from 'assert';
 import * as _ from 'lodash';
@@ -65,11 +65,20 @@ test(formalName('Only admin has the ability to remove members from the team', ['
     await t.hover(profileDialog.memberEntryByName(adminName1).self);
   });
 
-  await h(t).withLog('Then No "More" menu options', async () => {
-    await t.expect(profileDialog.memberEntryByName(adminName1).moreButton.exists).notOk();
+  await h(t).withLog('Then Show "more" button', async () => {
+    await t.expect(profileDialog.memberEntryByName(adminName1).moreButton.exists).ok();
+  }, true);
+  
+  await h(t).withLog('When I click the more button', async () => {
+    await t.click(profileDialog.memberEntryByName(adminName1).moreButton);
+  });
+
+  await h(t).withLog(`And no ${removeFromTeamText} button`, async () => {
+    await t.expect(profileDialog.memberMoreMenu.removeFromTeamItem.withExactText(removeFromTeamText).exists).notOk();
   }, true);
 
   await h(t).withLog('When admin1 hover admin2 row in members list', async () => {
+    await profileDialog.memberMoreMenu.quit();
     await t.hover(profileDialog.memberEntryByName(adminName2).self);
   });
 
@@ -131,7 +140,7 @@ test(formalName('Only admin has the ability to remove members from the team', ['
 
 });
 
-test(formalName('The remove team member permission should sync dynamically', ['JPT-1086', 'P1', 'RemoveTeamMember', 'Potar.he']), async (t) => {
+test.only(formalName('The remove team member permission should sync dynamically', ['JPT-1086', 'P1', 'RemoveTeamMember', 'Potar.he']), async (t) => {
   const app = new AppRoot(t);
 
   const users = h(t).rcData.mainCompany.users
@@ -195,7 +204,7 @@ test(formalName('The remove team member permission should sync dynamically', ['J
       await t.hover(profileDialog.nthMemberEntry(i).self);
     });
 
-    await h(t).withLog('Then No "More" menu options', async () => {
+    await h(t).withLog('Then no "more" button', async () => {
       await t.expect(profileDialog.nthMemberEntry(i).moreButton.exists).notOk();
     }, true);
   }
