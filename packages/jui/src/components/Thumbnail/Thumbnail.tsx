4eace8100c3085891656ecefb59879374400fdd4
wayne.zhou <wayne.zhou@ringcentral.com>
2019-02-21 13:31:36 +0800

    feat(FIJI-3652): [use svg icon] imporove type def

--- packages/jui/src/components/Thumbnail/Thumbnail.tsx
+++ packages/jui/src/components/Thumbnail/Thumbnail.tsx
@@ -7,31 +7,35 @@ import React from 'react';
 import styled from 'styled-components';
 import { width, shape } from '../../foundation/utils/styles';
 import { JuiIconography } from '../../foundation/Iconography';
-import { Omit } from '../../foundation/utils/typeHelper';
 
 type JuiThumbnailProps = {
   size?: 'small' | 'large';
-  url?: string;
-  iconType: string;
   style?: React.CSSProperties;
 };
+
+type JuiThumbnailWithUrlProps = {
+  url: string;
+} & JuiThumbnailProps;
+
+type JuiThumbnailWithIconProps = {
+  iconType: string;
+} & JuiThumbnailProps;
 
 const WrappedMuiIcon = ({
   iconType,
   size,
-  url,
   ...rest
-}: JuiThumbnailProps & { children: string }) => <JuiIconography {...rest} />;
+}: JuiThumbnailWithIconProps & { children: string }) => (
+  <JuiIconography {...rest} />
+);
 
-const StyledIcon = styled(WrappedMuiIcon)<JuiThumbnailProps>`
+const StyledIcon = styled(WrappedMuiIcon)`
   && {
     font-size: ${({ size }) => (size === 'small' ? width(5) : width(9))};
   }
 `;
 
-const StyledModifyImage = styled<Omit<JuiThumbnailProps, 'iconType'>, 'span'>(
-  'span',
-)`
+const StyledModifyImage = styled<JuiThumbnailWithUrlProps, 'span'>('span')`
   width: ${({ size }) => (size === 'small' ? width(5) : width(9))};
   height: ${({ size }) => (size === 'small' ? width(5) : width(9))};
   border-radius: ${({ size }) =>
@@ -42,16 +46,19 @@ const StyledModifyImage = styled<Omit<JuiThumbnailProps, 'iconType'>, 'span'>(
   background-position: center;
 `;
 
-class JuiThumbnail extends React.PureComponent<JuiThumbnailProps> {
+class JuiThumbnail extends React.PureComponent<
+  JuiThumbnailWithUrlProps | JuiThumbnailWithIconProps
+> {
   render() {
-    const { size, url, iconType, style } = this.props;
+    const { size, style, url, iconType } = this
+      .props as JuiThumbnailWithIconProps & JuiThumbnailWithUrlProps;
 
     return (
       <>
         {url ? (
           <StyledModifyImage url={url} style={style} />
         ) : (
-          <StyledIcon size={size} {...this.props}>
+          <StyledIcon size={size} iconType={iconType}>
             {iconType}
           </StyledIcon>
         )}
