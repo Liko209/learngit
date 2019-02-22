4fb2aa0383f916b275acdb0b7d97e495fa0fabae
rito.xiao <rito.xiao@ringcentral.com>
2019-02-22 17:19:14 +0800

    feat(FIJI-3137): [RC logged in API support] extract v1.0 as const

--- packages/sdk/src/api/ringcentral/constants.ts
+++ packages/sdk/src/api/ringcentral/constants.ts
@@ -4,14 +4,16 @@
  * Copyright © RingCentral. All rights reserved.
  */
 
+const API_VERSION = 'v1.0';
+
 const RINGCENTRAL_API = {
   API_OAUTH_TOKEN: '/oauth/token',
   API_REFRESH_TOKEN: '/oauth/token',
-  API_GENERATE_CODE: '/v1.0/interop/generate-code',
+  API_GENERATE_CODE: `/${API_VERSION}/interop/generate-code`,
   API_PROFILE: '/glip/profile',
-  API_CLIENT_INFO: '/v1.0/client-info',
-  API_ACCOUNT_INFO: '/v1.0/account/~',
-  API_EXTENSION_INFO: '/v1.0/account/~/extension/~',
-  API_ROLE_PERMISSION: '/v1.0/account/~/extension/~/authz-profile',
+  API_CLIENT_INFO: `/${API_VERSION}/client-info`,
+  API_ACCOUNT_INFO: `/${API_VERSION}/account/~`,
+  API_EXTENSION_INFO: `/${API_VERSION}/account/~/extension/~`,
+  API_ROLE_PERMISSION: `/${API_VERSION}/account/~/extension/~/authz-profile`,
 };
 export { RINGCENTRAL_API };
