ad430583a1f0c3cadac8493e3f872f8152f3d7fb
Henry Xu <henry.xu@ringcentral.com>
2019-02-27 14:21:22 +0800

    test(FIJI-3856): [deploy Jupiter to aws] clean workspace

--- autoDevOps/jenkins/gitlabIntegration.groovy
+++ autoDevOps/jenkins/gitlabIntegration.groovy
@@ -227,6 +227,7 @@ node(buildNode) {
     try {
         // start to build
         stage ('Collect Facts') {
+            cleanWs()
             sh 'env'
             sh 'df -h'
             sh 'uptime'
