449cee2cb7266bedd373462f2bebedbe52b21963
Valor Lin <valor.lin@ringcentral.com>
2019-02-28 19:33:53 +0800

    chore(FIJI-3848): [[JUI] [JuiNewVirtualizedList] basic] single quote

--- scripts/test.js
+++ scripts/test.js
@@ -3,33 +3,33 @@
  * @Date: 2018-08-30 08:44:54
  * Copyright © RingCentral. All rights reserved.
  */
-"use strict";
+'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.NODE_ENV = "test";
+process.env.NODE_ENV = 'test';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on("unhandledRejection", err => {
+process.on('unhandledRejection', err => {
   throw err;
 });
 
 // Ensure environment variables are read.
-const path = require("path");
-const jest = require("jest");
-const getPackages = require("./getPackages");
+const path = require('path');
+const jest = require('jest');
+const getPackages = require('./getPackages');
 
 let argv = process.argv.slice(2);
 
 // Watch unless on CI, in coverage mode, or explicitly running all tests
 if (
   !process.env.CI &&
-  argv.indexOf("--coverage") === -1 &&
-  argv.indexOf("--watchAll") === -1 &&
-  !argv.find(value => value.indexOf("--changedSince") !== -1)
+  argv.indexOf('--coverage') === -1 &&
+  argv.indexOf('--watchAll') === -1 &&
+  !argv.find(value => value.indexOf('--changedSince') !== -1)
 ) {
-  argv.push("--watch");
+  argv.push('--watch');
 }
 
 const packages = getPackages();
@@ -43,24 +43,24 @@ if (argv.length) {
 }
 
 switch (true) {
-  case argv.includes("application"):
-    process.env.APP = "<rootDir>/application";
+  case argv.includes('application'):
+    process.env.APP = '<rootDir>/application';
     break;
-  case argv.includes("sdk"):
-    process.env.APP = "<rootDir>/packages/sdk";
+  case argv.includes('sdk'):
+    process.env.APP = '<rootDir>/packages/sdk';
     break;
-  case argv.includes("foundation"):
-    process.env.APP = "<rootDir>/packages/foundation";
+  case argv.includes('foundation'):
+    process.env.APP = '<rootDir>/packages/foundation';
     break;
-  case argv.includes("jui"):
-    process.env.APP = "<rootDir>/packages/jui";
+  case argv.includes('jui'):
+    process.env.APP = '<rootDir>/packages/jui';
     break;
-  case argv.includes("voip"):
-    process.env.APP = "<rootDir>/packages/voip";
+  case argv.includes('voip'):
+    process.env.APP = '<rootDir>/packages/voip';
     break;
   default:
     process.env.APP =
-      "<rootDir>/{application|packages/sdk|packages/foundation|packages/voip|packages/jui}";
+      '<rootDir>/{application|packages/sdk|packages/foundation|packages/voip|packages/jui}';
     break;
 }
 
