42d44b953045a729f60feba113d975f7b374245d
Jeffrey Huang <jeffrey.huang@ringcentral.com>
2019-03-04 09:46:33 +0800

    feat(FIJI-3804): [FSM to control call components] call state and call window state

--- application/src/modules/telephony/index.ts
+++ application/src/modules/telephony/index.ts
@@ -1,2 +1,9 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2019-03-01 23:13:58
+ * Copyright © RingCentral. All rights reserved.
+ */
+
 export * from './TelephonyModule';
+export * from './store';
 export * from './module.config';
--- application/src/modules/telephony/module.config.ts
+++ application/src/modules/telephony/module.config.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2019-03-01 23:14:05
+ * Copyright © RingCentral. All rights reserved.
+ */
+
 import { TelephonyModule } from './TelephonyModule';
+import { TelephonyStore } from './store';
 
 const config = {
   entry: TelephonyModule,
+  provides: { TelephonyStore },
 };
 
 export { config };
