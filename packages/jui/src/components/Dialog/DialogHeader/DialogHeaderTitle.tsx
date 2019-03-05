7ab9e9c24e428664ad75cd297e908fc4a58ce823
Chris Zhan <chris.zhan@ringcentral.com>
2019-03-05 10:11:31 +0800

    feat(FIJI-3442): [[JUI] Viewer dialog titlebar responsive] line break and responsive

--- packages/jui/src/components/Dialog/DialogHeader/DialogHeaderTitle.tsx
+++ packages/jui/src/components/Dialog/DialogHeader/DialogHeaderTitle.tsx
@@ -8,6 +8,7 @@ import styled from '../../../foundation/styled-components';
 import MuiDialogTitle, {
   DialogTitleProps as MuiDialogTitleProps,
 } from '@material-ui/core/DialogTitle';
+import RootRef from '@material-ui/core/RootRef';
 import {
   typography,
   ellipsis,
@@ -18,10 +19,75 @@ type JuiDialogHeaderTitleProps = MuiDialogTitleProps & {
   variant?: 'regular' | 'responsive';
 };
 
-const WrappedDialogTitle = ({
-  variant,
-  ...rest
-}: JuiDialogHeaderTitleProps) => <MuiDialogTitle {...rest} />;
+class WrappedDialogTitle extends React.PureComponent<
+  JuiDialogHeaderTitleProps,
+  { overflow: boolean }
+> {
+  state = {
+    overflow: false,
+  };
+
+  rootRef: React.RefObject<HTMLElement> = React.createRef();
+
+  componentDidMount() {
+    this.checkWidth();
+    window.addEventListener('resize', this.checkWidth.bind(this));
+  }
+
+  componentDidUpdate(prevProps: JuiDialogHeaderTitleProps) {
+    if (prevProps.children !== this.props.children) {
+      this.checkWidth();
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.checkWidth.bind(this));
+  }
+
+  checkWidth() {
+    if (this.rootRef.current) {
+      const h2 = this.rootRef.current.querySelector('h2');
+      if (h2) {
+        const width = Array.from(h2.children).reduce(
+          (a, b) => a + b.getBoundingClientRect().width,
+          0,
+        );
+        const overflow = width > h2.getBoundingClientRect().width;
+        this.setState({
+          overflow,
+        });
+        if (overflow) {
+          Array.from(h2.children).forEach((child: HTMLElement) => {
+            child.style.alignSelf =
+              child.getBoundingClientRect().width >
+              h2.getBoundingClientRect().width
+                ? 'normal'
+                : 'center';
+            if (
+              child.style.alignSelf === 'center' &&
+              child.getBoundingClientRect().width >
+                h2.getBoundingClientRect().width
+            ) {
+              child.style.alignSelf = 'normal';
+            }
+          });
+        }
+      }
+    }
+  }
+
+  render() {
+    const { variant, className, ...rest } = this.props;
+    const classNames = this.state.overflow
+      ? `${className} vertical`
+      : className;
+    return (
+      <RootRef rootRef={this.rootRef}>
+        <MuiDialogTitle {...rest} className={classNames} />
+      </RootRef>
+    );
+  }
+}
 const JuiDialogHeaderTitle = styled<JuiDialogHeaderTitleProps>(
   WrappedDialogTitle,
 )`
@@ -38,6 +104,15 @@ const JuiDialogHeaderTitle = styled<JuiDialogHeaderTitleProps>(
         variant === 'responsive'
           ? typography('subheading1')
           : typography('title2')};
+    }
+
+    &.vertical h2 {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      * {
+        ${ellipsis()}
+      }
     }
   }
 `;
