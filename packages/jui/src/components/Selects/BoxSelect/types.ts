9e158df589b3a983d61a3b09e34d2983c4b3a1df
Devin Lin <devin.lin@ringcentral.com>
2019-03-12 16:31:54 +0800

    feat(FIJI-4057): [[Jui] Implements the BoxSelects component] add jui BoxSelect component

---
+++ packages/jui/src/components/Selects/BoxSelect/types.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Devin Lin (devin.lin@ringcentral.com)
+ * @Date: 2019-03-12 16:10:24
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+type HeightSize = 'default' | 'large';
+
+export { HeightSize };
