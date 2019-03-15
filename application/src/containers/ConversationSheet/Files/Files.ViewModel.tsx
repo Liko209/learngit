fac83daac0692e2c6933748d1cfe5fe616d35ee4
Isaac Liu <isaac.liu@ringcentral.com>
2019-03-15 09:17:05 +0000

    fix(FIJI-3519): [[UI] Optimize thumbnail of conversation card for image upload.] get thumbnail first.

--- application/src/containers/ConversationSheet/Files/Files.ViewModel.tsx
+++ application/src/containers/ConversationSheet/Files/Files.ViewModel.tsx
@@ -27,8 +27,12 @@ import {
   ToastType,
   ToastMessageAlign,
 } from '@/containers/ToastWrapper/Toast/types';
-import { RULE } from '@/common/generateModifiedImageURL';
+import {
+  RULE,
+  generateModifiedImageURL,
+} from '@/common/generateModifiedImageURL';
 import { FileItemUtils } from 'sdk/module/item/module/file/utils';
+import { getThumbnailSize } from 'jui/foundation/utils';
 import { UploadFileTracker } from './UploadFileTracker';
 import { getThumbnailURL } from '@/common/getThumbnailURL';
 
@@ -89,13 +93,32 @@ class FilesViewModel extends StoreViewModel<FilesViewProps> {
     if (FileItemUtils.isGifItem({ type })) {
       url = versionUrl || '';
     }
+
+    const size = getThumbnailSize(origWidth, origHeight);
+    url = getThumbnailURL(item, {
+      width: size.imageWidth,
+      height: size.imageHeight,
+    });
+    // fallback to origin size url
+    if (!url) {
+      url = getThumbnailURL(item, {
+        width: origWidth,
+        height: origHeight,
+      });
+    }
     if (
       !url &&
       origWidth > 0 &&
       origHeight > 0 &&
       FileItemUtils.isSupportPreview({ type })
     ) {
-      url = getThumbnailURL(item, { width: origWidth, height: origHeight });
+      const result = await generateModifiedImageURL({
+        rule,
+        origHeight,
+        origWidth,
+        id: item.id,
+      });
+      url = result.url;
     }
     if (!url) {
       url = versionUrl || '';
