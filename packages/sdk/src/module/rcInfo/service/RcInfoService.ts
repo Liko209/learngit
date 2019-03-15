1ec2632da8611d42ff21d01997ab134e88734cdd
Lewi Li <lewi.li@ringcentral.com>
2019-03-15 05:42:11 +0000

    feat(FIJI-3990): [Telephony] [SDK]Call permissions checking

--- packages/sdk/src/module/rcInfo/service/RcInfoService.ts
+++ packages/sdk/src/module/rcInfo/service/RcInfoService.ts
@@ -8,9 +8,12 @@ import { SERVICE } from '../../../service/eventKey';
 import { SubscribeController } from '../../base/controller/SubscribeController';
 import { EntityBaseService } from '../../../framework/service/EntityBaseService';
 import { RcInfoController } from '../controller/RcInfoController';
-
+import { ERcServiceFeaturePermission } from '../types';
+import { NewGlobalConfig } from '../../../service/config';
+import { ACCOUNT_TYPE_ENUM } from '../../../authenticator/constants';
 class RcInfoService extends EntityBaseService {
   private _rcInfoController: RcInfoController;
+
   constructor() {
     super(false);
     this.setSubscriptionController(
@@ -29,6 +32,15 @@ class RcInfoService extends EntityBaseService {
 
   requestRcInfo = async () => {
     await this.rcInfoController.requestRcInfo();
+  }
+
+  isVoipCallingAvailable() {
+    return (
+      NewGlobalConfig.getAccountType() === ACCOUNT_TYPE_ENUM.RC &&
+      this._rcInfoController.isRcFeaturePermissionEnabled(
+        ERcServiceFeaturePermission.VOIP_CALLING,
+      )
+    );
   }
 }
 
