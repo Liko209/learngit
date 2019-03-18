c9d3855e9dd5cca409b859972fd74112df6ed0a6
Paynter Chen <paynter.chen@ringcentral.com>
2019-03-18 11:39:10 +0000

    Feature/fiji 4202 Full screen image viewer_reset zoom

--- packages/jui/.storybook/config.js
+++ packages/jui/.storybook/config.js
@@ -13,7 +13,7 @@ import './index.css';
 
 const ThemeDecorator = storyFn => {
   return (
-    <ThemeProvider themeName='light'>
+    <ThemeProvider themeName="light">
       <div style={{ paddingTop: '25px' }}>{storyFn()}</div>
     </ThemeProvider>
   );
