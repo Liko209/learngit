1944cfc96475317d5ed1a8526def671cf516bec2
Shining <shining.miao@ringcentral.com>
2019-03-18 09:46:12 +0800

    fix(FIJI-4111): [The draft icon always displayed when user delete the "quote" message by selecting.] fix inputMessage cannot send after pasting text

--- packages/jui/src/pattern/MessageInput/Modules/clipboard.ts
+++ packages/jui/src/pattern/MessageInput/Modules/clipboard.ts
@@ -13,13 +13,14 @@ class PlainClipboard extends Clipboard {
     e.preventDefault();
     const range = this.quill.getSelection();
     const text = e.clipboardData.getData('text/plain');
+    this.container.innerHTML = '';
     const delta = new Delta()
       .retain(range.index)
       .delete(range.length)
       .insert(text);
     const index = text.length + range.index;
     const length = 0;
-    this.quill.updateContents(delta, 'silent');
+    this.quill.updateContents(delta, 'user');
     this.quill.setSelection(index, length, 'silent');
     this.quill.scrollIntoView();
   }
