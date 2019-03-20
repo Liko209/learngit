70c05e7d642414f27e82ae00e7cfe3800c4c3973
Aaron Huo <aaron.huo@lmxmn297.rcoffice.ringcentral.com>
2019-03-20 20:22:26 +0800

    feat(FIJI-4342): [Copy update_list of people who liked a post] move computed data from View to ViewModel.

--- application/src/containers/ConversationCard/ConversationCard.View.tsx
+++ application/src/containers/ConversationCard/ConversationCard.View.tsx
@@ -123,7 +123,7 @@ export class ConversationCard extends React.Component<
             <IdsToConversationSheet itemTypeIds={itemTypeIds} postId={id} />
           )}
         </JuiConversationCardBody>
-        <Footer id={id} />
+        <Footer postId={id} />
       </JuiConversationCard>
     );
   }
