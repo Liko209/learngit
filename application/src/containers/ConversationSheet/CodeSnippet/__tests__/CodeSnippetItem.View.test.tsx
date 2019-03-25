78341ae04f34430cecf1919d4e7efe12d22fb8b2
Chris Zhan <chris.zhan@ringcentral.com>
2019-03-25 17:18:24 +0800

    fix(FIJI-4372): [The expand code snippet will be collapsed when scrolling to see history posts then go back to the code snippet] add unit test

---
+++ application/src/containers/ConversationSheet/CodeSnippet/__tests__/CodeSnippetItem.View.test.tsx
@@ -0,0 +1,77 @@
+/*
+ * @Author: Wayne Zhou (wayne.zhou@ringcentral.com)
+ * @Date: 2019-01-21 13:01:55
+ * Copyright © RingCentral. All rights reserved.
+ */
+import React from 'react';
+import { CodeSnippetView } from '../CodeSnippetItem.View';
+import copy from 'copy-to-clipboard';
+import { shallow } from 'enzyme';
+
+jest.mock('copy-to-clipboard');
+
+describe('CodeSnippetItem.View', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('handleCopy()', () => {
+    const token = Date.now().toString();
+    const props = {
+      postItem: {
+        title: '',
+        body: token,
+        mode: '',
+        mimeType: '',
+        wrapLines: false,
+      },
+    };
+    it('should call copy when called', () => {
+      const codeSnippet = new CodeSnippetView(props);
+      codeSnippet.handleCopy();
+      expect(copy).toBeCalledWith(token);
+    });
+  });
+
+  describe('Expand/Collapse', () => {
+    const token = Date.now().toString();
+    const props: any = {
+      postItem: {
+        title: '',
+        body: token,
+        mode: '',
+        mimeType: '',
+        wrapLines: false,
+      },
+    };
+    it('should get the initial value from vm on mounted', () => {
+      props.isCollapse = false;
+      let codeSnippet = shallow(<CodeSnippetView {...props} />);
+      expect(codeSnippet.state().isCollapse).toBe(false);
+
+      props.isCollapse = true;
+      codeSnippet = shallow(<CodeSnippetView {...props} />);
+      expect(codeSnippet.state().isCollapse).toBe(true);
+
+      codeSnippet.setProps({ isCollapse: false });
+      expect(codeSnippet.state().isCollapse).toBe(false);
+    });
+    it('should update state if vm updated', () => {
+      props.isCollapse = true;
+      const codeSnippet = shallow(<CodeSnippetView {...props} />);
+      expect(codeSnippet.state().isCollapse).toBe(true);
+
+      codeSnippet.setProps({ isCollapse: false });
+      expect(codeSnippet.state().isCollapse).toBe(false);
+    });
+    it('should update vm on user toggle on view', () => {
+      props.isCollapse = true;
+      props.setCollapse = jest.fn();
+      const codeSnippet = new CodeSnippetView(props);
+      codeSnippet.handleCollapse();
+      expect(props.setCollapse).toHaveBeenCalledWith(true);
+      codeSnippet.handleExpand();
+      expect(props.setCollapse).toHaveBeenCalledWith(false);
+    });
+  });
+});
