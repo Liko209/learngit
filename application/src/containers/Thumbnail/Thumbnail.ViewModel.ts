35dccffa05f0148cc547a64a289da7d0b2c4c97d
Shining <shining.miao@ringcentral.com>
2019-03-26 09:53:17 +0800

    fix(FIJI-4389): [The sent image thumbnail on right shelf will flash after user send a image] the last url when versions store id is same as the last store id

--- application/src/containers/Thumbnail/Thumbnail.ViewModel.ts
+++ application/src/containers/Thumbnail/Thumbnail.ViewModel.ts
@@ -20,54 +20,20 @@ import {
 import { Props, ViewProps } from './types';
 import { RULE } from '@/common/generateModifiedImageURL';
 
-type Size = {
-  width: number;
-  height: number;
-};
-
 class ThumbnailViewModel extends StoreViewModel<Props> implements ViewProps {
   static DEFAULT_WIDTH = 36;
   static DEFAULT_HEIGHT = 36;
   @observable
   thumbsUrlWithSize: string;
+  @observable
+  _lastStoreFileId: number;
 
   constructor(props: Props) {
     super(props);
-    this.reaction(
-      () => ({ size: this._size, id: this._id }),
-      this._getThumbsUrlWithSize,
-      {
-        fireImmediately: true,
-        equals: comparer.structural,
-      },
-    );
-  }
-
-  @computed
-  private get _size() {
-    const { origWidth, origHeight } = this.file;
-    const size: Size = {
-      width: ThumbnailViewModel.DEFAULT_WIDTH,
-      height: ThumbnailViewModel.DEFAULT_WIDTH,
-    };
-    if (origWidth && origHeight) {
-      if (origWidth > origHeight) {
-        size.width = Math.max(
-          Math.round(
-            (origWidth / origHeight) * ThumbnailViewModel.DEFAULT_WIDTH,
-          ),
-          ThumbnailViewModel.DEFAULT_WIDTH,
-        );
-      } else {
-        size.height = Math.max(
-          Math.round(
-            (origHeight / origWidth) * ThumbnailViewModel.DEFAULT_WIDTH,
-          ),
-          ThumbnailViewModel.DEFAULT_WIDTH,
-        );
-      }
-    }
-    return size;
+    this.reaction(() => ({ id: this._id }), this._getThumbsUrlWithSize, {
+      fireImmediately: true,
+      equals: comparer.structural,
+    });
   }
 
   @computed
@@ -82,76 +48,26 @@ class ThumbnailViewModel extends StoreViewModel<Props> implements ViewProps {
 
   @action
   private _getThumbsUrlWithSize = async () => {
-    // const itemService = ItemService.getInstance() as ItemService;
-
-    // const { width, height } = this._size;
-    // this._thumbsUrlWithSize = await itemService.getThumbsUrlWithSize(
-    //   this._id,
-    //   width,
-    //   height,
-    // );
-    const file = this.file;
-    const thumbnail = await getThumbnailURLWithType(
-      {
-        id: file.id,
-        type: file.type,
-        versionUrl:
-          file.versions.length && file.versions[0].url
-            ? file.versions[0].url
-            : '',
-        versions: file.versions,
-      },
-      RULE.SQUARE_IMAGE,
-    );
-    this.thumbsUrlWithSize = thumbnail.url;
+    const { id, type, versions } = this.file;
+    if (versions[0].stored_file_id !== this._lastStoreFileId) {
+      const thumbnail = await getThumbnailURLWithType(
+        {
+          id,
+          type,
+          versions,
+          versionUrl: versions.length && versions[0].url ? versions[0].url : '',
+        },
+        RULE.SQUARE_IMAGE,
+      );
+      this._lastStoreFileId = versions[0].stored_file_id;
+      this.thumbsUrlWithSize = thumbnail.url;
+    }
   }
 
   @computed get icon() {
     const file = this.file;
     return file.iconType || '';
   }
-
-  // @computed
-  // get fileTypeOrUrl() {
-  //   const file = this.file;
-  //   const thumb = {
-  //     icon: file.iconType || '',
-  //     url: '',
-  //   };
-
-  //   if (file && file.type) {
-  //     let url: string;
-  //     if (FileItemUtils.isGifItem(file)) {
-  //       url = file.versionUrl!;
-  //     } else {
-  //       url = getThumbnailURL(
-  //         {
-  //           id: file.id,
-  //           type: file.type,
-  //           versionUrl: file.versionUrl || '',
-  //           versions: file.versions,
-  //         },
-  //         this._size,
-  //       ) as string;
-  //       console.log(url, file.versions[0].stored_file_id, 'shining22222222');
-  //     }
-  //     if (
-  //       !url &&
-  //       FileItemUtils.isSupportPreview(file) &&
-  //       file.origHeight &&
-  //       file.origWidth
-  //     ) {
-  //       url = this._thumbsUrlWithSize;
-  //       console.log(url, file.versions[0].stored_file_id, 'shining333333');
-  //     }
-  //     if (!url) {
-  //       url = file.versionUrl!;
-  //       console.log(url, file.versions[0].stored_file_id, 'shining4444444');
-  //     }
-  //     thumb.url = url;
-  //   }
-  //   return thumb;
-  // }
 
   isImage(fileItem: FileItemModel) {
     const { type, previewUrl } = getFileType(fileItem);
