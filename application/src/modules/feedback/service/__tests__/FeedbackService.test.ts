815227f24f1e62b2b140971dee443aa8b1e1c33d
Paynter Chen <paynter.chen@ringcentral.com>
2019-03-29 16:45:44 +0800

    feat(FIJI-4366): [Collect user feedback info] add ut

---
+++ application/src/modules/feedback/service/__tests__/FeedbackService.test.ts
@@ -0,0 +1,55 @@
+/*
+ * @Author: Paynter Chen
+ * @Date: 2019-03-29 10:48:59
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import * as filestack from 'filestack-js';
+import JSZip from 'jszip';
+import { FeedbackService } from '../FeedbackService';
+import { LogControlManager } from 'sdk/service/uploadLogControl/logControlManager';
+jest.mock('sdk/service/uploadLogControl/logControlManager', () => {
+  const mockLogMng = {
+    getRecentLogs: jest.fn(),
+  };
+  const mock = {
+    instance: () => mockLogMng,
+  };
+  return {
+    LogControlManager: mock,
+  };
+});
+jest.mock('filestack-js', () => {
+  const mockClient = {
+    upload: jest.fn(),
+  };
+  const mock = {
+    init: () => mockClient,
+  };
+  return mock;
+});
+jest.mock('jszip', () => {
+  const mock = {
+    file: jest.fn(),
+    generateAsync: jest.fn().mockImplementation(() => {}),
+  };
+  return () => mock;
+});
+
+describe('FeedbackService', () => {
+  describe('uploadRecentLogs()', () => {
+    it('should zip logs', async () => {
+      const feedbackService = new FeedbackService();
+      const jsZip = new JSZip();
+      const filestackClient = filestack.init('');
+      const logControlManager = LogControlManager.instance();
+      logControlManager.getRecentLogs.mockReturnValue([
+        { log: { message: 'tee' } },
+      ]);
+      await feedbackService.uploadRecentLogs();
+      expect(jsZip.file).toBeCalled();
+      expect(jsZip.generateAsync).toBeCalled();
+      expect(filestackClient.upload).toBeCalled();
+    });
+  });
+});
