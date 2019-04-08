cdd3775af82e3e3ada765c2306e11f85d79a7207
Aaron Huo <aaron.huo@lmxmn297.rcoffice.ringcentral.com>
2019-04-08 12:50:29 +0800

    fix(FIJI-4799): [Images preview in the conversation stream  display blurry when uploading multiple images] crop style

--- packages/jui/src/pattern/ConversationCard/Files/PreviewImage.tsx
+++ packages/jui/src/pattern/ConversationCard/Files/PreviewImage.tsx
@@ -119,22 +119,30 @@ class JuiPreviewImage extends PureComponent<JuiPreviewImageProps> {
     const { Actions, fileName, forceSize, url, placeholder } = this.props;
     let { width, height } = this.props;
     const imageProps = {} as SizeType;
-    const imageStyle: CSSProperties = { position: 'absolute', display: 'none' };
+    const imageStyle: CSSProperties = forceSize
+      ? { display: 'none' }
+      : { position: 'absolute', display: 'none' };
+
     if (this._loaded) {
+      const { justifyHeight, justifyWidth, top, left } = this._imageInfo;
+
       if (!forceSize) {
         height = this._imageInfo.height;
         width = this._imageInfo.width;
+
+        if (justifyHeight) {
+          imageProps.height = this._imageInfo.height;
+        } else if (justifyWidth) {
+          imageProps.width = this._imageInfo.width;
+        }
+
+        imageStyle.top = top;
+        imageStyle.left = left;
       }
-      const { justifyHeight, justifyWidth, top, left } = this._imageInfo;
-      imageStyle.top = top;
-      imageStyle.left = left;
+
       imageStyle.display = 'block';
-      if (justifyHeight) {
-        imageProps.height = this._imageInfo.height;
-      } else if (justifyWidth) {
-        imageProps.width = this._imageInfo.width;
-      }
     }
+
     return (
       <>
         {!this._loaded && placeholder}
