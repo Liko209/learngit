58d2ade4fc9c2163b0a9041b253ea14764d3eef5
Conner <conner.kang@ringcentral.com>
2019-04-09 10:39:31 +0800

    feat(FIJI-4733): [Make the app version info on the login page consistent with that on the About page] add electron module

--- application/src/containers/VersionInfo/LoginVersionStatus.tsx
+++ application/src/containers/VersionInfo/LoginVersionStatus.tsx
@@ -6,9 +6,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { withTranslation, WithTranslation } from 'react-i18next';
-import { observable, action } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { observer } from 'mobx-react';
 import { fetchVersionInfo } from './helper';
+import isElectron from '@/common/isElectron';
+import { GLOBAL_KEYS } from '@/store/constants';
+import { storeManager } from '@/store';
 
 type Props = WithTranslation;
 
@@ -27,6 +30,8 @@ const Wrapper = styled.div`
 
 @observer
 class LoginVersionStatusView extends React.Component<Props> {
+  private _globalStore = storeManager.getGlobalStore();
+
   @observable versionInfo: versionInfoType = {
     deployedTime: '',
     deployedCommit: '',
@@ -43,18 +48,35 @@ class LoginVersionStatusView extends React.Component<Props> {
     this.versionInfo = await fetchVersionInfo();
   }
 
+  @computed
+  get electronVersionInfo() {
+    const electronAppVersion = this._globalStore.get(
+      GLOBAL_KEYS.ELECTRON_APP_VERSION,
+    );
+    const electronVersion = this._globalStore.get(GLOBAL_KEYS.ELECTRON_VERSION);
+
+    return {
+      electronAppVersion,
+      electronVersion,
+    };
+  }
+
   render() {
     const { t } = this.props;
     const { deployedVersion, deployedCommit, deployedTime } = this.versionInfo;
+    const { electronVersion, electronAppVersion } = this.electronVersionInfo;
 
-    const version = `${t(
-      'home.version',
-    )}: ${deployedVersion} ${deployedCommit}`;
+    let version = `${t('home.version')}: ${deployedVersion}`;
+    if (isElectron && electronVersion) {
+      version += ` (E. ${electronVersion} - ${electronAppVersion})`;
+    }
+    const lastCommit = `${t('home.lastCommit')}: ${deployedCommit}`;
     const time = `${t('home.deployedTime')}: ${deployedTime}`;
 
     return (
       <Wrapper>
         <div>{version}</div>
+        <div>{lastCommit}</div>
         <div>{time}</div>
       </Wrapper>
     );
