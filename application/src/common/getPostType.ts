11ff822776d38a7c5145e6f7043b42bef52aaebd
Andy Hu <andy.hu@ringcentral.com>
2019-04-10 16:27:49 +0800

    fix(FIJI-4893): [fix indentions] fix indentions

--- application/src/common/getPostType.ts
+++ application/src/common/getPostType.ts
@@ -1,15 +1,15 @@
- enum POST_TYPE {
+enum POST_TYPE {
   NOTIFICATION = 1,
   POST,
 }
 
- const ActivityDataKeyMappingPostType = {
+const ActivityDataKeyMappingPostType = {
   set_abbreviation: POST_TYPE.NOTIFICATION,
   members: POST_TYPE.NOTIFICATION,
 };
 
- const getPostType = (key: string) => {
+const getPostType = (key: string) => {
   return ActivityDataKeyMappingPostType[key] || POST_TYPE.POST;
 };
 
-export {POST_TYPE, getPostType, ActivityDataKeyMappingPostType }
+export { POST_TYPE, getPostType, ActivityDataKeyMappingPostType };
