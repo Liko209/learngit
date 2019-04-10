51692917f77647d41f074c5e7da78c02eee4ff96
Lex. Huang <531971909@qq.com>
2019-04-10 18:43:02 +0800

    test(FIJI-4178): [Active call_keypad] add tests

---
+++ application/src/modules/telephony/container/Keypad/__tests__/Keypad.ViewModel.test.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: Lex Huang (lex.huang@ringcentral.com)
+ * @Date: 2019-04-10 18:29:48
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import { container, decorate, injectable } from 'framework';
+import { TelephonyStore } from '../../../store';
+import { KeypadViewModel } from '../Keypad.ViewModel';
+
+decorate(injectable(), TelephonyStore);
+
+container.bind(TelephonyStore).to(TelephonyStore);
+
+let keypadViewModel: KeypadViewModel;
+
+beforeAll(() => {
+  keypadViewModel = new KeypadViewModel();
+});
+
+describe('KeypadViewModel', () => {
+  it('Should enter the keypad panel', async () => {
+    keypadViewModel.keypad();
+    expect(keypadViewModel._telephonyStore.keypadEntered).toBe(true);
+  });
+});
