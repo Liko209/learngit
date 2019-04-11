7f421be77ece5564cf8d41b0d11552bc56413b36
Conner <conner.kang@ringcentral.com>
2019-04-11 10:58:24 +0800

    feat(FIJI-3142): [UnRegister Module] update UT

--- packages/framework/src/__tests__/Jupiter.test.ts
+++ packages/framework/src/__tests__/Jupiter.test.ts
@@ -1,20 +1,17 @@
 import { Jupiter, container } from '../Jupiter';
 
-class MyService {
-  add(a: number, b: number) {
-    return a + b;
-  }
-  dispose() {
-    return 'dispose';
-  }
-}
-
 describe('Jupiter', () => {
   afterEach(() => {
     container.unbindAll();
   });
 
   describe('bindProvide()', () => {
+    class MyService {
+      add(a: number, b: number) {
+        return a + b;
+      }
+    }
+
     it('should be able to use string as key', () => {
       const MY_SERVICE = 'MySrv';
       const jupiter = new Jupiter();
@@ -33,7 +30,37 @@ describe('Jupiter', () => {
     });
   });
 
+  describe('bootstrapModule()', () => {
+    const bootstrapFun = jest.fn();
+    class MyService {
+      bootstrap() {
+        bootstrapFun();
+      }
+    }
+    it('should be able bootstrap module', async () => {
+      const MY_SERVICE = 'MySrv';
+      const jupiter = new Jupiter();
+      jupiter.bindProvide({
+        name: MY_SERVICE,
+        value: MyService,
+      });
+      expect(jupiter.get<MyService>(MY_SERVICE)).toBeInstanceOf(MyService);
+
+      const afterBootstrap = jest.fn();
+      await jupiter.bootstrapModule(MY_SERVICE, afterBootstrap);
+      expect(bootstrapFun).toBeCalled();
+      expect(afterBootstrap).toBeCalled();
+      expect(container.isBound(MY_SERVICE)).toBeTruthy();
+    });
+  });
+
   describe('unRegisterModule()', () => {
+    const disposeFun = jest.fn();
+    class MyService {
+      dispose() {
+        disposeFun();
+      }
+    }
     it('should be able unRegister module', async () => {
       const MY_SERVICE = 'MySrv';
       const jupiter = new Jupiter();
@@ -41,10 +68,11 @@ describe('Jupiter', () => {
         name: MY_SERVICE,
         value: MyService,
       });
-      expect(container.get<MyService>(MY_SERVICE)).toBeInstanceOf(MyService);
+      expect(jupiter.get<MyService>(MY_SERVICE)).toBeInstanceOf(MyService);
 
       const afterUnregisterFun = jest.fn();
       await jupiter.unRegisterModule(MY_SERVICE, afterUnregisterFun);
+      expect(disposeFun).toBeCalled();
       expect(afterUnregisterFun).toBeCalled();
       expect(container.isBound(MY_SERVICE)).toBeFalsy();
     });
