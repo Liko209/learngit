995495d72a79621517eec0ef04197ff349c1d782
Conner <conner.kang@ringcentral.com>
2019-04-12 10:23:22 +0800

    feat(FIJI-3142): [UnRegister Module] update UT

--- application/src/modules/home/store/__tests__/HomeStore.test.ts
+++ application/src/modules/home/store/__tests__/HomeStore.test.ts
@@ -7,7 +7,7 @@
 import { HomeStore, SubModuleConfig } from '../HomeStore';
 
 describe('Home Store', () => {
-  const moduleName = ['dashboard', 'messages', 'phone', 'meeting'];
+  const mockModuleName = ['dashboard', 'messages', 'phone', 'meeting'];
   const mockSubModules: SubModuleConfig[] = [
     {
       route: {
@@ -30,12 +30,26 @@ describe('Home Store', () => {
       },
     },
   ];
+  const mockSubRoutes = [
+    {
+      path: '/dashboard',
+    },
+    {
+      path: '/messages',
+    },
+    {
+      path: '/phone',
+    },
+    {
+      path: '/meeting',
+    },
+  ];
   let homeStore: HomeStore;
 
   beforeEach(() => {
     homeStore = new HomeStore();
     mockSubModules.forEach((m, i) => {
-      homeStore.addSubModule(moduleName[i], m);
+      homeStore.addSubModule(mockModuleName[i], m);
     });
   });
   afterEach(() => {
@@ -56,6 +70,32 @@ describe('Home Store', () => {
     it('should get first matched path when there is path which is not registered', () => {
       homeStore.setDefaultRouterPaths(['/settings', '/dashboard', '/messages']);
       expect(homeStore.defaultRouterPath).toEqual('/dashboard');
+    });
+  });
+
+  describe('subModuleConfigs', () => {
+    it('should get subModule configs', () => {
+      expect(homeStore.subModuleConfigs).toEqual(mockSubModules);
+    });
+  });
+
+  describe('subRoutes', () => {
+    it('should get subModule configs', () => {
+      expect(homeStore.subRoutes).toEqual(mockSubRoutes);
+    });
+  });
+
+  describe('navConfigs', () => {
+    it('should get empty navConfigs', () => {
+      expect(homeStore.navConfigs).toEqual([]);
+    });
+  });
+
+  describe('getSubModule()', () => {
+    it('should get subModule by name', () => {
+      const name = mockModuleName[0];
+      const subModule = mockSubModules[0];
+      expect(homeStore.getSubModule(name)).toEqual(subModule);
     });
   });
 });
