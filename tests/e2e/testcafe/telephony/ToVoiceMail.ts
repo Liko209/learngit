4b9da7692d5801ddf7d52502992fd2d0b0bf330b
potar.he <potar.he@ringcentral.com>
2019-04-19 13:11:15 +0800

    test(FIJI-4127): [[E2E]FIJI-4127 Incoming call_to voicemail] JPT-1495

---
+++ tests/e2e/testcafe/telephony/ToVoiceMail.ts
@@ -0,0 +1,137 @@
+/*
+ * @Author: Alexander Zaverukha (alexander.zaverukha@ringcentral.com)
+ * @Date: 2019-04-17 10:14:54
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { formalName } from '../libs/filter';
+import { h } from '../v2/helpers';
+import { setupCase, teardownCase } from '../init';
+import { AppRoot } from '../v2/page-models/AppRoot';
+import { SITE_URL, BrandTire } from '../config';
+import { ITestMeta } from '../v2/models';
+import { WebphoneSession } from '../v2/webphone/session';
+
+fixture('Telephony/ToVoiceMail')
+  .beforeEach(setupCase(BrandTire.RCOFFICE))
+  .afterEach(teardownCase());
+
+test.meta(<ITestMeta>{
+  caseIds: ['JPT-1599'],
+  priority: ['P2'],
+  maintainers: ['alexander.zaverukha'],
+  keywords: ['VoiceMail']
+})('Can should the tooltip when hovering on the to voicemail button', async (t) => {
+  const loginUser = h(t).rcData.mainCompany.users[0];
+  const caller = h(t).rcData.mainCompany.users[1];
+  const app = new AppRoot(t);
+  const tooltipText = 'Send to voicemail';
+  const callerWebPhone = await h(t).webphone(caller);
+
+  await h(t).withLog(`Given I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  });
+
+  await h(t).withLog('When I call this extension', async () => {
+    await callerWebPhone.makeCall(`${loginUser.company.number}#${loginUser.extension}`);
+  });
+
+  const telephonyDialog = app.homePage.telephonyDialog;
+  await h(t).withLog('Then telephony dialog is displayed', async () => {
+    await telephonyDialog.ensureLoaded();
+  });
+
+  await h(t).withLog('When I hover on the to voicemail button', async () => {
+    await telephonyDialog.hoverSendToVoiceMailButton();
+  });
+
+  await h(t).withLog(`Then Display a tooltip: '${tooltipText}`, async () => {
+    await telephonyDialog.showTooltip(tooltipText);
+  });
+  await callerWebPhone.close();
+});
+
+test.meta(<ITestMeta>{
+  caseIds: ['JPT-1510'],
+  priority: ['P2'],
+  maintainers: ['ali.naffaa'],
+  keywords: ['VoiceMail']
+})('User can receive the new incoming call  when user ignored the incoming call', async (t) => {
+  const loginUser = h(t).rcData.mainCompany.users[0];
+  const caller = h(t).rcData.mainCompany.users[1];
+  const app = new AppRoot(t);
+  const loginUserWebPhone = await h(t).webphone(loginUser);
+  let callerWebPhone = undefined;
+
+  await h(t).withLog(`Given I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  });
+
+  await h(t).withLog(`And I also login in another platform (webphone) ${caller.company.number}#${caller.extension}`, async () => {
+    callerWebPhone = await h(t).webphone(caller);
+  });
+
+  const telephonyDialog = app.homePage.telephonyDialog;
+  await h(t).withLog('When I receive an in-comming call', async () => {
+    await callerWebPhone.makeCall(`${loginUser.company.number}#${loginUser.extension}`);
+  });
+
+  await h(t).withLog('And I ignore this in-comming call in Jupiter', async () => {
+    await telephonyDialog.ensureLoaded();
+    await telephonyDialog.clickIgnoreButton();
+  });
+
+  await h(t).withLog('Then this call should be ignored in Jupiter', async () => {
+    await telephonyDialog.ensureDismiss();
+  });
+
+  await h(t).withLog('But in the other platform (webPhone) this call should still ringing', async () => {
+    await loginUserWebPhone.update();
+    const actual = loginUserWebPhone.status;
+
+    await t.expect(actual).eql('invited');
+  });
+
+  await loginUserWebPhone.close();
+  await callerWebPhone.close();
+});
+
+test.meta(<ITestMeta>{
+  caseIds: ['JPT-1595'],
+  priority: ['P1'],
+  maintainers: ['ali.naffaa'],
+  keywords: ['VoiceMail']
+})('Can send to voicemail when receiving an incoming call', async (t) => {
+  const loginUser = h(t).rcData.mainCompany.users[0];
+  const caller = h(t).rcData.mainCompany.users[1];
+  const app = new AppRoot(t);
+  let callerWebPhone: WebphoneSession;
+
+  await h(t).withLog(`Given I login in another platform (webphone) ${caller.company.number}#${caller.extension}`, async () => {
+    callerWebPhone = await h(t).webphone(caller);
+  });
+
+  await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  });
+
+  const telephonyDialog = app.homePage.telephonyDialog;
+  await h(t).withLog('When I receive an in-comming call', async () => {
+    await callerWebPhone.makeCall(`${loginUser.company.number}#${loginUser.extension}`);
+  });
+
+  await h(t).withLog('And I ignore this in-comming call in Jupiter', async () => {
+    await telephonyDialog.ensureLoaded();
+    await telephonyDialog.clickSendToVoiceMailButton();
+  });
+
+  await h(t).withLog('Then the Jupiter navigate back to the view it was before the call rang', async () => {
+    await telephonyDialog.ensureDismiss();
+  });
+
+  //TODO: check The caller should hear the leave voicemail prompt
+
+  await callerWebPhone.close();
+});
