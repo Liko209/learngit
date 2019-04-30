c7359b10851d11ae520a3e18762bb571d9ddf6b8
ken.li <ken.li@ringcental.com>
2019-04-30 09:36:52 +0800

    feat(FIJI-5492): [Implement JUIEmoji] add story

---
+++ packages/jui/src/pattern/Emoji/__stories__/Emoji.story.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Jeffrey Huang(jeffrey.huang@ringcentral.com)
+ * @Date: 2018-09-13 15:50:01
+ * Copyright © RingCentral. All rights reserved.
+ */
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Emoji } from '..';
+
+storiesOf('Pattern/Emoji', module).add('Emoji Button', () => {
+  return (
+    <div>
+      <Emoji handlerIcon="emoji" />
+    </div>
+  );
+});
