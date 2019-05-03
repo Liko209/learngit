72bc1b054f36a6e0689c5141896b77e457819966
ken.li <ken.li@ringcental.com>
2019-05-03 10:26:48 +0800

    feat(FIJI-5493): [Emoji Module and integrate with MessageInput] resolve conflict

--- application/src/modules/emoji/container/index.ts
+++ application/src/modules/emoji/container/index.ts
@@ -1,10 +1,6 @@
 /*
  * @Author: ken.li
-<<<<<<< HEAD
  * @Date: 2019-05-03 00:54:11
-=======
- * @Date: 2019-04-29 15:49:12
->>>>>>> feature/FIJI-923
  * Copyright © RingCentral. All rights reserved.
  */
 
