124368004ba9eb5ebd5f3c1b9b379351c76bf54b
isaac.liu <isaac.liu@ringcentral.com>
2019-05-06 16:15:48 +0800

    fix(FIJI-5600): [Full search results dialog's minimum height is wrong] fix discussion.

--- packages/jui/src/pattern/GlobalSearch/FullSearch/FullSearch.tsx
+++ packages/jui/src/pattern/GlobalSearch/FullSearch/FullSearch.tsx
@@ -5,14 +5,14 @@
  */
 
 import styled from '../../../foundation/styled-components';
-import { spacing } from '../../../foundation/utils/styles';
+import { spacing, height } from '../../../foundation/utils/styles';
 
 const JuiFullSearch = styled.div`
   padding: ${spacing(2, 0, 0)};
   display: flex;
   flex-direction: column;
   min-height: 0;
-  height: calc(100% - 48px);
+  height: calc(100% - ${height(12)});
 `;
 
 export { JuiFullSearch };
