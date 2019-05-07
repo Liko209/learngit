aff3e0b96d4bd1ff0c0e0b25f3daeb21272956a0
Lip Wang <lip.wang@ringcentral.com>
2019-05-07 01:59:32 +0000

    feat(FIJI-5321): [Detect Sleep Mode] implementation

---
+++ packages/foundation/src/utils/checker/SleepModeDetector.ts
@@ -0,0 +1,60 @@
+/*
+ * @Author: Lip Wang (lip.wang@ringcentral.com)
+ * @Date: 2019-05-06 09:39:38
+ * Copyright © RingCentral. All rights reserved.
+ */
+
+import { HeartBeatCheck } from './HeartBeatCheck';
+import { mainLogger } from '../../log';
+
+const CHECK_CONNECTED_INTERVAL = 1000; // 1s
+const CHECK_CONNECTED_MAX_TIMEOUT = 5000; // 5s
+
+type SleepModeDetectorCallback = (interval: number) => void;
+
+const TAG = 'SleepModeDetector';
+
+class SleepModeDetector {
+  private _heartBeatCheck: HeartBeatCheck;
+  private _callbacksMap = new Map<string, SleepModeDetectorCallback>();
+  constructor() {
+    this._heartBeatCheck = new HeartBeatCheck(
+      CHECK_CONNECTED_INTERVAL,
+      CHECK_CONNECTED_MAX_TIMEOUT,
+      this._wakeUpFromSleepMode.bind(this),
+    );
+  }
+
+  public subScribe(key: string, callback: SleepModeDetectorCallback) {
+    mainLogger.info(TAG, ' subScribe:', key);
+    if (this._callbacksMap.has(key)) {
+      mainLogger.info(`SleepModeDetector subscribe, key:${key} has existed`);
+      return false;
+    }
+    this._callbacksMap.set(key, callback);
+    return true;
+  }
+
+  public unSubscribe(key: string) {
+    mainLogger.info(TAG, ' unSubscribe:', key);
+    if (this._callbacksMap.has(key)) {
+      this._callbacksMap.delete(key);
+    }
+  }
+
+  public cleanUp() {
+    this._heartBeatCheck && this._heartBeatCheck.cleanUp();
+    delete this._heartBeatCheck;
+    this._callbacksMap.clear();
+  }
+
+  private _wakeUpFromSleepMode(interval: number) {
+    mainLogger.info(TAG, ' _wakeUpFromSleepMode:', interval);
+    this._callbacksMap.forEach((value, key, map) => {
+      value(interval);
+    });
+  }
+}
+
+const sleepModeDetector = new SleepModeDetector();
+export { sleepModeDetector, SleepModeDetector };
