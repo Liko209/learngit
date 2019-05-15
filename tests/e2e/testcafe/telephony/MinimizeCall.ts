c7c215c5a6993dc1993ffcf4ade844abbe3d1457
Naya Fang <naya.fang@ringcentral.com>
2019-05-15 09:02:02 +0800

    [[e2e]Phase I_Minimized call view-Mute&End tooltip ] implement JPT-1792, JPT-1793

--- tests/e2e/testcafe/telephony/MinimizeCall.ts
+++ tests/e2e/testcafe/telephony/MinimizeCall.ts
@@ -175,4 +175,136 @@ test.meta(<ITestMeta>{
     await telephonyDialog.keysRecordShouldBe('##')
   });
 
+test.meta(<ITestMeta>{
+    priority: ['P2'],
+    caseIds: ['JPT-1793'],
+    maintainers: ['Naya.Fang'],
+    keywords: ['MinimizedCall']
+  })(' Hover the "end" button then show the tooltip', async (t) => {
+    const users = h(t).rcData.mainCompany.users;
+    const loginUser = users[4]
+    const anotherUser = users[5];
+    const app = new AppRoot(t);
+    const tooltipText = 'End call';
+    await h(t).glip(loginUser).init();
+    await h(t).scenarioHelper.resetProfile(loginUser);
+    let chat = <IGroup>{
+      type: 'DirectMessage',
+      owner: loginUser,
+      members: [loginUser, anotherUser]
+    }
+    await h(t).withLog('Given I have a 1:1 chat', async () => {
+      await h(t).scenarioHelper.createOrOpenChat(chat);
+    });
+
+    let session: WebphoneSession;
+    await h(t).withLog('When user login webphone', async () => {
+      session = await h(t).newWebphoneSession(anotherUser);
+    });
+
+    await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+      await h(t).directLoginWithUser(SITE_URL, loginUser);
+      await app.homePage.ensureLoaded();
+    });
+
+    // conversation page header
+    const chatEntry = app.homePage.messageTab.directMessagesSection.conversationEntryById(chat.glipId);
+    await h(t).withLog('Then I open the 1:1 chat', async () => {
+      await chatEntry.enter();
+    });
+
+    const conversationPage = app.homePage.messageTab.conversationPage;
+    const telephonyDialog = app.homePage.telephonyDialog;
+    const minimizeCallWindow = app.homePage.minimizeCallWindow;
+    await h(t).withLog('And I click the call button', async () => {
+      await conversationPage.clickTelephonyButton();
+      await telephonyDialog.ensureLoaded();
+    });
+
+    await h(t).withLog('And the callee answer the call', async () => {
+      await session.answer();
+      await session.waitForStatus('accepted');
+    });
+    await h(t).withLog('And I click the “minimized”icon', async () => {
+      await telephonyDialog.clickMinimizeButton();
+    });
+    await h(t).withLog('And I hover the “end” button', async () => {
+      await minimizeCallWindow.hoverhandupButton();
+    });
+    await h(t).withLog('Then show the tooltip',async()=>{
+      await telephonyDialog.showTooltip(tooltipText);
+    });
+  })
+
+test.meta(<ITestMeta>{
+    priority: ['P2'],
+    caseIds: ['JPT-1792'],
+    maintainers: ['Naya.Fang'],
+    keywords: ['MinimizedCall']
+  })('Hover the "mute"/"unmute" button then show the tooltip', async (t) => {
+    const users = h(t).rcData.mainCompany.users;
+    const loginUser = users[4]
+    const anotherUser = users[5];
+    const app = new AppRoot(t);
+    const tooltipTextmute = 'Mute';
+    const tooltipTextunmute = 'Unmute';
+    await h(t).glip(loginUser).init();
+    await h(t).scenarioHelper.resetProfile(loginUser);
+    let chat = <IGroup>{
+      type: 'DirectMessage',
+      owner: loginUser,
+      members: [loginUser, anotherUser]
+    }
+    await h(t).withLog('Given I have a 1:1 chat', async () => {
+      await h(t).scenarioHelper.createOrOpenChat(chat);
+    });
+
+    let session: WebphoneSession;
+    await h(t).withLog('When user login webphone', async () => {
+      session = await h(t).newWebphoneSession(anotherUser);
+    });
+
+    await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+      await h(t).directLoginWithUser(SITE_URL, loginUser);
+      await app.homePage.ensureLoaded();
+    });
+
+    // conversation page header
+    const chatEntry = app.homePage.messageTab.directMessagesSection.conversationEntryById(chat.glipId);
+    await h(t).withLog('Then I open the 1:1 chat', async () => {
+      await chatEntry.enter();
+    });
+
+    const conversationPage = app.homePage.messageTab.conversationPage;
+    const telephonyDialog = app.homePage.telephonyDialog;
+    const minimizeCallWindow = app.homePage.minimizeCallWindow;
+    await h(t).withLog('And I click the call button', async () => {
+      await conversationPage.clickTelephonyButton();
+      await telephonyDialog.ensureLoaded();
+    });
+
+    await h(t).withLog('And the callee answer the call', async () => {
+      await session.answer();
+      await session.waitForStatus('accepted');
+    });
+    await h(t).withLog('And I click the “minimized”icon', async () => {
+      await telephonyDialog.clickMinimizeButton();
+    });
+    await h(t).withLog('And I hover the “mute” button', async () => {
+      await minimizeCallWindow.hoverMuteButton();
+    });
+    await h(t).withLog('Then show the tooltip',async()=>{
+      await minimizeCallWindow.showTooltip(tooltipTextmute);
+    });
+    await h(t).withLog('When I click the “mute”button', async () => {
+      await minimizeCallWindow.clickMuteButton();
+    });
+    await h(t).withLog('And I hover the “unmute” button', async () => {
+      await minimizeCallWindow.hoverunMuteButton();
+    });
+    await h(t).withLog('Then show the tooltip',async()=>{
+      await minimizeCallWindow.showTooltip(tooltipTextunmute);
+    });
+  })
+
 });
