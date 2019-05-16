5bf0be3d3e4536ebfef1f5d49ade851eeed4e635
Aaron Huo <aaron.huo@lmxmn297.rcoffice.ringcentral.com>
2019-05-16 17:41:21 +0800

    fix(FIJI-5845): [App will be blank page when enter text to search on the global search] refactor

--- packages/jui/src/hoc/withHighlight/utils.ts
+++ packages/jui/src/hoc/withHighlight/utils.ts
@@ -7,13 +7,13 @@ import objectPath from 'object-path';
 
 const _wrapMatchedWord = (fullText: string, reg: RegExp) => {
   const matchedInEntityRegex = new RegExp(
-    `(?<=&\\w*)(${reg.source})(?=\\w*;)`,
+    `(&[^&]*)(${reg.source})(?=\\w*;)`,
     reg.flags,
   );
   const indexesOfMatchedInEntity: number[] = [];
   let result = matchedInEntityRegex.exec(fullText);
   while (result) {
-    indexesOfMatchedInEntity.push(result.index);
+    indexesOfMatchedInEntity.push(result.index + result[1].length);
     result = matchedInEntityRegex.exec(fullText);
   }
   return fullText.replace(reg, (match: string, ...args: any[]) => {
