21de95b4e85e09b102b2d117624520c4fc8f2dcf
Lex. Huang <531971909@qq.com>
2019-05-16 17:26:09 +0800

    fix(FIJI-5827): [The incoming call number is not Caller selected number] make sure the cursor at the end of the input field

--- application/src/modules/telephony/container/DialerHeader/DialerHeader.View.tsx
+++ application/src/modules/telephony/container/DialerHeader/DialerHeader.View.tsx
@@ -4,17 +4,42 @@
  * Copyright © RingCentral. All rights reserved.
  */
 
-import React from 'react';
+import React, { createRef, RefObject } from 'react';
+import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
 import { JuiHeader } from 'jui/pattern/Dialer';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { DialerHeaderViewProps } from './types';
 import { Avatar } from '@/containers/Avatar';
+import { isFunction } from 'lodash';
+
+// HACK: `HTMLInputElement | any` to fix `createTextRange` missing in standard HTML spec
+const focusCampo = (inputField: HTMLInputElement | any) => {
+  inputField.blur();
+  if (inputField && inputField.value.length === 0) {
+    return;
+  }
+  if (isFunction(inputField.createTextRange as any)) {
+    const FieldRange = (inputField.createTextRange as Function)();
+    FieldRange.moveStart('character', inputField.value.length);
+    FieldRange.collapse();
+    FieldRange.select();
+  } else if (inputField.selectionStart || inputField.selectionStart === 0) {
+    const elemLen = inputField.value.length;
+    inputField.selectionStart = elemLen;
+    inputField.selectionEnd = elemLen;
+  }
+  requestAnimationFrame(() => {
+    inputField && inputField.focus();
+  });
+};
+
+type Props = DialerHeaderViewProps & WithTranslation;
 
 @observer
-class DialerHeaderViewComponent extends React.Component<
-  DialerHeaderViewProps & WithTranslation
-> {
+class DialerHeaderViewComponent extends React.Component<Props> {
+  private _inputRef: RefObject<any> = createRef();
+
   private _Avatar = () => {
     const { uid } = this.props;
     return (
@@ -36,6 +61,20 @@ class DialerHeaderViewComponent extends React.Component<
 
   componentDidMount() {
     this._focusOnInput();
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (
+      nextProps.inputString !== undefined &&
+      nextProps.inputString !== this.props.inputString &&
+      this._inputRef &&
+      this._inputRef.current
+    ) {
+      const input = (ReactDOM.findDOMNode(
+        this._inputRef.current,
+      ) as HTMLDivElement).querySelector('input');
+      input && focusCampo(input);
+    }
   }
 
   render() {
@@ -72,6 +111,7 @@ class DialerHeaderViewComponent extends React.Component<
         deleteInputString={deleteInputString}
         onKeyDown={onKeyDown}
         Back={Back}
+        ref={this._inputRef}
       />
     );
   }
