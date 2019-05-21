ba0294055d517e3d8800e85afc8db992699b023a
Hank Huang <hank.huang@ringcentral.com>
2019-05-21 08:35:39 +0800

    feat(FIJI-5572): [Jupiter Initial Localization - Hank] 'Update code'

--- tests/e2e/testcafe/l10n/RightRail/Events.ts
+++ tests/e2e/testcafe/l10n/RightRail/Events.ts
@@ -4,6 +4,7 @@ import { formalName } from "../../libs/filter";
 import { h } from '../../v2/helpers';
 import { AppRoot } from "../../v2/page-models/AppRoot";
 import { v4 as uuid } from "uuid"
+import { IGroup } from "../../v2/models";
 
 fixture('RightRail/Events')
   .beforeEach(setupCase(BrandTire.RCOFFICE))
@@ -11,16 +12,15 @@ fixture('RightRail/Events')
 test(formalName('Events display on the right rail', ['P2', 'RightRail', 'Events', 'V1.4', 'Hank.Huang']), async (t) => {
   const app = new AppRoot(t);
   const loginUser = h(t).rcData.mainCompany.users[5];
-  const otherUser = h(t).rcData.mainCompany.users[6];
-  const teamName = `H-Team ${uuid()}`;
-  let teamID: string;
+  const team = <IGroup> {
+    name: `H-${uuid()}`,
+    type: "Team",
+    owner: loginUser,
+    members: [loginUser]
+  }
 
-  await h(t).withLog(`Given I have a team conversation: "${teamName}"`, async () => {
-    teamID = await h(t).platform(loginUser).createAndGetGroupId({
-      type: 'Team',
-      name: teamName,
-      members: [loginUser.rcId, otherUser.rcId],
-    });
+  await h(t).withLog(`Given I have a team conversation: "${team.name}"`, async () => {
+    await h(t).scenarioHelper.createTeam(team);
   });
   await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
     await h(t).directLoginWithUser(SITE_URL, loginUser);
@@ -32,7 +32,7 @@ test(formalName('Events display on the right rail', ['P2', 'RightRail', 'Events'
   await h(t).withLog('When I open the created team conversation and open "Events" tab on right rail', async () => {
     const teamsSection = app.homePage.messageTab.teamsSection;
     const EventsEntry = rightRail.eventsEntry;
-    await teamsSection.conversationEntryById(teamID).enter();
+    await teamsSection.conversationEntryById(team.glipId).enter();
     await rightRail.openMore();
     await EventsEntry.enter();
   });
@@ -40,7 +40,7 @@ test(formalName('Events display on the right rail', ['P2', 'RightRail', 'Events'
 
   await h(t).withLog(`When ${loginUser} create a note in the created team conversation` , async () =>{
     const eventTitle = `H-${uuid()}`;
-    await h(t).glip(loginUser).createSimpleEvent(teamID, eventTitle ,loginUser.rcId);
+    await h(t).glip(loginUser).createSimpleEvent({groupIds: team.glipId, title: eventTitle});
   });
   await h(t).withLog('And the text "Events" should be display', async () => {
     const listSubTitle = rightRail.listSubTitle;
--- tests/e2e/testcafe/l10n/RightRail/Notes.ts
+++ tests/e2e/testcafe/l10n/RightRail/Notes.ts
@@ -4,6 +4,7 @@ import { formalName } from "../../libs/filter";
 import { h } from '../../v2/helpers';
 import { AppRoot } from "../../v2/page-models/AppRoot";
 import { v4 as uuid } from "uuid"
+import { IGroup } from "../../v2/models";
 
 fixture('RightRail/Notes')
   .beforeEach(setupCase(BrandTire.RCOFFICE))
@@ -11,16 +12,15 @@ fixture('RightRail/Notes')
 test(formalName('Notes display on the right rail', ['P2', 'RightRail', 'Notes', 'V1.4', 'Hank.Huang']), async (t) => {
   const app = new AppRoot(t);
   const loginUser = h(t).rcData.mainCompany.users[5];
-  const otherUser = h(t).rcData.mainCompany.users[6];
-  const teamName = `H-Team ${uuid()}`;
-  let teamID: string;
+  const team = <IGroup>{
+    name: `H-${uuid()}`,
+    type: "Team",
+    owner: loginUser,
+    members: [loginUser]
+  }
 
-  await h(t).withLog(`Given I have a team conversation: "${teamName}"`, async () => {
-    teamID = await h(t).platform(loginUser).createAndGetGroupId({
-      type: 'Team',
-      name: teamName,
-      members: [loginUser.rcId, otherUser.rcId],
-    });
+  await h(t).withLog(`Given I have a team conversation: "${team.glipId}"`, async () => {
+    await h(t).scenarioHelper.createTeam(team);
   });
   await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
     await h(t).directLoginWithUser(SITE_URL, loginUser);
@@ -31,19 +31,19 @@ test(formalName('Notes display on the right rail', ['P2', 'RightRail', 'Notes', 
 
   await h(t).withLog('When I open the created team conversation and open "Notes" tab on right rail', async () => {
     const teamsSection = app.homePage.messageTab.teamsSection;
-    await teamsSection.conversationEntryById(teamID).enter();
+    await teamsSection.conversationEntryById(team.glipId).enter();
     await rightRail.openMore();
     await rightRail.notesEntry.enter();
   });
-  await h(t).log('Then I take screenshot' , { screenshotPath:'Jupiter_RightRail_NotesEmpty' });
+  await h(t).log('Then I take screenshot', { screenshotPath: 'Jupiter_RightRail_NotesEmpty' });
 
-  await h(t).withLog(`When ${loginUser} create a note in the created team conversation` , async () =>{
+  await h(t).withLog(`When ${loginUser} create a note in the created team conversation`, async () => {
     const noteTitle = `H-${uuid()}`;
-    await h(t).glip(loginUser).createSimpleNote(teamID, noteTitle);
+    await h(t).glip(loginUser).createSimpleNote(team.glipId, noteTitle);
   });
   await h(t).withLog('And the text "Notes" should be display', async () => {
     const listSubTitle = rightRail.listSubTitle;
     await t.expect(listSubTitle.exists).ok();
   });
-  await h(t).log('Then I take screenshot' , { screenshotPath:'Jupiter_RightRail_NotesList' });
+  await h(t).log('Then I take screenshot', { screenshotPath: 'Jupiter_RightRail_NotesList' });
 });
