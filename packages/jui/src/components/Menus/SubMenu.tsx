3b0c174f2b17f331fc50859bbba2527833528c1a
Shining Miao <shining.miao@ringcentral.com>
2019-05-27 06:46:50 +0000

    feat(FIJI-6015): [Menus - Submenu] add SubMenu Component

---
+++ packages/jui/src/components/Menus/SubMenu.tsx
@@ -0,0 +1,65 @@
+/*
+ * @Author: Shining Miao (shining.miao@ringcentral.com)
+ * @Date: 2019-05-23 14:10:03
+ * Copyright © RingCentral. All rights reserved.
+ */
+import React, { useState, useRef } from 'react';
+import { StyledMenuItem } from '../Menus/MenuItem';
+import { JuiMenuList } from '../Menus/MenuList';
+import { JuiPopperMenu } from '../../pattern/PopperMenu';
+import { JuiIconography } from '../../foundation/Iconography';
+import styled from '../../foundation/styled-components';
+
+type JuiSubMenuProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const StyledSubMenuItem = styled(StyledMenuItem)`
+  && {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const JuiSubMenu = React.memo((props: JuiSubMenuProps) => {
+  const [anchorEl, setAnchorEl] = useState<EventTarget & Element | null>(null);
+
+  const openPopper = (event: React.MouseEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closePopper = () => {
+    setAnchorEl(null);
+  };
+
+  const Anchor = useRef(() => {
+    const { title } = props;
+    return (
+      <StyledSubMenuItem>
+        {title}
+        <JuiIconography iconSize="medium" iconColor={['grey', '600']}>
+          arrow_right
+        </JuiIconography>
+      </StyledSubMenuItem>
+    );
+  }).current;
+
+  const open = Boolean(anchorEl);
+
+  return (
+    <JuiPopperMenu
+      open={open}
+      anchorEl={anchorEl}
+      Anchor={Anchor}
+      placement="right-start"
+      onMouseEnter={openPopper}
+      onMouseLeave={closePopper}
+      onClose={closePopper}
+    >
+      <JuiMenuList>{props.children}</JuiMenuList>
+    </JuiPopperMenu>
+  );
+});
+
+export { JuiSubMenu };
