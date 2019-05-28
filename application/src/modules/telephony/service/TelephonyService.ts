503f8dae9da74015af6bc0219edc617fa348c832
Andy Hu <andy.hu@ringcentral.com>
2019-05-28 15:35:54 +0800

    feat(FIJI-6132): [Apply default Phone setting] fix receive phone call bug

--- application/src/modules/telephony/service/TelephonyService.ts
+++ application/src/modules/telephony/service/TelephonyService.ts
@@ -33,7 +33,7 @@ import { Profile } from 'sdk/module/profile/entity';
 import ProfileModel from '@/store/models/Profile';
 import { getSingleEntity, getEntity, getGlobalValue } from '@/store/utils';
 import { ENTITY_NAME, GLOBAL_KEYS } from '@/store/constants';
-import { CALL_WINDOW_STATUS } from '../FSM';
+import { CALL_WINDOW_STATUS, CALL_STATE } from '../FSM';
 import { AccountService } from 'sdk/module/account';
 import { IClientService, CLIENT_SERVICE } from '@/modules/common/interface';
 import { CALLING_OPTIONS } from 'sdk/module/profile';
@@ -81,7 +81,7 @@ class TelephonyService {
 
   private _onReceiveIncomingCall = async (callInfo: TelephonyCallInfo) => {
     const isDefaultPhoneApp =
-      getSingleEntity(ENTITY_NAME.PROFILE, 'callingOption') ===
+      getSingleEntity(ENTITY_NAME.PROFILE, 'callOption') ===
       CALLING_OPTIONS.GLIP;
     if (!isDefaultPhoneApp) {
       return;
@@ -438,8 +438,32 @@ class TelephonyService {
     }
     return '';
   }
+  makeRCPhoneCall(phoneNumber: string) {
+    const buildURL = (phoneNumber: string) => {
+      enum RCPhoneCallURL {
+        'RC' = 'rcmobile',
+        'ATT' = 'attvr20',
+        'TELUS' = 'rctelus',
+      }
+      const currentCompanyId = getGlobalValue(GLOBAL_KEYS.CURRENT_COMPANY_ID);
+      const { rcBrand } = getEntity(ENTITY_NAME.COMPANY, currentCompanyId);
+      return `${RCPhoneCallURL[rcBrand] ||
+        RCPhoneCallURL['RC']}://call?number=${encodeURIComponent(phoneNumber)}`;
+    };
+    const url = buildURL(phoneNumber);
+    this._clientService.invokeApp(url);
+    if (this._telephonyStore.callState === CALL_STATE.DIALING) {
+      this._telephonyStore.closeDialer();
+    }
+  }
 
   makeCall = async (toNumber: string) => {
+    const isJupiterDefaultPhoneApp =
+      getSingleEntity(ENTITY_NAME.PROFILE, 'callOption') ===
+      CALLING_OPTIONS.GLIP;
+    if (!isJupiterDefaultPhoneApp) {
+      return this.makeRCPhoneCall(toNumber);
+    }
     // FIXME: move this logic to SDK and always using callerID
     const idx = this._telephonyStore.callerPhoneNumberList.findIndex(
       (phone) =>
@@ -820,22 +844,6 @@ class TelephonyService {
       time,
       timeUnit,
     );
-  }
-
-  makeRCPhoneCall(phoneNumber: string) {
-    const buildURL = (phoneNumber: string) => {
-      enum RCPhoneCallURL {
-        'RC' = 'rcmobile',
-        'ATT' = 'attvr20',
-        'TELUS' = 'rctelus',
-      }
-      const currentCompanyId = getGlobalValue(GLOBAL_KEYS.CURRENT_COMPANY_ID);
-      const { rcBrand } = getEntity(ENTITY_NAME.COMPANY, currentCompanyId);
-      return `${RCPhoneCallURL[rcBrand] ||
-        RCPhoneCallURL['RC']}://call?number=${encodeURIComponent(phoneNumber)}`;
-    };
-    const url = buildURL(phoneNumber);
-    this._clientService.invokeApp(url);
   }
 }
 
