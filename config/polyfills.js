f245241e7617d035d0287572c25057df35908a07
Shining <shining.miao@ringcentral.com>
2019-05-31 19:21:30 +0800

    fix(FIJI-6357): [The forward button is enabled when user hasn't the permission of forward] revert polyfill.js

--- config/polyfills.js
+++ config/polyfills.js
@@ -60,26 +60,26 @@ if (process.env.NODE_ENV === 'test') {
   });
 
   // mock console for jest
-  // global.console = {
-  //   assert: jest.fn(),
-  //   clear: jest.fn(),
-  //   context: jest.fn(),
-  //   count: jest.fn(),
-  //   countReset: jest.fn(),
-  //   debug: jest.fn(),
-  //   error: jest.fn(),
-  //   group: jest.fn(),
-  //   groupCollapsed: jest.fn(),
-  //   groupEnd: jest.fn(),
-  //   info: jest.fn(),
-  //   log: jest.fn(),
-  //   time: jest.fn(),
-  //   timeEnd: jest.fn(),
-  //   timeLog: jest.fn(),
-  //   timeStamp: jest.fn(),
-  //   trace: jest.fn(),
-  //   warn: jest.fn(),
-  // };
+  global.console = {
+    assert: jest.fn(),
+    clear: jest.fn(),
+    context: jest.fn(),
+    count: jest.fn(),
+    countReset: jest.fn(),
+    debug: jest.fn(),
+    error: jest.fn(),
+    group: jest.fn(),
+    groupCollapsed: jest.fn(),
+    groupEnd: jest.fn(),
+    info: jest.fn(),
+    log: jest.fn(),
+    time: jest.fn(),
+    timeEnd: jest.fn(),
+    timeLog: jest.fn(),
+    timeStamp: jest.fn(),
+    trace: jest.fn(),
+    warn: jest.fn(),
+  };
 
   global.fetch = require('jest-fetch-mock');
 }
