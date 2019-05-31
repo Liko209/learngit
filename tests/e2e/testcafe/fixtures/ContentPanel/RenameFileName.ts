21f18ad7d40e9c9d4f5b17172e8f332dc696d871
mia.cai <mia.cai@ringcentral.com>
2019-05-31 22:33:04 +0800

    feat(FIJI-6086): [[E2E]Edit filename] add cases

---
+++ tests/e2e/testcafe/fixtures/ContentPanel/RenameFileName.ts
@@ -0,0 +1,301 @@
+import { h } from '../../v2/helpers';
+import { setupCase, teardownCase } from '../../init';
+import { AppRoot } from '../../v2/page-models/AppRoot';
+import { SITE_URL, BrandTire } from '../../config';
+import { ITestMeta } from "../../v2/models";
+import { v4 as uuid } from 'uuid';
+import { IGroup } from '../../v2/models';
+
+fixture('ContentPanel/RenameFileName')
+  .beforeEach(setupCase(BrandTire.RC_FIJI_GUEST))
+  .afterEach(teardownCase());
+
+// todo
+test.only.meta(<ITestMeta>{
+  priority: ['P1'],
+  caseIds: ['JPT-2061'],
+  maintainers: ['Mia.cai'],
+  keywords: ['ContentPanel/RenameFileName']
+})(`Can rename file/file successfully`, async (t) => {
+  const renameFileMenu = 'Edit filename';
+  const filenames = ['1'];
+  const suffixs = ['psd']
+  // todo  should check source
+  const filesPath = ['../../sources/1.psd'];
+  const message = uuid();
+  const loginUser = h(t).rcData.mainCompany.users[4];
+
+  let team = <IGroup>{
+    type: "Team",
+    name: uuid(),
+    owner: loginUser,
+    members: [loginUser]
+  }
+  await h(t).withLog(`Given I have a team named: ${team.name}`, async () => {
+    await h(t).scenarioHelper.createTeam(team);
+  });
+
+  const app = new AppRoot(t);
+  const conversationPage = app.homePage.messageTab.conversationPage;
+  const moreActionOnFile = app.homePage.moreActionOnFile;
+  const renameFileDialog = moreActionOnFile.renameFileDialog;
+  const rightRail = app.homePage.messageTab.rightRail;
+  await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  });
+
+  await h(t).withLog('And I open the team and upload a file file', async () => {
+    await app.homePage.messageTab.teamsSection.conversationEntryById(team.glipId).enter();
+    await conversationPage.uploadFilesToMessageAttachment(filesPath[0]);
+    await conversationPage.sendMessage(message);
+    await conversationPage.nthPostItem(-1).waitForPostToSend();
+  });
+
+  const posts = app.homePage.messageTab.conversationPage.posts;
+  const filesTabItem = rightRail.filesTab.nthItem(0);
+  const viewerDialog = app.homePage.viewerDialog;
+  const postItem = app.homePage.messageTab.conversationPage.nthPostItem(0);
+  const Entries =[postItem,filesTabItem,viewerDialog];
+
+  for(let i = 0 ; i < Entries.length; i++ ){
+
+    if( i == 0 ){   
+      await h(t).withLog(`And I click the more button of the file`, async() => {
+        await moreActionOnFile.clickMore();
+      });
+    }else if( i == 1 ){
+      await h(t).withLog(`When I click Files Tab on the right self(Entry2:right self)`, async () => {
+        await rightRail.filesEntry.enter();
+        await rightRail.filesEntry.shouldBeOpened();
+      })
+
+      await h(t).withLog(`When I hover the file item`, async () => {
+        await filesTabItem.nameShouldBe(filenames[0]);
+        await t.hover(filesTabItem.self)
+      });
+
+      await h(t).withLog(`And I click the more button of the file`, async() => {
+        await filesTabItem.clickMore();
+      });
+
+    }else{
+
+      await h(t).withLog('When I click the file(Entry3:file viewer)', async () => {
+        await t.click(posts.nth(-1).find('img'));
+        await viewerDialog.ensureLoaded();
+      });
+      await h(t).withLog(`And I click the more button of the file`, async() => {
+        await moreActionOnFile.clickMore();
+      });
+
+    }
+  
+    await h(t).withLog(`Then will show ${renameFileMenu} menu at the top`, async() => {
+      await moreActionOnFile.renameFileMenuAtTop(renameFileMenu);
+    });
+
+    await h(t).withLog(`When I click the ${renameFileMenu} menu of the file`, async() => {
+      await moreActionOnFile.clickRenameFileMenu();
+    });
+  
+    await h(t).withLog(`Then will show the rename file dialog`, async() => {
+      await moreActionOnFile.ensureLoaded();
+    });
+  
+    await h(t).withLog(`And pre-populate in the input field the existing file name with the suffix`, async() => {
+      await renameFileDialog.existFileNameWithSuffix(filenames[0],suffixs[0]);
+    });
+  
+    let newFileName = `file name ${i}`;
+    await h(t).withLog(`When I update the file name`, async() => {
+      await renameFileDialog.updateFileName(newFileName);
+    });
+  
+    await h(t).withLog(`And I click the Cancel button`, async() => {
+      await renameFileDialog.clickCancelButton();
+    });
+  
+    await h(t).withLog(`Then the dialog should be closed`, async() => {
+      await renameFileDialog.ensureDismiss();
+    });
+
+    await h(t).withLog(`And the filename should remain unchanged`, async() => {
+      if( i == 1){
+        await filesTabItem.nameShouldBe(filenames[0]);
+      }else{
+        await postItem.nameShouldBe(filenames[0]);
+      }
+    });
+
+    return
+
+    if( i == 0 ){
+
+      await h(t).withLog(`When I click the more button of the file`, async() => {
+        await moreActionOnFile.clickMore();
+      });
+
+    }else if( i == 1 ){
+
+      await h(t).withLog(`When I hover the file ${filenames[0]} item(Entry2:right self)`, async () => {
+        await filesTabItem.nameShouldBe(filenames[0]);
+        await t.hover(filesTabItem.self)
+      });
+      await h(t).withLog(`And I click the more button of the file`, async() => {
+        await filesTabItem.clickMore();
+      });
+
+    }else{
+
+      await h(t).withLog('When I click the file(Entry3:file viewer)', async () => {
+        await t.click(posts.nth(-1).find('img'));
+        await viewerDialog.ensureLoaded();
+      });
+
+      await h(t).withLog(`And I click the more button of the file`, async() => {
+        await moreActionOnFile.clickMore();
+      });
+
+    }
+  
+    await h(t).withLog(`Then will show ${renameFileMenu} menu at the top`, async() => {
+      await moreActionOnFile.renameFileMenuAtTop(renameFileMenu);
+    });
+
+    await h(t).withLog(`When I click the ${renameFileMenu} menu of the file`, async() => {
+      await moreActionOnFile.clickRenameFileMenu();
+    });
+  
+    await h(t).withLog(`Then will show the rename file dialog`, async() => {
+      await moreActionOnFile.ensureLoaded();
+    });
+
+    await h(t).withLog(`When I update the file name`, async() => {
+      await renameFileDialog.updateFileName(newFileName);
+    });
+  
+    await h(t).withLog(`And I click the Save button`, async() => {
+      await renameFileDialog.clickSaveButton();
+    });
+  
+    await h(t).withLog(`Then the dialog should be closed`, async() => {
+      await renameFileDialog.ensureDismiss();
+    });
+
+    await h(t).withLog(`And show the new file name`, async() => {
+      if( i == 1){
+        await filesTabItem.nameShouldBe(newFileName);
+      }else{
+        await postItem.nameShouldBe(newFileName);
+      }
+    });
+    filenames[0] = newFileName;
+
+  }
+  
+});
+
+test.meta(<ITestMeta>{
+  priority: ['P2'],
+  caseIds: ['JPT-2057','JPT-2074','JPT-2057'], // TODO case id ????
+  maintainers: ['Mia.cai'],
+  keywords: ['ContentPanel/RenameFileName']
+})(`Unsupported characters should be replaced by_ when saving the file name;Show 'More' tooltip for the more icon;Rename file option is enabled to every member except guests;`, async (t) => {
+  const renameFileMenu = 'Edit filename';
+  const filesPath = ['../../sources/1.psd'];
+    // TODO
+  const nameWithUnSupportChar = 'file*/';
+  const newFileName = 'file__';
+  const moreTooltip = 'More';
+  const message = uuid();
+  const loginUser = h(t).rcData.mainCompany.users[4];
+  const guest = h(t).rcData.guestCompany.users[0];
+  const app = new AppRoot(t);
+  const conversationPage = app.homePage.messageTab.conversationPage;
+  const moreActionOnFile = app.homePage.moreActionOnFile;
+  const renameFileDialog = moreActionOnFile.renameFileDialog;
+  const postItem = app.homePage.messageTab.conversationPage.nthPostItem(0);
+
+  let team = <IGroup>{
+    type: "Team",
+    name: uuid(),
+    owner: loginUser,
+    members: [loginUser,guest]
+  }
+  await h(t).withLog(`Given I have a team named: ${team.name}`, async () => {
+    await h(t).scenarioHelper.createTeam(team);
+  });
+
+  await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  });
+
+  await h(t).withLog('And I open the team and upload a file file', async () => {
+    await app.homePage.messageTab.teamsSection.conversationEntryById(team.glipId).enter();
+    await conversationPage.uploadFilesToMessageAttachment(filesPath[0]);
+    await conversationPage.sendMessage(message);
+    await conversationPage.nthPostItem(-1).waitForPostToSend();
+  });
+
+  await h(t).withLog(`When I am on hover the file`, async () => {
+    await t.hover(postItem.img);
+  })  
+
+  await h(t).withLog(`Then show ${moreTooltip} tooltip`, async () => {
+    await postItem.showTooltip(moreTooltip);
+  }); 
+
+  await h(t).withLog(`When I click the more button of the file`, async() => {
+    await moreActionOnFile.clickMore();
+  });
+
+  await h(t).withLog(`Then will show ${renameFileMenu} menu at the top`, async() => {
+    await moreActionOnFile.renameFileMenuAtTop(renameFileMenu);
+  });
+
+  await h(t).withLog(`When I click the ${renameFileMenu} menu of the file`, async() => {
+    await moreActionOnFile.clickRenameFileMenu();
+  });
+
+  await h(t).withLog(`Then will show the rename file dialog`, async() => {
+    await moreActionOnFile.ensureLoaded();
+  });
+
+  await h(t).withLog(`When I update the file name`, async() => {
+    await renameFileDialog.updateFileName(nameWithUnSupportChar);
+  });
+
+  await h(t).withLog(`And I click the Save button`, async() => {
+    await renameFileDialog.clickSaveButton();
+  });
+
+  await h(t).withLog(`Then the dialog should be closed`, async() => {
+    await renameFileDialog.ensureDismiss();
+  });
+
+  await h(t).withLog(`And show the new file name`, async() => {
+    await postItem.nameShouldBe(newFileName);
+  });
+
+  //
+  await h(t).withLog(`And I login Jupiter with guest`, async () => {
+    await h(t).directLoginWithUser(SITE_URL, guest);
+    await app.homePage.ensureLoaded();
+  });
+
+  await h(t).withLog('And I open the team', async () => {
+    await app.homePage.messageTab.teamsSection.conversationEntryById(team.glipId).enter();
+  });
+
+  await h(t).withLog(`And I click the more button of the file`, async() => {
+    await moreActionOnFile.clickMore();
+  });
+
+  await h(t).withLog(`Then ${renameFileMenu} menu will be disabled`, async() => {
+    // todo check menu disabled
+  });
+
+});
+
---
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MoreActionOnFile.ts
@@ -0,0 +1,76 @@
+import { BaseWebComponent } from "../../BaseWebComponent";
+
+
+export class MoreActionOnFile extends BaseWebComponent{
+
+  get self(){
+    return this.getSelectorByAutomationId('fileActionMore');
+  }
+
+  get more(){
+    return this.getSelectorByAutomationId('fileActionMore');
+  }
+
+  get renameFileMenu(){
+    return this.getSelectorByAutomationId('fileNameEditItem');
+  }
+
+  get renameFileDialog(){
+    return this.getComponent(RenameFileDialog);
+  }
+
+  async clickMore(){
+    await this.t.click(this.more);
+  }
+  
+  async clickRenameFileMenu(){
+    await this.t.click(this.renameFileMenu);
+  }
+
+  async renameFileMenuAtTop(menu:string){
+    // await this.t.expect(this.renameFileMenu.withText(menu)).ok();
+    await this.t.expect(this.renameFileMenu.find('li').nth(0).withText(menu)).ok();
+  }
+
+}
+
+export class RenameFileDialog extends BaseWebComponent{
+
+    get self(){
+      return this.getSelectorByAutomationId('fileNameEditDialogContainer');
+    }
+  
+    get cancelButton(){
+      return this.getSelectorByAutomationId('DialogCancelButton');
+    }
+  
+    get saveButton(){
+      return this.getSelectorByAutomationId('DialogOKButton');
+    }
+   
+    get fileNameInput(){
+      return this.getSelectorByAutomationId('fileNameEditSuffixFollowTextField').find('textarea').nth(2);
+    }
+  
+    get fileNameSuffix(){
+      return this.getSelectorByAutomationId('followSuffixText');
+    }
+  
+    async clickCancelButton(){
+      await this.t.click(this.cancelButton);
+    }
+  
+    async clickSaveButton(){
+      await this.t.click(this.saveButton);
+    }
+  
+    async updateFileName(text:string){
+      await this.clickAndTypeText(this.fileNameInput,text, { replace: true, paste: true });
+    }
+  
+    async existFileNameWithSuffix(name:string, suffix:string){
+      await this.t.expect(this.fileNameInput.withText(name)).ok();
+      await this.t.expect(this.fileNameSuffix.withText(suffix)).ok();
+    }
+  
+  }
