5b64540de2a7e6ec133c79e37ef52e60224c80a5
alessia <alessia.li@ringcentral.com>
2019-06-04 17:43:28 +0800

    feat(FIJI-5353): [Notification Not enabled Banner] update ut

--- application/src/modules/app/container/TopBanner/Banners/NotificationEnableBanner/__tests__/NotificationEnableBanner.ViewModel.test.ts
+++ application/src/modules/app/container/TopBanner/Banners/NotificationEnableBanner/__tests__/NotificationEnableBanner.ViewModel.test.ts
@@ -5,12 +5,23 @@
  */
 
 import { NotificationEnableBannerViewModel } from '../NotificationEnableBanner.ViewModel';
+import * as userAgent from '@/common/isUserAgent';
+import { getEntity } from '@/store/utils/entities';
+jest.mock('@/store/utils/entities');
 
-jest.mock('@/common/isUserAgent', () => {
-  return {
-    isElectron: jest.fn().mockReturnValue(false),
-  };
-});
+function setUpMock(
+  isElectron: boolean,
+  browserPermission: NotificationPermission,
+  wantNotifications: boolean,
+) {
+  userAgent.isElectron = isElectron;
+  getEntity.mockReturnValue({
+    value: {
+      browserPermission,
+      wantNotifications,
+    },
+  });
+}
 
 describe('NotificationEnableBannerViewModel', () => {
   beforeEach(() => {
@@ -18,8 +29,25 @@ describe('NotificationEnableBannerViewModel', () => {
   });
   describe('isShow', () => {
     it('should return false when in Electron [JPT-2093]', () => {
+      setUpMock(true, 'default', true);
       const viewModel = new NotificationEnableBannerViewModel();
       expect(viewModel.isShow).toBe(false);
+    });
+    it('should return false when wantNotifications is false', () => {
+      setUpMock(false, 'granted', false);
+      const viewModel = new NotificationEnableBannerViewModel();
+      expect(viewModel.isShow).toBe(false);
+    });
+    it('should return false when browserPermission is "granted"', () => {
+      setUpMock(false, 'granted', true);
+      const viewModel = new NotificationEnableBannerViewModel();
+      expect(viewModel.isShow).toBe(false);
+    });
+    it('should return true when browserPermission is not "granted" and wantNotifications is true and not on Electron', () => {
+      setUpMock(false, 'default', true);
+      console.log(223424);
+      const viewModel = new NotificationEnableBannerViewModel();
+      expect(viewModel.isShow).toBe(true);
     });
   });
 });
