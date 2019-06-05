93c82451743eea6ff0318b04782d32091d9b7997
Looper Wang <looper.wang@ringcentral.com>
2019-06-05 19:55:20 +0800

    feat(FIJI-6071): [[JUI] preview loading and done status] update UT.

--- application/src/modules/viewer/fileViewer/__tests__/fileViewer.ViewModel.test.tsx
+++ application/src/modules/viewer/fileViewer/__tests__/fileViewer.ViewModel.test.tsx
@@ -7,11 +7,33 @@
 import { getEntity } from '@/store/utils';
 import { FileViewerViewModel } from '../fileViewer.ViewModel';
 import FileItemModel from '@/store/models/FileItem';
+import { Notification } from '@/containers/Notification';
+import {
+  ToastMessageAlign,
+  ToastType,
+} from '@/containers/ToastWrapper/Toast/types';
 jest.mock('@/store/utils');
+
+jest.mock('@/containers/Notification');
+jest.mock('@/utils/error');
+
+function toastParamsBuilder(message: string) {
+  return {
+    message,
+    type: ToastType.ERROR,
+    messageAlign: ToastMessageAlign.LEFT,
+    fullWidth: false,
+    dismissible: false,
+  };
+}
 
 describe('FileViewerViewModel', () => {
   beforeAll(() => {
     jest.resetAllMocks();
+  });
+
+  beforeEach(() => {
+    Notification.flashToast = jest.fn();
   });
   describe('viewerDestroyer()', () => {
     it('should dismiss be call when call viewerDestroyer function', () => {
@@ -60,13 +82,31 @@ describe('FileViewerViewModel', () => {
     });
   });
   describe('actions()', () => {
-    it('should be return not undefined when call actions()', () => {
+    it('The viewer should be closed automatically when the file being previewed was deleted [JPT-2155]', async () => {
       const dismiss = jest.fn();
       const vm = new FileViewerViewModel(
-        { versions: [{ pages: undefined }] } as FileItemModel,
+        {
+          deactivated: true,
+          versions: [{ pages: undefined }],
+        } as FileItemModel,
         dismiss,
       );
-      expect(vm.actions).not.toEqual(undefined);
+      expect(Notification.flashToast).toHaveBeenCalledWith(
+        toastParamsBuilder('viewer.ImageDeleted'),
+      );
+    });
+    it('The viewer should not show error when the file being previewed was not deleted', async () => {
+      const dismiss = jest.fn();
+      const vm = new FileViewerViewModel(
+        {
+          deactivated: true,
+          versions: [{ pages: undefined }],
+        } as FileItemModel,
+        dismiss,
+      );
+      expect(Notification.flashToast).toHaveBeenCalledWith(
+        toastParamsBuilder('viewer.ImageDeleted'),
+      );
     });
   });
 });
