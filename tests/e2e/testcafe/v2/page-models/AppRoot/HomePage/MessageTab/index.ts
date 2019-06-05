e705bc0c3920e5d9a5d0de4cdf4ef710e91b18bc
Lorna Li <lorna.li@ringcentral.com>
2019-06-05 17:38:48 +0800

    l10n(FIJI-5569): [Jupiter Initial Localization - Lorna] Update TeamMemberSettings case

--- tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessageTab/index.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessageTab/index.ts
@@ -320,7 +320,7 @@ export class MessageTab extends BaseWebComponent {
   }
 
   get teamsSection() {
-    return this.getSection('Teams');
+    return this.getSection('团队');
   }
 
   get mentionsEntry() {
