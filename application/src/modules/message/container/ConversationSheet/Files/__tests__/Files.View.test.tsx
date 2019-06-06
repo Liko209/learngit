18de998304baa17d778e7d8531449a253155b762
Looper Wang <looper.wang@ringcentral.com>
2019-06-06 13:51:10 +0800

    feat(FIJI-6071): [[JUI] preview loading and done status] update.

--- application/src/modules/message/container/ConversationSheet/Files/__tests__/Files.View.test.tsx
+++ application/src/modules/message/container/ConversationSheet/Files/__tests__/Files.View.test.tsx
@@ -157,7 +157,7 @@ describe('FilesView', () => {
         postId: 1,
         progresses: { get: () => 1 },
       };
-      props.files[1].item.type = 'ppt';
+      props.files[1][0].item.type = 'ppt';
       const wrapper = shallow(<FilesView {...props} />);
       wrapper
         .find(JuiFileWithPreview)
