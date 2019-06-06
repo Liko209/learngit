ef1102f762eb054e3eca048b685a9b2f6b7521bb
Looper Wang <looper.wang@ringcentral.com>
2019-06-06 12:32:01 +0800

    feat(FIJI-6069): [[Application] Viewer VIew] update UT.

--- application/src/modules/viewer/FileViewerManager/__tests__/fileViewer.ViewModel.test.tsx
+++ application/src/modules/viewer/FileViewerManager/__tests__/fileViewer.ViewModel.test.tsx
@@ -5,7 +5,8 @@
  */
 
 import { getEntity } from '@/store/utils';
-import { FileViewerViewModel } from '../fileViewer.ViewModel';
+jest.mock('@/store/utils');
+import { FileViewerViewModel } from '../FileViewer.ViewModel';
 import FileItemModel from '@/store/models/FileItem';
 import { Notification } from '@/containers/Notification';
 import {
@@ -43,63 +44,112 @@ describe('FileViewerViewModel', () => {
       expect(dismiss).toBeCalled();
     });
   });
-  // describe('pages()', () => {
-  //   it('should be return undefined when pages undefined', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: undefined }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     expect(vm.pages).toEqual(undefined);
-  //   });
-  //   it('should be return not undefined when pages not undefined', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: [1, 2] }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     expect(vm.pages.length).toEqual(2);
-  //   });
-  // });
-  // describe('info()', () => {
-  //   it('should be return not undefined when call info()', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: undefined }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     expect(vm.info).not.toEqual(undefined);
-  //   });
-  // });
-  // describe('handleTextFieldChange()', () => {
-  //   it('should be return 2  when call input 2 length 2', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: [1, 2] }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     vm.handleTextFieldChange({ target: { value: '2' } });
-  //     expect(vm['_textFieldValue']).toEqual(2);
-  //   });
+  describe('pages()', () => {
+    it('should be return undefined when pages undefined', () => {
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
 
-  //   it('should be return 2  when call input 3 length 2', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: [1, 2] }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     vm.handleTextFieldChange({ target: { value: '3' } });
-  //     expect(vm['_textFieldValue']).toEqual(2);
-  //   });
+      expect(vm.pages).toEqual(undefined);
+    });
+    it('should be return not undefined when pages not undefined', () => {
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: [1, 2] }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
 
-  //   it('should be return 1 when call input -1 length 2', () => {
-  //     const dismiss = jest.fn();
-  //     const vm = new FileViewerViewModel(
-  //       { versions: [{ pages: [1, 2] }] } as FileItemModel,
-  //       dismiss,
-  //     );
-  //     vm.handleTextFieldChange({ target: { value: '-1' } });
-  //     expect(vm['_textFieldValue']).toEqual(1);
-  //   });
-  // });
+      expect(vm.pages && vm.pages.length).toEqual(2);
+    });
+  });
+
+  describe('update()', () => {
+    it('should _currentPageIdx be 1 when pageIdx =1 and _currentPageIdx = 1 ', () => {
+      const data = { scale: 1, pageIdx: 1 };
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
+      vm['_currentPageIdx '] = 1;
+      vm.onUpdate(data);
+      expect(vm['_currentPageIdx']).toEqual(1);
+    });
+    it('should _currentPageIdx be 2 when pageIdx =1 and _currentPageIdx = 2', () => {
+      const data = { scale: 1, pageIdx: 2 };
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
+      vm['_currentPageIdx '] = 1;
+      vm.onUpdate(data);
+      expect(vm['_currentPageIdx']).toEqual(2);
+    });
+
+    it('should _currentScale be 1 when pageIdx =1 and _currentScale = 1 ', () => {
+      const data = { scale: 0.5, pageIdx: 1 };
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
+      vm['_currentScale '] = 0.5;
+      vm.onUpdate(data);
+      expect(vm['_currentScale']).toEqual(0.5);
+    });
+    it('should _currentScale be 2 when pageIdx =1 and _currentScale = 2', () => {
+      const data = { scale: 1, pageIdx: 1 };
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
+      vm['_currentScale '] = 0.5;
+      vm.onUpdate(data);
+      expect(vm['_currentScale']).toEqual(1);
+    });
+  });
+  describe('title()', () => {
+    it('should be return not undefined when call title()', () => {
+      const dismiss = jest.fn();
+      const vm = new FileViewerViewModel(1, dismiss);
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: undefined }],
+      });
+      expect(vm.title).not.toEqual(undefined);
+    });
+  });
+  describe('handleTextFieldChange()', () => {
+    it('should be return 2  when call input 2 length 2', () => {
+      const dismiss = jest.fn();
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: [1, 2] }],
+      });
+      const vm = new FileViewerViewModel(1, dismiss);
+      vm.handleTextFieldChange({ target: { value: '2' } });
+      expect(vm['_textFieldValue']).toEqual(2);
+    });
+    it('should be return 2  when call input 3 length 2', () => {
+      const dismiss = jest.fn();
+      const vm = new FileViewerViewModel(1, dismiss);
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: [1, 2] }],
+      });
+      vm.handleTextFieldChange({ target: { value: '3' } });
+      expect(vm['_textFieldValue']).toEqual(2);
+    });
+
+    it('should be return 1 when call input -1 length 2', () => {
+      const dismiss = jest.fn();
+      const vm = new FileViewerViewModel(1, dismiss);
+      (getEntity as jest.Mock).mockReturnValue({
+        versions: [{ pages: [1, 2] }],
+      });
+      vm.handleTextFieldChange({ target: { value: '-1' } });
+      expect(vm['_textFieldValue']).toEqual(1);
+    });
+  });
 });
