a31a363bc33c427c3a4db0440c53e517a9a4b375
alessia <alessia.li@ringcentral.com>
2019-06-11 11:07:56 +0800

    fix(FIJI-6571): [Can't trigger Notification when the browser is 'Allow' and turn on 'Desktop notifications for browser'] fix ut

--- application/src/modules/notification/notificationSettingManager/NotificationBrowserSettingItem/__tests__/NotificationBrowserSettingItem.ViewModel.test.tsx
+++ application/src/modules/notification/notificationSettingManager/NotificationBrowserSettingItem/__tests__/NotificationBrowserSettingItem.ViewModel.test.tsx
@@ -6,53 +6,22 @@
 
 import { NotificationBrowserSettingItemViewModel } from '../NotificationBrowserSettingItem.ViewModel';
 jest.mock('@/utils/i18nT', () => (key: string) => key);
-import {
-  ERROR_CODES_NETWORK,
-  JNetworkError,
-  JServerError,
-  ERROR_CODES_SERVER,
-} from 'sdk/error';
-import { Notification } from '@/containers/Notification';
-jest.mock('@/containers/Notification');
 
-function setUpMock(errorType: 'network' | 'server') {
-  (NotificationBrowserSettingItemViewModel.prototype.settingItemEntity.valueSetter = jest
-    .fn()
-    .mockImplementation(() => {
-      if (errorType === 'network') {
-        throw new JNetworkError(ERROR_CODES_NETWORK.NOT_NETWORK, 'NOT_NETWORK');
-      }
-      if (errorType === 'server') {
-        throw new JServerError(ERROR_CODES_SERVER.GENERAL, 'GENERAL');
-      }
-    })),
-    (Notification.flashToast = jest.fn().mockImplementation(() => {}));
+function setUpMock() {
+  NotificationBrowserSettingItemViewModel.prototype.settingItemEntity.valueSetter = jest.fn();
 }
 
-describe('NotificationBrowserSettingItemView', () => {
-  describe('render()', () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-    it('Should display flash toast notification when setToggleState failed for network issue', async () => {
-      setUpMock('network');
-      const vm = new NotificationBrowserSettingItemViewModel();
-      await vm.setToggleState(true);
-      expect(Notification.flashToast).toHaveBeenCalledWith(
-        expect.objectContaining({
-          message: 'setting.errorText.network',
-        }),
-      );
-    });
-    it('Should display flash toast notification when setToggleState failed for server issue', async () => {
-      setUpMock('server');
-      const vm = new NotificationBrowserSettingItemViewModel();
-      await vm.setToggleState(true);
-      expect(Notification.flashToast).toHaveBeenCalledWith(
-        expect.objectContaining({
-          message: 'setting.errorText.server',
-        }),
-      );
-    });
+describe('NotificationBrowserSettingItemViewModel', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('Should call valueSetter when call setToggleState', async () => {
+    setUpMock();
+    const vm = new NotificationBrowserSettingItemViewModel();
+    await vm.setToggleState(true);
+    expect(
+      NotificationBrowserSettingItemViewModel.prototype.settingItemEntity
+        .valueSetter,
+    ).toHaveBeenCalledWith({ desktopNotifications: true });
   });
 });
