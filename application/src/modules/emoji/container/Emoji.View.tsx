78d540713d643285f1190f8452f10de37a514c0f
ken.li <ken.li@ringcental.com>
2019-06-11 12:10:30 +0800

    feat(FIJI-6620): [Emoji i18n plus] add translation file

--- application/src/modules/emoji/container/Emoji.View.tsx
+++ application/src/modules/emoji/container/Emoji.View.tsx
@@ -15,6 +15,36 @@ class EmojiView extends Component<Types> {
   handleKeepOpenChange = () => {
     this.props.setEmojiOpenStatus();
   }
+  private _getEmojiI18nTexts = () => {
+    return {
+      search: i18nP('message.emoji.i18n.search'),
+      clear: i18nP('message.emoji.i18n.clear'), // Accessible label on "clear" button
+      notfound: i18nP('message.emoji.i18n.notfound'),
+      skintext: i18nP('message.emoji.i18n.skintext'),
+      categories: {
+        search: i18nP('message.emoji.i18n.categories.search'),
+        recent: i18nP('message.emoji.i18n.categories.recent'),
+        people: i18nP('message.emoji.i18n.categories.people'),
+        nature: i18nP('message.emoji.i18n.categories.nature'),
+        foods: i18nP('message.emoji.i18n.categories.foods'),
+        activity: i18nP('message.emoji.i18n.categories.activity'),
+        places: i18nP('message.emoji.i18n.categories.places'),
+        objects: i18nP('message.emoji.i18n.categories.objects'),
+        symbols: i18nP('message.emoji.i18n.categories.symbols'),
+        flags: i18nP('message.emoji.i18n.categories.flags'),
+        custom: i18nP('message.emoji.i18n.categories.custom'),
+      },
+      categorieslabel: i18nP('message.emoji.i18n.categorieslabel'), // Accessible title for the list of categories
+      skintones: {
+        1: i18nP('message.emoji.i18n.skintones.default'),
+        2: i18nP('message.emoji.i18n.skintones.light'),
+        3: i18nP('message.emoji.i18n.skintones.mediumLight'),
+        4: i18nP('message.emoji.i18n.skintones.medium'),
+        5: i18nP('message.emoji.i18n.skintones.mediumDark'),
+        6: i18nP('message.emoji.i18n.skintones.dark'),
+      },
+    };
+  }
   render() {
     const {
       handleEmojiClick,
@@ -25,6 +55,7 @@ class EmojiView extends Component<Types> {
     } = this.props;
     return (
       <JuiEmojiWithTheme
+        i18nObj={this._getEmojiI18nTexts()}
         set={set}
         sheetSize={sheetSize}
         title={title}
