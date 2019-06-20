bc19603cba0047915e22db8076ec0a2a59647d5f
Potar He <potar.he@ringcentral.com>
2019-06-20 11:12:07 +0000

    test(FIIJ-6884): [e2e-new] implement JPT-3,197,1370,143,1273

--- tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessageTab/LeftRail.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/MessageTab/LeftRail.ts
@@ -1,8 +1,21 @@
 import { BaseWebComponent } from "../../../BaseWebComponent";
+import { ClientFunction } from "testcafe";
 
 export class LeftRail extends BaseWebComponent {
 
   get self() {
     return this.getSelectorByAutomationId('leftRail');
+  }
+
+  get scrollDiv() {
+    return this.self.child('div').nth(1);
+  }
+
+  async scrollToY(y: number) {
+    const scrollDivElement = this.scrollDiv;
+    await ClientFunction((_y) => {
+      scrollDivElement().scrollTop = _y;
+    },
+      { dependencies: { scrollDivElement } })(y);
   }
 }
