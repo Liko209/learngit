a32ebaefe738621b7af4aa26b43a1a823ed28a1c
Chris Zhan <chris.zhan@ringcentral.com>
2019-06-24 11:40:43 +0800

    fix(FIJI-7012): [Can't render the emoji when sending a link with an emoji] fix

--- application/src/common/postParser/parsers/AtMentionParser.tsx
+++ application/src/common/postParser/parsers/AtMentionParser.tsx
@@ -9,7 +9,8 @@ import { IPostParser, ParserType, AtMentionParserOption } from '../types';
 import { ParseContent } from '../ParseContent';
 import { JuiAtMention } from 'jui/components/AtMention';
 import { PostParser } from './PostParser';
-import { AT_MENTION_REGEX, b64DecodeUnicode } from '../utils';
+import { AT_MENTION_REGEX } from '../utils';
+import { AtMentionTransformer } from './AtMentionTransformer';
 
 class AtMentionParser extends PostParser implements IPostParser {
   type = ParserType.AT_MENTION;
@@ -19,12 +20,7 @@ class AtMentionParser extends PostParser implements IPostParser {
   }
 
   getReplaceElement(strValue: string) {
-    const {
-      map = {},
-      customReplaceFunc,
-      innerContentParser,
-      textEncoded,
-    } = this.options;
+    const { map = {}, customReplaceFunc, innerContentParser } = this.options;
     const result = this.getRegexp().exec(strValue);
     if (!result || !result[0]) {
       return strValue;
@@ -32,8 +28,7 @@ class AtMentionParser extends PostParser implements IPostParser {
     const id = result[1];
     const user = map[id] || {};
     const { name, isCurrent } = user;
-    const text =
-      name || (textEncoded ? b64DecodeUnicode(result[2]) : result[2]);
+    const text = name || AtMentionTransformer.atMentionDataMap[id];
     if (customReplaceFunc) {
       return customReplaceFunc(strValue, id, text, !!isCurrent);
     }
