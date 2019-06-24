5c7a6983daa5b4e91f784a8f2119fbba64e7a24a
Joy Zhang <joy.zhang@ringcentral.com>
2019-06-24 10:28:02 +0800

    feat(FIJI-4686): [VoicemailActions] delete VoicemailActions

--- application/src/modules/phone/container/VoicemailActions/Delete/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: joy.zhang (joy.zhang@ringcentral.com)
- * @Date: 2019-05-24 07:28:07
- * Copyright © RingCentral. All rights reserved.
- */
-
-export * from './Delete';
--- application/src/modules/phone/container/VoicemailActions/Download/constants.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: joy.zhang (joy.zhang@ringcentral.com)
- * @Date: 2019-06-05 10:15:26
- * Copyright © RingCentral. All rights reserved.
- */
-
-export const URI_CONTENT_DISPOSITION_ATTACHMENT = '&contentDisposition=Attachment';
