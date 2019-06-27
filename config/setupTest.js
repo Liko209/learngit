d17fa3c6b4991b24c70025296c28564cf70d6063
Valor Lin <valor.lin@ringcentral.com>
2019-06-27 10:09:35 +0800

    chore(FIJI-6910): [Support to use VL in a container that it's height depends on children] refactor jest setup

--- config/setupTest.js
+++ config/setupTest.js
@@ -3,11 +3,7 @@
  * @Date: 2019-05-08 23:01:16
  * Copyright © RingCentral. All rights reserved.
  */
-
-import 'jest-styled-components';
-import themeHandler from 'rcui/foundation/styles/ThemeHandler';
-import light from './light.json';
-jest
-  .spyOn(themeHandler, 'loadUrl')
-  .mockImplementation(() => Promise.resolve(light));
-import 'jest-styled-components';
+import './jest/setup/setupConsole';
+import './jest/setup/setupTimezone';
+import './jest/setup/setupMobx';
+import './jest/setup/setupStyledTheme';
