7aeb154292defe462420f64028166e8058594491
kasni.huang <kasni.huang@ringcentral.com>
2019-07-03 14:55:59 +0800

    feat(FIJI-6703): [Integrate firebase performance monitor] -- implementation

--- packages/framework/src/__tests__/Jupiter.test.ts
+++ packages/framework/src/__tests__/Jupiter.test.ts
@@ -83,7 +83,7 @@ describe('Jupiter', () => {
       provides: [MyServiceProvides],
     };
 
-    it.skip('should be register module', async () => {
+    it('should be register module', async () => {
       const jupiter = new Jupiter();
       jupiter.registerModuleAsync(
         () => new Promise(res => res({ config: myServiceModuleConfig })),
