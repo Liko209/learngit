349947514a0fe99f8a5a3a5525acdb1d81077feb
Paynter Chen <paynter.chen@ringcentral.com>
2019-07-11 17:26:16 +0800

    feat(FIJI-6367): [Integration Test] Router only log when error

--- tests/shield/src/sdk/mocks/server/Router.ts
+++ tests/shield/src/sdk/mocks/server/Router.ts
@@ -3,12 +3,16 @@
  * @Date: 2019-07-10 16:18:42
  * Copyright © RingCentral. All rights reserved.
  */
-import { IRouter, IJRequest, IJResponse, Handler } from '../../types';
+import {
+ IRouter, IJRequest, IJResponse, Handler
+} from '../../types';
 import _ from 'lodash';
 import pathToRegexp from 'path-to-regexp';
 import { createDebug } from 'sdk/__tests__/utils';
 import { createResponse } from './utils';
-const debug = createDebug('Router');
+
+const debug = createDebug('Router', false);
+const error = createDebug('Router', true);
 
 export class Router implements IRouter {
   private _routes: {
@@ -26,7 +30,7 @@ export class Router implements IRouter {
     const array = this._routes[option.method] || [];
     const target = _.find(array, it => it.regexp.test(option.path));
     return target ? target.handler : null;
-  }
+  };
 
   dispatch = (request: IJRequest) => {
     const array = this._routes[request.method] || [];
@@ -42,7 +46,7 @@ export class Router implements IRouter {
       return target.handler(request, query);
       // debug('dispatch -> target', request.path, target);
     }
-    debug('no rule match', request.path, request.method);
+    error('no rule match', request.path, request.method);
     return createResponse({
       request,
       data: {},
@@ -50,7 +54,7 @@ export class Router implements IRouter {
       statusText: 'Mock data not found',
       headers: {},
     } as IJResponse);
-  }
+  };
 
   use(method: string, path: string, handler: Handler) {
     this._routes[method] = this._routes[method] || [];
