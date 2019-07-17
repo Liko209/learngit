e9ab66152e0677822d316c26bb6f87b7be0f3392
Hank Huang <hank.huang@ringcentral.com>
2019-07-17 09:18:23 +0800

    feat(FIJI-7409): [Jupiter Initial Localization - Hank] 'Update code'

--- tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/AboutDialog.ts
+++ tests/e2e/testcafe/v2/page-models/AppRoot/HomePage/AboutDialog.ts
@@ -12,11 +12,11 @@ export class AboutDialog extends BaseWebComponent {
   }
 
   get content() {
-    return this.getSelectorByAutomationId("DialogContent", this.self)
+    return this.getSelectorByAutomationId("DialogContent", this.self);
   }
 
   get doneButton() {
-    return this.getSelectorByAutomationId("DialogOKButton", this.self)
+    return this.getSelectorByAutomationId("DialogOKButton", this.self);
   }
 
   async clickDoneButton() {
