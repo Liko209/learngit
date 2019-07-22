8c0cf7033cf68b3c5f2b01f3f3b8b53100716be8
Nello Huang <nello.huang@ringcentral.com>
2019-07-22 17:27:29 +0800

    feat(FIJI-7620): [checkbox component] update label color

--- packages/rcui/src/components/Forms/FormLabel.tsx
+++ packages/rcui/src/components/Forms/FormLabel.tsx
@@ -3,10 +3,21 @@
  * @Date: 2019-07-22 15:29:22
  * Copyright © RingCentral. All rights reserved.
  */
+import React from 'react';
 import FormLabel, { FormLabelProps } from '@material-ui/core/FormLabel';
+import styled from '../../foundation/styled-components';
+import { palette } from '../../foundation/shared/theme';
 
 type RuiFormLabelProps = FormLabelProps;
 
-const RuiFormLabel = FormLabel;
+const StyledFormLabel = styled(FormLabel)`
+  && {
+    color: ${palette('text', 'primary')};
+  }
+`;
+
+const RuiFormLabel = React.memo((props: RuiFormLabelProps) => {
+  return <StyledFormLabel {...props} />;
+});
 
 export { RuiFormLabel, RuiFormLabelProps };
