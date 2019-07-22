afa7f502c70a00e20309100985a70445b4dfdfc7
Paynter Chen <paynter.chen@ringcentral.com>
2019-07-22 16:28:57 +0800

    feat(FIJI-6367): [Integration Test] fix npm run test 'foundation/src/src' error

--- jest-base.config.js
+++ jest-base.config.js
@@ -27,7 +27,7 @@ module.exports = {
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
     '^@/(.*)$': '<rootDir>/application/src/$1',
-    '^foundation/(.*)$': '<rootDir>/packages/foundation/src/$1',
+    '^foundation/(?!src)(.*)$': '<rootDir>/packages/foundation/src/$1',
     '^sdk/(.*)$': '<rootDir>/packages/sdk/src/$1',
     '^jui/(.*)$': '<rootDir>/packages/jui/src/$1',
     '^rcui/(.*)$': '<rootDir>/packages/rcui/src/$1',
