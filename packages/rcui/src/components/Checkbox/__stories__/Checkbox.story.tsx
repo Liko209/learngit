d4e7ac31d33457c3b7f6a1b3e62eee3939693c54
Nello Huang <nello.huang@ringcentral.com>
2019-07-22 16:13:17 +0800

    feat(FIJI-7620): [checkbox component] add checkbox

---
+++ packages/rcui/src/components/Checkbox/__stories__/Checkbox.story.tsx
@@ -0,0 +1,57 @@
+/*
+ * @Author: Nello Huang (nello.huang@ringcentral.com)
+ * @Date: 2019-07-22 11:11:47
+ * Copyright © RingCentral. All rights reserved.
+ */
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select } from '@storybook/addon-knobs';
+import { RuiCheckbox } from '../Checkbox';
+import {
+  RuiFormGroup,
+  RuiFormControlLabel,
+  RuiFormControl,
+  RuiFormLabel,
+} from '../../Forms';
+import { alignCenterDecorator } from '../../../storybook/decorators';
+
+storiesOf('CheckBox', module)
+  // .addDecorator(alignCenterDecorator)
+  .add('checkbox', () => {
+    const color = select(
+      'color',
+      {
+        primary: 'primary',
+        secondary: 'secondary',
+        default: 'default',
+      },
+      'primary',
+    );
+    return (
+      <div style={{ padding: '0 30px' }}>
+        <RuiFormControlLabel
+          control={<RuiCheckbox color={color} />}
+          label="SecondarySecondarySecondarySecondarySecondarySecondarySecondarySecondarySecondarySecondary"
+        />
+        <br />
+        <RuiFormControlLabel
+          control={<RuiCheckbox color={color} indeterminate />}
+          label="Indeterminate"
+        />
+        <br />
+        <RuiFormControl>
+          <RuiFormLabel>Pick two</RuiFormLabel>
+          <RuiFormGroup>
+            <RuiFormControlLabel
+              control={<RuiCheckbox color={color} />}
+              label="one"
+            />
+            <RuiFormControlLabel
+              control={<RuiCheckbox color={color} />}
+              label="two"
+            />
+          </RuiFormGroup>
+        </RuiFormControl>
+      </div>
+    );
+  });
---
+++ packages/rcui/src/components/Checkbox/index.ts
@@ -0,0 +1,6 @@
+/*
+ * @Author: Nello Huang (nello.huang@ringcentral.com)
+ * @Date: 2019-07-22 10:37:46
+ * Copyright © RingCentral. All rights reserved.
+ */
+export * from './Checkbox';
