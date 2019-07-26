80c9367adacaca0cf86fd59f0c0c98a99fd0602d
skye.wang <skye.wang@ringcentral.com>
2019-07-26 15:58:41 +0800

    test(FIJI-7672)[E2E] Manually set presence

--- tests/e2e/testcafe/fixtures/PresenceSetting/SetPresence.ts
+++ tests/e2e/testcafe/fixtures/PresenceSetting/SetPresence.ts
@@ -3,7 +3,6 @@
  * @Date: 2019-07-26 10:14:54
  * Copyright © RingCentral. All rights reserved.
  */
-import { v4 as uuid } from 'uuid';
 import { h, H } from '../../v2/helpers'
 import { setupCase, teardownCase } from '../../init';
 import { AppRoot } from '../../v2/page-models/AppRoot';
@@ -16,7 +15,7 @@ fixture('Telephony/ToVoiceMail')
 
 test.meta(<ITestMeta>{
   caseIds: ['JPT-2537'],
-  priority: ['P2'],
+  priority: ['P1'],
   maintainers: ['skye.wang'],
   keywords: ['Presence']
 })('Can manually set presence', async (t) => {
@@ -24,8 +23,6 @@ test.meta(<ITestMeta>{
   await h(t).scenarioHelper.resetProfile(loginUser);
   const app = new AppRoot(t);
   const toolTipAvailable = 'Available';
-  const toolTipInvisible = 'Offline';
-  const toolTipDoNotDisturb = 'Do not disturb';
 
   await h(t).withLog(`Given I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`, async () => {
     await h(t).directLoginWithUser(SITE_URL, loginUser);
@@ -48,4 +45,4 @@ test.meta(<ITestMeta>{
 
   });
 
-});;
+});
