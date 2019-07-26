7b0ab5504f917904f463e2846d2c4871de0f0355
Nello Huang <nello.huang@ringcentral.com>
2019-07-26 18:58:19 +0800

    feat(FIJI-7596): [use mockContainer replace registerModule] add mockContainer

---
+++ tests/shield/src/application/mockContainer.ts
@@ -0,0 +1,115 @@
+/*
+ * @Author: Nello Huang (nello.huang@ringcentral.com)
+ * @Date: 2019-07-26 10:50:57
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { container } from 'framework';
+import { descriptorAOP } from '../core/utils';
+import { mockMethods } from '../shared';
+
+const mockContainerCache = new Map();
+type MockMethod = { method: string; data: any; type?: 'resolve' | 'reject' };
+type MockMethodType = string | MockMethod[];
+
+const _getMockInstance = function(
+  methods?: MockMethodType,
+  mockData?: any,
+  methodType?: 'resolve' | 'reject',
+) {
+  // we don't use real instance only fake a object
+  const instance = {};
+
+  if (Array.isArray(methods)) {
+    methods.forEach(({ method, data, type }) => {
+      mockMethods(instance, method, data, type);
+    });
+  } else if (typeof methods === 'string') {
+    mockMethods(instance, methods, mockData, methodType);
+  } else {
+    // we shouldn't use @mockContainer(key)
+    // throw Error('methods must be array or string');
+  }
+
+  return instance;
+};
+
+const _mockContainer = function(
+  key: any,
+  method?: MockMethodType,
+  mockData?: any,
+  methodType?: 'resolve' | 'reject',
+) {
+  const getInstance = _getMockInstance(method, mockData, methodType);
+  const name = typeof key === 'function' ? key : key.name;
+  mockContainerCache.set(name, getInstance);
+  container.get = jest.fn().mockImplementation((serviceName: string) => {
+    if (mockContainerCache.get(serviceName)) {
+      return mockContainerCache.get(serviceName);
+    }
+    return null;
+  });
+};
+
+const _fakeInstance = function(
+  key: any,
+  method?: MockMethodType,
+  mockData?: any,
+  methodType?: 'resolve' | 'reject',
+) {
+  return function(
+    target: any,
+    property: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const oldFn = descriptor.value;
+    const hasParam = oldFn.length > 0;
+
+    const _mockService = () => {
+      _mockContainer(key, method, mockData, methodType);
+    };
+
+    descriptor.value = descriptorAOP(hasParam, _mockService, oldFn);
+    return descriptor;
+  };
+};
+
+function mockContainer(
+  mockObject: any,
+  method: MockMethodType,
+  mockData?: any,
+) {
+  return _fakeInstance(mockObject, method, mockData);
+}
+
+const _enhanceMethod = (methods: MockMethod[], type: 'resolve' | 'reject') => {
+  return methods.map((method: MockMethod) => {
+    return {
+      ...method,
+      type: method.type || type,
+    };
+  });
+};
+
+mockContainer.resolve = function(
+  mockObject: any,
+  methods: MockMethodType,
+  mockData?: any,
+) {
+  const enhanceMethods = Array.isArray(methods)
+    ? _enhanceMethod(methods, 'resolve')
+    : methods;
+  return _fakeInstance(mockObject, enhanceMethods, mockData, 'resolve');
+};
+
+mockContainer.reject = function(
+  mockObject: any,
+  methods: MockMethodType,
+  mockData?: any,
+) {
+  const enhanceMethods = Array.isArray(methods)
+    ? _enhanceMethod(methods, 'reject')
+    : methods;
+  return _fakeInstance(mockObject, enhanceMethods, mockData, 'reject');
+};
+
+export { mockContainer };
