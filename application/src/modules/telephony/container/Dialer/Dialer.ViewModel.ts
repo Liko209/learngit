c5db231756c539bae97cc056055bc6c8ac3c379d
Joy Zhang <joy.zhang@ringcentral.com>
2019-07-29 13:29:35 +0800

    feat(FIJI-7621): [Dialer] show dialog on dialer

--- application/src/modules/telephony/container/Dialer/Dialer.ViewModel.ts
+++ application/src/modules/telephony/container/Dialer/Dialer.ViewModel.ts
@@ -6,17 +6,25 @@
 
 import { StoreViewModel } from '@/store/ViewModel';
 import { container } from 'framework';
-import { computed } from 'mobx';
+import { computed, action } from 'mobx';
+import { i18nP } from '@/utils/i18nT';
 import { DialerProps, DialerViewProps } from './types';
+import { CALL_WINDOW_STATUS } from '../../FSM';
 import { TelephonyStore } from '../../store';
+import { TelephonyService } from '../../service';
 import { CALL_STATE } from 'sdk/module/telephony/entity';
 import { analyticsCollector } from '@/AnalyticsCollector';
+import { TELEPHONY_SERVICE } from '@/modules/telephony/interface/constant';
+import { simpleE911Dialog, alertE911Dialog } from '../E911Dialog';
 
-class DialerViewModel extends StoreViewModel<DialerProps>
-  implements DialerViewProps {
+class DialerViewModel extends StoreViewModel<DialerProps> implements DialerViewProps {
   private _telephonyStore: TelephonyStore = container.get(TelephonyStore);
+  private _telephonyService: TelephonyService = container.get(TELEPHONY_SERVICE);
 
   dialerId = this._telephonyStore.dialerId;
+
+  shouldShowConfirm = true;
+  shouldShowPrompt = true;
 
   constructor(props: DialerProps) {
     super(props);
@@ -28,6 +36,50 @@ class DialerViewModel extends StoreViewModel<DialerProps>
         }
       },
     );
+    this.reaction(
+      () => this.callWindowState,
+      async (callWindowState: CALL_WINDOW_STATUS) => {
+        if (!this.shouldDisplayDialer || callWindowState === CALL_WINDOW_STATUS.MINIMIZED) {
+          return;
+        }
+        const needConfirmE911 = await this._telephonyService.needConfirmE911();
+        if (!needConfirmE911 && this.shouldShowPrompt) {
+          return this.showPromptDialog();
+        }
+        if (needConfirmE911 && this.shouldShowConfirm) {
+          return this.showConfirmDialog();
+        }
+      },
+    );
+  }
+
+  @computed
+  get isDetached() {
+    return this._telephonyStore.isDetached;
+  }
+
+  @action
+  showConfirmDialog() {
+    alertE911Dialog({
+      id: 'emergencyConfirm',
+      content: i18nP('telephony.prompt.emergencyAddressIsUnknown'),
+      onOK: this._telephonyService.openE911,
+      onCancel: () => {
+        this.shouldShowConfirm = false;
+      },
+      okText: i18nP('telephony.e911.confirmEmergencyAddress'),
+    });
+  }
+
+  @action
+  showPromptDialog() {
+    simpleE911Dialog({
+      id: 'emergencyPrompt',
+      content: i18nP('telephony.prompt.e911ServiceMayBeLimitedOrUnavailable'),
+      onCancel: () => {
+        this.shouldShowPrompt = false;
+      },
+    });
   }
 
   @computed
