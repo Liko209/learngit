6758c5b26fb8622721581d8fd7899dbffd0ac5ab
andy.hu <andy.hu@ringcentral.com>
2019-07-30 16:10:51 +0800

    fix(FIJI-7565): [[Message] Implement notification and sounds] fix when user choose off

--- application/src/modules/notification/agent/SoundNotification.ts
+++ application/src/modules/notification/agent/SoundNotification.ts
@@ -1,6 +1,11 @@
 import { IMediaService, MediaOptions } from '@/interface/media';
 import { ISoundNotification, Sounds } from '../interface';
-import { RingsList, SoundsList } from 'sdk/module/profile';
+import {
+  RingsList,
+  SoundsList,
+  SOUNDS_TYPE,
+  RINGS_TYPE,
+} from 'sdk/module/profile';
 import { mainLogger } from 'sdk';
 
 export class SoundNotification implements ISoundNotification {
@@ -8,7 +13,8 @@ export class SoundNotification implements ISoundNotification {
   private _library: Map<string, string> = new Map();
   constructor() {
     [...SoundsList, ...RingsList].forEach(item => {
-      this._library.set(item.id, item.url);
+      if (![SOUNDS_TYPE.Off, RINGS_TYPE.Off].includes(item.id))
+        this._library.set(item.id, item.url);
     });
   }
   create(name: Sounds, opts: Exclude<MediaOptions, 'src'>) {
