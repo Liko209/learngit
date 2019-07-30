684a0f6a3cc41fa147df1f704125d3cffd802962
Valor Lin <valor.lin@ringcentral.com>
2019-07-30 14:29:59 +0800

    style(FIJI-7692): [Implement <VirtualizedSelect/> component] chore

--- packages/jui/src/foundation/utils/filterReactElement.ts
+++ packages/jui/src/foundation/utils/filterReactElement.ts
@@ -1,10 +1,9 @@
-import React from 'react';
-
 /*
  * @Author: Valor Lin (valor.lin@ringcentral.com)
  * @Date: 2019-07-26 19:10:57
  * Copyright © RingCentral. All rights reserved.
  */
+import React from 'react';
 
 function filterReactElement<T>(children: React.ReactNode) {
   const elementChildren: React.ReactElement<T>[] = [];
