bc57aee0477b87299eb0b6bb4c926f10269d812e
andy.hu <andy.hu@ringcentral.com>
2019-07-31 11:25:57 +0800

    feat(FIJI-7684): [[DND] Mute sounds] fix lint

--- application/src/modules/notification/manager/NotificationManager.ts
+++ application/src/modules/notification/manager/NotificationManager.ts
@@ -3,15 +3,7 @@
  * @Date: 2019-04-01 15:16:45
  * Copyright Ã‚Â© RingCentral. All rights reserved.
  */
-import { computed } from 'mobx';
-import { getGlobalValue, getEntity } from '@/store/utils';
-import { GLOBAL_KEYS } from '@/store/constants';
-import { ENTITY_NAME } from '@/store';
-import { Presence } from 'sdk/module/presence/entity';
-import PresenceModel from '@/store/models/Presence';
-import { PRESENCE } from 'sdk/module/presence/constant';
-import { Person } from 'sdk/module/person/entity';
-import PersonModel from '@/store/models/Person';
+
 import { INotificationService, NotificationOpts } from '../interface';
 
 type NotificationId = number | string;
