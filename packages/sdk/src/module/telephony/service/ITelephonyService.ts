1dcb721d06d0408e4cdb5682c7a2018a58310fe3
Lip Wang <lip.wang@ringcentral.com>
2019-08-01 01:30:35 +0000

    feat(FIJI-6206): [Track 4 mini events in weather map for Jupiter]

--- packages/sdk/src/module/telephony/service/ITelephonyService.ts
+++ packages/sdk/src/module/telephony/service/ITelephonyService.ts
@@ -1,5 +1,10 @@
+import { TelephonyDataCollectionInfoConfigType } from '../types';
+
 export interface ITelephonyService {
   getVoipCallPermission: () => Promise<boolean>;
   getRingerDevicesList: () => MediaDeviceInfo[];
   isEmergencyAddrConfirmed: () => boolean;
+  setDataCollectionInfoConfig: (
+    info: TelephonyDataCollectionInfoConfigType,
+  ) => void;
 }
