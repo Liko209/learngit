514a6edd38aec6e02dc03e84e724f8cdc0364b36
ken.li <ken.li@ringcentral.com>
2019-08-02 14:30:59 +0800

    feat(FIJI-6623): [@team mention] adjust function

--- application/src/AnalyticsCollector/AnalyticsCollector.ts
+++ application/src/AnalyticsCollector/AnalyticsCollector.ts
@@ -89,7 +89,7 @@ class AnalyticsCollector {
     source: string,
     postType: string,
     destination: string,
-    isAtTeam: boolean,
+    isAtTeam = false,
   ) {
     dataAnalysis.track('Jup_Web/DT_msg_postSent', {
       source,
