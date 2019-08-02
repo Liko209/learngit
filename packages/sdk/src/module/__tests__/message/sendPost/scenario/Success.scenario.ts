a4c6e4880aeecaea26eda87a27c741f9621e7fa9
Paynter Chen <paynter.chen@ringcentral.com>
2019-08-02 10:16:41 +0800

    feat(FIJI-7526): [IntegrationTest scenario case] use class to implement Scenario

---
+++ packages/sdk/src/module/__tests__/message/sendPost/scenario/Success.scenario.ts
@@ -0,0 +1,59 @@
+/*
+ * @Author: Paynter Chen
+ * @Date: 2019-07-25 16:21:56
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { readJson, ScenarioFactory, ItContext, Scenario } from 'shield/sdk';
+import { IGlipPostPost } from 'shield/sdk/mocks/glip/api/post/post.post.contract';
+import { GlipInitialDataHelper } from 'shield/sdk/mocks/glip/data/data';
+import { GlipGroup } from 'shield/sdk/mocks/glip/types';
+import { GlipScenario } from 'shield/sdk/mocks/glip/GlipScenario';
+
+export class Success extends GlipScenario {
+  post: { text: string; id: number; };
+  team: GlipGroup;
+  constructor(
+    protected context: ItContext,
+    protected glipIndexDataHelper: GlipInitialDataHelper,
+    public props?: {
+      teamId?: number;
+      teamName?: string;
+      uids?: number[];
+      postText?: string;
+    },
+  ) {
+    super(context, glipIndexDataHelper, props);
+    const {
+      teamName = 'Test Team with thomas',
+      uids = [123],
+      postText = '',
+      teamId = undefined,
+    } = props || {};
+    const { helper } = context;
+    const glipDataHelper = helper.glipDataHelper();
+    const team = this.team = glipDataHelper.team.createTeam(
+      teamName,
+      uids,
+      teamId ? { _id: teamId } : undefined,
+    );
+    glipIndexDataHelper.teams.insertOrUpdate(team);
+    // glipIndexDataHelper.profile.insertOrUpdate(i)
+
+    this.post = helper.mockResponse(
+      readJson<IGlipPostPost>(require('../data/SEND_POST.SUCCESS.json')),
+      data => {
+        // modify the response
+        data.request.data!.group_id = team._id;
+        data.response.data.group_id = team._id;
+        postText && (data.response.data.text = postText);
+        data.response.data._id = helper
+          .glipDataHelper()
+          .post.factory.build()._id;
+        return {
+          text: postText || data.request.data!.text!,
+          id: data.response.data._id,
+        };
+      },
+    );
+  }
+}
---
+++ tests/shield/src/sdk/BaseScenario.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Paynter Chen
+ * @Date: 2019-07-25 16:21:56
+ * Copyright © RingCentral. All rights reserved.
+ */
+import { ItContext } from './types';
+
+export class BaseScenario {
+  constructor(
+    protected context: ItContext
+  ) {}
+}
