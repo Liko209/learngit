6f0fb3456bde154c41546d83dc8f53d04a53994a
Travis Xu <travis.xu@ringcentral.com>
2019-08-02 10:45:55 +0000

    chore(FIJI-7965): [provide a simple service script to relay beacon message to sumologic] commit

---
+++ scripts/relay.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const https = require("https");
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = Number(process.env.PORT) || 7000;
+
+http.createServer(function (request, response) {
+    let data = "";
+
+    request.on("data", function (chunk) {
+        data += chunk;
+    });
+
+    request.on("end", function () {
+        response.writeHead(201);
+        response.end();
+
+        data = JSON.parse(data);
+        const options = {
+            method: 'POST',
+            headers: data.headers
+        };
+        sendRequest(data.url, options, Buffer.from(data.content || '', 'base64'));
+    });
+}).listen(PORT, HOST);
+console.log(`server is started, listening on ${HOST}:${PORT}`);
+
+function sendRequest(url, options, content) {
+    const protocol = url.startsWith("https") ? https : http;
+    const request = protocol.request(url, options, function (response) {
+        let result = "";
+        response.setEncoding("utf8");
+        response.on("data", function (chunk) {
+            result += chunk;
+        });
+        response.on("end", function () {
+            const timestamp = new Date().toISOString();
+            const msg = {
+                url,
+                statusCode: response.statusCode,
+            };
+            if (response.statusCode === 200) {
+                console.log(`INFO ${timestamp} ${JSON.stringify(msg)}`);
+            } else {
+                msg.requestHeaders = options.headers;
+                msg.requestData = content;
+                msg.responseHeaders = response.headers;
+                msg.responseData = result;
+                console.error(`ERROR ${timestamp} ${JSON.stringify(msg)}`);
+            }
+        });
+    });
+
+    request.on("error", function (error) {
+        const timestamp = new Date().toISOString();
+        const msg = {
+            url,
+            requestHeaders: options.headers,
+            requestData: content,
+        };
+        console.error(`ERROR ${timestamp} ${JSON.stringify(msg)}`);
+    });
+    request.write(content);
+    request.end();
+}
