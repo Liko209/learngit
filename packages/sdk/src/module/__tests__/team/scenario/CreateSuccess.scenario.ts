9cedcc181ac32d83722193f0b61814dd0adf3288
Paynter Chen <paynter.chen@ringcentral.com>
2019-08-05 12:52:38 +0800

    feat(FIJI-7526): [IntegrationTest scenario case] remove base CreateEntity Scenario

--- packages/sdk/src/module/__tests__/team/scenario/CreateSuccess.scenario.ts
+++ packages/sdk/src/module/__tests__/team/scenario/CreateSuccess.scenario.ts
@@ -3,25 +3,36 @@
  * @Date: 2019-07-25 16:21:56
  * Copyright © RingCentral. All rights reserved.
  */
-import { ItContext } from 'shield/sdk';
+import { readApiJson, ItContext, get } from 'shield/sdk';
 import { GlipInitialDataHelper } from 'shield/sdk/mocks/glip/data/data';
 import { GlipGroup } from 'shield/sdk/mocks/glip/types';
-import { UpdateSpec } from 'foundation/utils/update';
-import { CreateEntitySuccess } from './CreateEntitySuccess.scenario';
+import { GlipScenario } from 'shield/sdk/mocks/glip/GlipScenario';
+import { UpdateSpec, update } from 'foundation/utils/update';
+import { IGlipTeamPost } from 'shield/sdk/mocks/glip/api/team/team.post.contract';
 
-export class CreateSuccess extends CreateEntitySuccess<GlipGroup> {
-  protected templateJson = require('../data/CREATE_TEAM.SUCCESS.json');
+export class CreateSuccess extends GlipScenario {
+  createdTeam: GlipGroup;
   constructor(
     protected context: ItContext,
     protected glipIndexDataHelper: GlipInitialDataHelper,
     public props: {
-      entity?: UpdateSpec<GlipGroup>;
+      teamInfo?: UpdateSpec<GlipGroup>;
     },
   ) {
     super(context, glipIndexDataHelper, props);
-  }
+    const { helper } = context;
 
-  getApiJson() {
-    return require('../data/CREATE_TEAM.SUCCESS.json');
+    const templateJson = readApiJson<IGlipTeamPost>(
+      require('../data/CREATE_TEAM.SUCCESS.json'),
+    );
+    const modifiedJson = update(templateJson, {
+      response: {
+        data: get(props, v => v.teamInfo),
+      },
+    });
+    this.createdTeam = helper.mockResponse(
+      modifiedJson,
+      api => api.response.data,
+    );
   }
 }
