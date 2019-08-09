757c6f5b218d8c3abc28845a8ac9dc7c9a4d377e
Looper Wang <looper.wang@ringcentral.com>
2019-08-09 10:52:13 +0800

    feat(FIJI-8150): [Convert to team only available for groups not for 1:1.] update .

--- application/src/containers/NewConversation/NewConversation.View.tsx
+++ application/src/containers/NewConversation/NewConversation.View.tsx
@@ -67,18 +67,17 @@ class NewConversationComponent extends React.Component<NewConversationProps> {
       >
         <Loading loading={loading} alwaysComponentShow delay={0}>
           <JuiTopText data-test-automation-id="newConversationDescription">
+            {t('people.prompt.newConversationBaseTip')}
             {members.length > 2 ? (
               <Trans
-                defaults={t('people.prompt.newConversationTip')}
+                defaults={t('people.prompt.newConversationGroupTip')}
                 components={[
                   <JuiLink handleOnClick={this._openConvertToTeam}>
                     convert to a team
                   </JuiLink>,
                 ]}
               />
-            ) : (
-              t('people.prompt.newConversationTipForOneOne')
-            )}
+            ) : null}
           </JuiTopText>
           <ContactSearch
             onSelectChange={handleSearchContactChange}
