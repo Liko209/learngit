b824b06e0c965555fdbd5a9687fe8c283bd235d1
Chris Zhan <chris.zhan@ringcentral.com>
2019-08-09 14:51:26 +0800

    fix(FIJI-8147): [[sometimes]The count and avatar in members list is incorrect when resizing page] fix observe timing

--- application/src/modules/message/container/RightShelfMemberList/RightShelfMemberList.View.tsx
+++ application/src/modules/message/container/RightShelfMemberList/RightShelfMemberList.View.tsx
@@ -57,8 +57,19 @@ class RightShelfMemberListViewComponent extends Component<Props> {
 
   componentDidMount() {
     this.props.init();
-    if (this._resizeObserver && this._header.current) {
-      this._resizeObserver.observe(this._header.current);
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this._resizeObserver) {
+      if (
+        this._header.current &&
+        prevProps.shouldHide &&
+        !this.props.shouldHide
+      ) {
+        this._resizeObserver.observe(this._header.current);
+      } else if (!prevProps.shouldHide && this.props.shouldHide) {
+        this._resizeObserver.disconnect();
+      }
     }
   }
 
@@ -134,6 +145,7 @@ class RightShelfMemberListViewComponent extends Component<Props> {
       shownGuestIds,
       allMemberLength,
       isTeam,
+      shouldHide,
     } = this.props;
     const addButtonTip = isTeam
       ? t('people.team.addTeamMembers')
@@ -143,8 +155,6 @@ class RightShelfMemberListViewComponent extends Component<Props> {
       CONVERSATION_TYPES.ME,
       CONVERSATION_TYPES.SMS,
     ].includes(group.type);
-    const shouldHide =
-      !group || group.isMocked || group.type === CONVERSATION_TYPES.ME;
     return shouldHide ? null : (
       <>
         <MemberListHeader
