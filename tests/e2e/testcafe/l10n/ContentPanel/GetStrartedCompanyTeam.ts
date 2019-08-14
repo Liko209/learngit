590710d1e5d33cd1ea678db8fd8dfeefdf25f451
Hanny Han <hanny.han@ringcentral.com>
2019-08-14 10:17:51 +0800

    test(FIJI-8248): [Jupiter 1.7 Localization - Localization Automation - Hanny] 'Add V1.7 case'

---
+++ tests/e2e/testcafe/l10n/ContentPanel/GetStrartedCompanyTeam.ts
@@ -0,0 +1,51 @@
+import { setupCase, teardownCase } from "../../init";
+import { BrandTire, SITE_URL } from "../../config";
+import { formalName } from "../../libs/filter";
+import { AppRoot } from "../../v2/page-models/AppRoot";
+import { h } from "../../v2/helpers";
+import { v4 as uuid } from 'uuid';
+import * as _ from "lodash";
+import { IGroup } from "../../v2/models";
+
+fixture('ContentPanel/GetStartedCompanyTeam')
+.beforeEach(setupCase(BrandTire.RCOFFICE))
+.afterEach(teardownCase())
+
+test(formalName('Check the GetStarted page ', ['P2', 'ContentPanel','Messages', 'GetStartedCompanyTeam', 'V1.6', 'Hanny.Han']),
+async(t: TestController) => {
+
+  const users=h(t).rcData.mainCompany.users;
+  const loginUser = users[4];
+  const app=new AppRoot(t);
+
+  let team = <IGroup>{
+    name: "CompanyTeam",
+    type: "Team",
+    owner: loginUser,
+    members: [loginUser],
+  }
+
+  await h(t).withLog(`Given I own a company team: "${team.name}"`, async () => {
+    await h(t).scenarioHelper.createTeam(team);
+  });
+
+  await h(t).withLog(`And I login Jupiter with ${loginUser.company.number}#${loginUser.extension}`,async()=>{
+    await h(t).directLoginWithUser(SITE_URL, loginUser);
+    await app.homePage.ensureLoaded();
+  })
+
+  const conversationPage = app.homePage.messageTab.conversationPage;
+  const rightRail=app.homePage.messageTab.rightRail;
+  await h(t).withLog(`When I open the company team ${team.name}`, async () => {
+    await app.homePage.messageTab.teamsSection.conversationEntryById(team.glipId).enter();
+    await conversationPage.waitUntilPostsBeLoaded;
+    try{
+      await  rightRail.clickExpandStatusButton();
+    }
+    catch(e){
+      console.log("needn't click show detail");
+     }
+  });
+
+  await h(t).log('Then I capture screenshot',{screenshotPath:'Jupiter_ContentPanel_GetStartedCompanyTeam'});
+});
