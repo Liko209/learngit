38d505650b1a09b08c83ae78f79653d9453c3403
Lip Wang <lip.wang@ringcentral.com>
2019-08-29 09:06:07 +0800

    feat(FIJI-8463): [LD should be removed from Core] add UT

--- application/src/permissions/ld/LaunchDarklyController.ts
+++ application/src/permissions/ld/LaunchDarklyController.ts
@@ -47,8 +47,9 @@ class LaunchDarklyController extends AbstractPermissionController
     );
     const person = await personService.getById(userId);
     const host: string = window.location.host;
+    const { clientId } = Api.httpConfig.launchdarkly;
     const params = {
-      clientId: Api.httpConfig.launchdarkly.clientId,
+      clientId,
       user: {
         key: `${userId}`,
         name: (person && person['display_name']) || '',
@@ -67,7 +68,6 @@ class LaunchDarklyController extends AbstractPermissionController
         mainLogger.log('incoming event launchDarklyUpdateCallback');
       },
     };
-    const { clientId } = Api.httpConfig.launchdarkly;
     const isRunningE2E = EnvConfig.getIsRunningE2E();
     if (clientId && !isRunningE2E) {
       this.launchDarklyClient = new LaunchDarklyClient(params);
