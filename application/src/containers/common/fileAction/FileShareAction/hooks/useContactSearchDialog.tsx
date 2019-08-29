bc7450caba47ac40767b8739e38cf39b75a84bab
andy.hu <andy.hu@ringcentral.com>
2019-08-29 14:42:55 +0800

    fix(FIJI-5729): [Share a file to another conversation] add sort func

--- application/src/containers/common/fileAction/FileShareAction/hooks/useContactSearchDialog.tsx
+++ application/src/containers/common/fileAction/FileShareAction/hooks/useContactSearchDialog.tsx
@@ -3,18 +3,29 @@ import { Dialog } from '@/containers/Dialog';
 import React, { useCallback } from 'react';
 import { ServiceLoader, ServiceConfig } from 'sdk/module/serviceLoader';
 import { SearchService } from 'sdk/module/search';
+import { SortableModel } from 'sdk/src/framework/model';
+import { Group } from 'sdk/module/group';
 
 const searchFunc = async (searchKey: string) => {
   const searchService = ServiceLoader.getInstance<SearchService>(
     ServiceConfig.SEARCH_SERVICE,
   );
+  const sortFunc = (a: SortableModel<Group>, b: SortableModel<Group>) =>
+    b.entity.most_recent_content_modified_at -
+    a.entity.most_recent_content_modified_at;
+
   const result = await searchService.doFuzzySearchPersonsAndGroups(
     searchKey,
-    { excludeSelf: false, fetchAllIfSearchKeyEmpty: false, meFirst: true },
+    {
+      excludeSelf: false,
+      fetchAllIfSearchKeyEmpty: false,
+      meFirst: true,
+    },
     {
       myGroupsOnly: true,
       fetchAllIfSearchKeyEmpty: true,
       meFirst: true,
+      sortFunc,
     },
   );
   return result.sortableModels;
