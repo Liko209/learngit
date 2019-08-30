b0a127b2c90764963bc6102c7873034f428bc1da
andy.hu <andy.hu@ringcentral.com>
2019-08-30 10:09:21 +0800

    fix(FIJI-8692): [The recent conversation list is incorrect when the user share a file] added test

--- application/src/containers/common/fileAction/FileShareAction/hooks/useContactSearchDialog.tsx
+++ application/src/containers/common/fileAction/FileShareAction/hooks/useContactSearchDialog.tsx
@@ -7,32 +7,29 @@ import { SortableModel } from 'sdk/framework/model';
 import { Group } from 'sdk/module/group';
 import { SortUtils } from 'sdk/framework/utils';
 
+const sortFunc = (a: SortableModel<Group>, b: SortableModel<Group>) => {
+  const result = SortUtils.compareArrayOfSameLens(b.sortWeights, a.sortWeights);
+  if (result !== 0) {
+    return result;
+  }
+
+  const mostRecentA = a.entity.most_recent_post_created_at || 0;
+  const mostRecentB = b.entity.most_recent_post_created_at || 0;
+  if (mostRecentA > mostRecentB) {
+    return -1;
+  }
+
+  if (mostRecentA < mostRecentB) {
+    return 1;
+  }
+
+  return SortUtils.compareLowerCaseString(a.lowerCaseName, b.lowerCaseName);
+};
+
 const searchFunc = async (searchKey: string) => {
   const searchService = ServiceLoader.getInstance<SearchService>(
     ServiceConfig.SEARCH_SERVICE,
   );
-  const sortFunc = (a: SortableModel<Group>, b: SortableModel<Group>) => {
-    const result = SortUtils.compareArrayOfSameLens(
-      b.sortWeights,
-      a.sortWeights,
-    );
-    if (result !== 0) {
-      return result;
-    }
-
-    const mostRecentA = a.entity.most_recent_post_created_at || 0;
-    const mostRecentB = b.entity.most_recent_post_created_at || 0;
-    if (mostRecentA > mostRecentB) {
-      return -1;
-    }
-
-    if (mostRecentA < mostRecentB) {
-      return 1;
-    }
-
-    return SortUtils.compareLowerCaseString(a.lowerCaseName, b.lowerCaseName);
-  };
-
   const result = await searchService.doFuzzySearchPersonsAndGroups(
     searchKey,
     {
@@ -49,7 +46,6 @@ const searchFunc = async (searchKey: string) => {
     },
     searchKey ? undefined : sortFunc,
   );
-
   return result.sortableModels;
 };
 
@@ -75,4 +71,4 @@ const useContactSearchDialog = (
   }, [onSelect]);
   return [openDialog];
 };
-export { useContactSearchDialog };
+export { useContactSearchDialog, sortFunc };
