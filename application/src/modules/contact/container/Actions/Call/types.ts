ac3687584da9863a411354f2e14dc8940b43cf66
Nello Huang <nello.huang@ringcentral.com>
2019-09-02 10:02:20 +0800

    fix(FIJI-8735): [Shouldn't display Call button when hover on login user's row] if is me not show call

--- application/src/modules/contact/container/Actions/Call/types.ts
+++ application/src/modules/contact/container/Actions/Call/types.ts
@@ -8,6 +8,7 @@ import { BUTTON_TYPE } from 'jui/pattern/Phone/VoicemailItem';
 import { NavType, ContactType } from '../types';
 
 type CallProps = {
+  id: number;
   entity: NavType;
   contactType: ContactType;
   phoneNumber: string;
@@ -15,6 +16,7 @@ type CallProps = {
 
 type CallViewProps = CallProps & {
   type: BUTTON_TYPE;
+  isMe: boolean;
   doCall: (event: React.MouseEvent<HTMLElement, MouseEvent>) => Promise<void>;
 };
 
